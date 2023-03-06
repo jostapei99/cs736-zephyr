Balaklaka/zephyr

Bluetooth: Mesh: Add support for Opcodes Aggregator models
Includes Opcodes Aggregator Server and Client. To use Opcodes Aggregator
client features with some client model, that client should support async
API.

Signed-off-by: Michał Narajowski <michal.narajowski@codecoup.pl>

include/zephyr/bluetooth/mesh/access.h
@@ -132,6 +132,8 @@ struct bt_mesh_elem {
 #define BT_MESH_MODEL_ID_REMOTE_PROV_CLI           0x0005
 #define BT_MESH_MODEL_ID_SAR_CFG_SRV               0xbf52
 #define BT_MESH_MODEL_ID_SAR_CFG_CLI               0xbf53
+#define BT_MESH_MODEL_ID_OP_AGG_SRV                0xbf54
+#define BT_MESH_MODEL_ID_OP_AGG_CLI                0xbf55
 #define BT_MESH_MODEL_ID_LARGE_COMP_DATA_SRV       0xbf56
 #define BT_MESH_MODEL_ID_LARGE_COMP_DATA_CLI       0xbf57
 #define BT_MESH_MODEL_ID_PRIV_BEACON_SRV           0xbf40

subsys/bluetooth/mesh/access.c
@@ -27,6 +27,7 @@
 #include "transport.h"
 #include "access.h"
 #include "foundation.h"
+#include "op_agg.h"
 #include "settings.h"
 
 /* Model publication information for persistent storage. */
@@ -1015,12 +1016,54 @@ static int get_opcode(struct net_buf_simple *buf, uint32_t *opcode)
 	CODE_UNREACHABLE;
 }
 
-void bt_mesh_model_recv(struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+static int element_model_recv(struct bt_mesh_msg_ctx *ctx,
+			      struct net_buf_simple *buf, uint16_t addr,
+			      struct bt_mesh_elem *elem, uint32_t opcode)
 {
-	struct bt_mesh_model *model;
 	const struct bt_mesh_model_op *op;
+	struct bt_mesh_model *model;
+	struct net_buf_simple_state state;
+	int err;
+
+	op = find_op(elem, opcode, &model);
+	if (!op) {
+		BT_ERR("No OpCode 0x%08x for elem 0x%02x", opcode, elem->addr);
+		return ACCESS_STATUS_WRONG_OPCODE;
+	}
+
+	if (!bt_mesh_model_has_key(model, ctx->app_idx)) {
+		BT_ERR("Wrong key");
+		return ACCESS_STATUS_WRONG_KEY;
+	}
+
+	if (!model_has_dst(model, addr)) {
+		BT_ERR("Invalid address 0x%02x", addr);
+		return ACCESS_STATUS_INVALID_ADDRESS;
+	}
+
+	if ((op->len >= 0) && (buf->len < (size_t)op->len)) {
+		BT_ERR("Too short message for OpCode 0x%08x", opcode);
+		return ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD;
+	} else if ((op->len < 0) && (buf->len != (size_t)(-op->len))) {
+		BT_ERR("Invalid message size for OpCode 0x%08x", opcode);
+		return ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD;
+	}
+
+	net_buf_simple_save(buf, &state);
+	err = op->func(model, ctx, buf);
+	net_buf_simple_restore(buf, &state);
+
+	if (err) {
+		return ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD;
+	}
+	return ACCESS_STATUS_SUCCESS;
+}
+
+int bt_mesh_model_recv(struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+{
+	int err = ACCESS_STATUS_SUCCESS;
 	uint32_t opcode;
-	int i;
+	uint16_t index;
 
 	BT_DBG("app_idx 0x%04x src 0x%04x dst 0x%04x", ctx->app_idx, ctx->addr,
 	       ctx->recv_dst);
@@ -1033,55 +1076,47 @@ void bt_mesh_model_recv(struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 
 	if (get_opcode(buf, &opcode) < 0) {
 		BT_WARN("Unable to decode OpCode");
-		return;
+		return ACCESS_STATUS_WRONG_OPCODE;
 	}
 
 	BT_DBG("OpCode 0x%08x", opcode);
 
-	for (i = 0; i < dev_comp->elem_count; i++) {
-		struct net_buf_simple_state state;
+	if (BT_MESH_ADDR_IS_UNICAST(ctx->recv_dst)) {
+		index = ctx->recv_dst - dev_comp->elem[0].addr;
 
-		op = find_op(&dev_comp->elem[i], opcode, &model);
-		if (!op) {
-			BT_DBG("No OpCode 0x%08x for elem %d", opcode, i);
-			continue;
+		if (index >= dev_comp->elem_count) {
+			BT_ERR("Invalid address 0x%02x", ctx->recv_dst);
+			err = ACCESS_STATUS_INVALID_ADDRESS;
+		} else {
+			struct bt_mesh_elem *elem = &dev_comp->elem[index];
+
+			err = element_model_recv(ctx, buf, ctx->recv_dst, elem, opcode);
 		}
+	} else {
+		for (index = 0; index < dev_comp->elem_count; index++) {
+			struct bt_mesh_elem *elem = &dev_comp->elem[index];
 
-		if (!bt_mesh_model_has_key(model, ctx->app_idx)) {
-			continue;
+			if (bt_mesh_elem_find_group(elem, ctx->recv_dst)) {
+				(void)element_model_recv(ctx, buf, ctx->recv_dst, elem, opcode);
+			}
 		}
-
-		if (!model_has_dst(model, ctx->recv_dst)) {
-			continue;
-		}
-
-		if ((op->len >= 0) && (buf->len < (size_t)op->len)) {
-			BT_ERR("Too short message for OpCode 0x%08x", opcode);
-			continue;
-		} else if ((op->len < 0) && (buf->len != (size_t)(-op->len))) {
-			BT_ERR("Invalid message size for OpCode 0x%08x",
-			       opcode);
-			continue;
-		}
-
-		/* The callback will likely parse the buffer, so
-		 * store the parsing state in case multiple models
-		 * receive the message.
-		 */
-		net_buf_simple_save(buf, &state);
-		(void)op->func(model, ctx, buf);
-		net_buf_simple_restore(buf, &state);
 	}
 
 	if (IS_ENABLED(CONFIG_BT_MESH_ACCESS_LAYER_MSG) && msg_cb) {
 		msg_cb(opcode, ctx, buf);
 	}
+
+	return err;
 }
 
 int bt_mesh_model_send(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx,
 		       struct net_buf_simple *msg,
 		       const struct bt_mesh_send_cb *cb, void *cb_data)
 {
+	if (IS_ENABLED(CONFIG_BT_MESH_OP_AGG) && bt_mesh_op_agg_accept(ctx)) {
+		return bt_mesh_op_agg_send(model, ctx, msg, cb);
+	}
+
 	if (!bt_mesh_model_has_key(model, ctx->app_idx)) {
 		BT_ERR("Model not bound to AppKey 0x%04x", ctx->app_idx);
 		return -EINVAL;

subsys/bluetooth/mesh/foundation.h
@@ -97,7 +97,8 @@
 #define OP_SAR_CFG_RX_GET                  BT_MESH_MODEL_OP_2(0xB8, 0x06)
 #define OP_SAR_CFG_RX_SET                  BT_MESH_MODEL_OP_2(0xB8, 0x07)
 #define OP_SAR_CFG_RX_STATUS               BT_MESH_MODEL_OP_2(0xB8, 0x08)
-
+#define OP_OPCODES_AGGREGATOR_SEQUENCE     BT_MESH_MODEL_OP_2(0xB8, 0x09)
+#define OP_OPCODES_AGGREGATOR_STATUS       BT_MESH_MODEL_OP_2(0xB8, 0x10)
 #define OP_LARGE_COMP_DATA_GET             BT_MESH_MODEL_OP_2(0xB8, 0x11)
 #define OP_LARGE_COMP_DATA_STATUS          BT_MESH_MODEL_OP_2(0xB8, 0x12)
 #define OP_MODELS_METADATA_GET             BT_MESH_MODEL_OP_2(0xB8, 0x13)
@@ -131,6 +132,13 @@
 #define STATUS_CANNOT_SET                  0x0f
 #define STATUS_UNSPECIFIED                 0x10
 #define STATUS_INVALID_BINDING             0x11
+
+#define ACCESS_STATUS_SUCCESS                0x00
+#define ACCESS_STATUS_INVALID_ADDRESS        0x01
+#define ACCESS_STATUS_WRONG_KEY              0x02
+#define ACCESS_STATUS_WRONG_OPCODE           0x03
+#define ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD 0x04
+#define ACCESS_STATUS_RESPONSE_OVERFLOW      0x05
 
 void bt_mesh_model_reset(void);
 

subsys/bluetooth/mesh/op_agg_cli.c
@@ -1,0 +1,208 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <bluetooth/mesh.h>
+
+#define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_MESH_DEBUG_MODEL)
+#define LOG_MODULE_NAME bt_mesh_op_agg_cli
+#include "common/log.h"
+
+#include "access.h"
+#include "foundation.h"
+#include "net.h"
+#include "mesh.h"
+#include "op_agg.h"
+
+/** Mesh Opcodes Aggregator Client Model Context */
+static struct bt_mesh_op_agg_cli {
+	/** Composition data model entry pointer. */
+	struct bt_mesh_model *model;
+
+	/* Internal parameters for tracking message responses. */
+	struct bt_mesh_msg_ack_ctx ack_ctx;
+} cli;
+
+static int32_t msg_timeout;
+
+static int handle_status(struct bt_mesh_model *model,
+			 struct bt_mesh_msg_ctx *ctx,
+			 struct net_buf_simple *buf)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+	struct net_buf_simple msg;
+	uint8_t status;
+	uint16_t elem_addr, addr;
+	int err;
+
+	BT_DBG("net_idx 0x%04x app_idx 0x%04x src 0x%04x len %u: %s",
+	       ctx->net_idx, ctx->app_idx, ctx->addr, buf->len,
+	       bt_hex(buf->data, buf->len));
+
+	if (!bt_mesh_msg_ack_ctx_match(&cli.ack_ctx,
+				       OP_OPCODES_AGGREGATOR_STATUS, ctx->addr,
+				       NULL)) {
+		BT_WARN("Unexpected Opcodes Aggregator Status");
+		return -ENOENT;
+	}
+
+	status = net_buf_simple_pull_u8(buf);
+	elem_addr = net_buf_simple_pull_le16(buf);
+
+	while (buf->len > 0) {
+		err = bt_mesh_op_agg_decode_msg(&msg, buf);
+		if (err) {
+			BT_ERR("Cannot decode aggregated message %d", err);
+			bt_mesh_op_agg_ctx_reinit();
+			return -EINVAL;
+		}
+
+		if (agg->srcs->len < 2) {
+			BT_ERR("Mismatch in sources address buffer");
+			bt_mesh_op_agg_ctx_reinit();
+			return -ENOENT;
+		}
+
+		addr = net_buf_simple_pull_le16(agg->srcs);
+
+		/* Empty item means unacked msg. */
+		if (!msg.len) {
+			continue;
+		}
+
+		ctx->recv_dst = addr;
+		err = bt_mesh_model_recv(ctx, &msg);
+		if (err) {
+			BT_ERR("Opcodes Aggregator receive error %d", err);
+			bt_mesh_op_agg_ctx_reinit();
+			return err;
+		}
+	}
+
+	bt_mesh_msg_ack_ctx_rx(&cli.ack_ctx);
+
+	return 0;
+}
+
+const struct bt_mesh_model_op _bt_mesh_op_agg_cli_op[] = {
+	{ OP_OPCODES_AGGREGATOR_STATUS, BT_MESH_LEN_MIN(3), handle_status },
+	BT_MESH_MODEL_OP_END,
+};
+
+static int op_agg_cli_init(struct bt_mesh_model *model)
+{
+	if (!bt_mesh_model_in_primary(model)) {
+		BT_ERR("Opcodes Aggregator Client only allowed in primary element");
+		return -EINVAL;
+	}
+
+	/* Opcodes Aggregator Client model shall use the device key and
+	 * application keys.
+	 */
+	model->keys[0] = BT_MESH_KEY_DEV_ANY;
+
+	msg_timeout = CONFIG_BT_MESH_OP_AGG_CLI_TIMEOUT;
+	cli.model = model;
+	bt_mesh_msg_ack_ctx_init(&cli.ack_ctx);
+
+	return 0;
+}
+
+int bt_mesh_op_agg_cli_seq_start(uint16_t net_idx, uint16_t app_idx, uint16_t dst,
+				 uint16_t elem_addr)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	if (!BT_MESH_ADDR_IS_UNICAST(elem_addr)) {
+		BT_ERR("Element address shall be a unicast address");
+		return -EINVAL;
+	}
+
+	if (agg->initialized) {
+		BT_ERR("Opcodes Aggregator is already configured");
+		return -EALREADY;
+	}
+
+	agg->net_idx = net_idx;
+	agg->app_idx = app_idx;
+	agg->addr = dst;
+	agg->ack = false;
+	agg->rsp_err = 0;
+	agg->initialized = true;
+
+	net_buf_simple_init(agg->srcs, 0);
+	bt_mesh_model_msg_init(agg->sdu, OP_OPCODES_AGGREGATOR_SEQUENCE);
+	net_buf_simple_add_le16(agg->sdu, elem_addr);
+
+	return 0;
+}
+
+int bt_mesh_op_agg_cli_seq_send(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+	struct bt_mesh_msg_ctx ctx = {
+		.net_idx = agg->net_idx,
+		.app_idx = agg->app_idx,
+		.addr = agg->addr,
+	};
+	int err;
+
+	if (!agg->initialized) {
+		BT_ERR("Opcodes Aggregator not initialized");
+		return -EINVAL;
+	}
+
+	err = bt_mesh_msg_ack_ctx_prepare(&cli.ack_ctx, OP_OPCODES_AGGREGATOR_STATUS, agg->addr,
+					  NULL);
+	if (err) {
+		return err;
+	}
+
+	agg->initialized = false;
+
+	err = bt_mesh_model_send(cli.model, &ctx, agg->sdu, NULL, NULL);
+	if (err) {
+		BT_ERR("model_send() failed (err %d)", err);
+		bt_mesh_msg_ack_ctx_clear(&cli.ack_ctx);
+		return err;
+	}
+
+	return bt_mesh_msg_ack_ctx_wait(&cli.ack_ctx, K_MSEC(msg_timeout));
+}
+
+void bt_mesh_op_agg_cli_seq_abort(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	agg->initialized = false;
+}
+
+bool bt_mesh_op_agg_cli_seq_is_started(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	return agg->initialized;
+}
+
+size_t bt_mesh_op_agg_cli_seq_tailroom(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	return net_buf_simple_tailroom(agg->sdu);
+}
+
+int32_t bt_mesh_op_agg_cli_timeout_get(void)
+{
+	return msg_timeout;
+}
+
+void bt_mesh_op_agg_cli_timeout_set(int32_t timeout)
+{
+	msg_timeout = timeout;
+}
+
+const struct bt_mesh_model_cb _bt_mesh_op_agg_cli_cb = {
+	.init = op_agg_cli_init,
+};

subsys/bluetooth/mesh/op_agg_srv.c
@@ -1,0 +1,126 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <bluetooth/mesh.h>
+
+#define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_MESH_DEBUG_MODEL)
+#define LOG_MODULE_NAME bt_mesh_op_agg_srv
+#include "common/log.h"
+
+#include "access.h"
+#include "foundation.h"
+#include "net.h"
+#include "mesh.h"
+#include "transport.h"
+#include "op_agg.h"
+
+/** Mesh Opcodes Aggragator Server Model Context */
+static struct bt_mesh_op_agg_srv {
+	/** Composition data model entry pointer. */
+	struct bt_mesh_model *model;
+} srv;
+
+static int handle_sequence(struct bt_mesh_model *model,
+			   struct bt_mesh_msg_ctx *ctx,
+			   struct net_buf_simple *buf)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+	struct net_buf_simple_state state;
+	struct net_buf_simple msg;
+	uint16_t elem;
+	uint8_t *status;
+	int err;
+
+	elem = net_buf_simple_pull_le16(buf);
+	ctx->recv_dst = elem;
+
+	bt_mesh_model_msg_init(agg->sdu, OP_OPCODES_AGGREGATOR_STATUS);
+	status = net_buf_simple_add_u8(agg->sdu, 0);
+	net_buf_simple_add_le16(agg->sdu, elem);
+
+	agg->net_idx = ctx->net_idx;
+	agg->app_idx = ctx->app_idx;
+	agg->addr = ctx->addr;
+	agg->initialized = true;
+
+	if (!BT_MESH_ADDR_IS_UNICAST(elem)) {
+		BT_WARN("Address is not unicast, ignoring.");
+		return -EINVAL;
+	}
+
+	net_buf_simple_save(buf, &state);
+	while (buf->len > 0) {
+		err = bt_mesh_op_agg_decode_msg(&msg, buf);
+		if (err) {
+			BT_ERR("Unable to parse Opcodes Aggregator Sequence message (err %d)", err);
+			return err;
+		}
+	}
+	net_buf_simple_restore(buf, &state);
+
+	if (!bt_mesh_elem_find(elem)) {
+		*status = ACCESS_STATUS_INVALID_ADDRESS;
+		goto send;
+	}
+
+	while (buf->len > 0) {
+		(void) bt_mesh_op_agg_decode_msg(&msg, buf);
+
+		agg->ack = false;
+		agg->rsp_err = 0;
+		err = bt_mesh_model_recv(ctx, &msg);
+
+		if (agg->rsp_err) {
+			*status = agg->rsp_err;
+			break;
+		}
+
+		if (err) {
+			*status = err;
+			break;
+		}
+
+		if (!agg->ack) {
+			net_buf_simple_add_u8(agg->sdu, 0);
+		}
+	}
+
+send:
+	agg->initialized = false;
+	err = bt_mesh_model_send(model, ctx, agg->sdu, NULL, NULL);
+	if (err) {
+		BT_ERR("Unable to send Opcodes Aggregator Status");
+		return err;
+	}
+
+	return 0;
+}
+
+const struct bt_mesh_model_op _bt_mesh_op_agg_srv_op[] = {
+	{ OP_OPCODES_AGGREGATOR_SEQUENCE, BT_MESH_LEN_MIN(2), handle_sequence },
+	BT_MESH_MODEL_OP_END,
+};
+
+static int op_agg_srv_init(struct bt_mesh_model *model)
+{
+	if (!bt_mesh_model_in_primary(model)) {
+		BT_ERR("Opcodes Aggregator Server only allowed in primary element");
+		return -EINVAL;
+	}
+
+	/* Opcodes Aggregator Server model shall use the device key and
+	 * application keys.
+	 */
+	model->keys[0] = BT_MESH_KEY_DEV_ANY;
+
+	srv.model = model;
+
+	return 0;
+}
+
+const struct bt_mesh_model_cb _bt_mesh_op_agg_srv_cb = {
+	.init = op_agg_srv_init,
+};

jostapei99/cs736-zephyr

Co-authored-by: Ludvig Samuelsen Jordet <ludvig.jordet@nordicsemi.no>
Co-authored-by: Mia Koen <mia.koen@nordicsemi.no>
Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

include/zephyr/bluetooth/mesh/access.h
@@ -132,6 +132,8 @@ struct bt_mesh_elem {
 #define BT_MESH_MODEL_ID_PRIV_BEACON_CLI           0x000b
 #define BT_MESH_MODEL_ID_SAR_CFG_SRV               0x000e
 #define BT_MESH_MODEL_ID_SAR_CFG_CLI               0x000f
+#define BT_MESH_MODEL_ID_OP_AGG_SRV                0x0010
+#define BT_MESH_MODEL_ID_OP_AGG_CLI                0x0011
 #define BT_MESH_MODEL_ID_LARGE_COMP_DATA_SRV       0x0012
 #define BT_MESH_MODEL_ID_LARGE_COMP_DATA_CLI       0x0013
 

subsys/bluetooth/mesh/access.c
@@ -25,6 +25,7 @@
 #include "transport.h"
 #include "access.h"
 #include "foundation.h"
+#include "op_agg.h"
 #include "settings.h"
 
 #define LOG_LEVEL CONFIG_BT_MESH_ACCESS_LOG_LEVEL
@@ -1017,12 +1018,54 @@ static int get_opcode(struct net_buf_simple *buf, uint32_t *opcode)
 	CODE_UNREACHABLE;
 }
 
-void bt_mesh_model_recv(struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+static int element_model_recv(struct bt_mesh_msg_ctx *ctx,
+			      struct net_buf_simple *buf, uint16_t addr,
+			      struct bt_mesh_elem *elem, uint32_t opcode)
 {
-	struct bt_mesh_model *model;
 	const struct bt_mesh_model_op *op;
+	struct bt_mesh_model *model;
+	struct net_buf_simple_state state;
+	int err;
+
+	op = find_op(elem, opcode, &model);
+	if (!op) {
+		LOG_ERR("No OpCode 0x%08x for elem 0x%02x", opcode, elem->addr);
+		return ACCESS_STATUS_WRONG_OPCODE;
+	}
+
+	if (!bt_mesh_model_has_key(model, ctx->app_idx)) {
+		LOG_ERR("Wrong key");
+		return ACCESS_STATUS_WRONG_KEY;
+	}
+
+	if (!model_has_dst(model, addr)) {
+		LOG_ERR("Invalid address 0x%02x", addr);
+		return ACCESS_STATUS_INVALID_ADDRESS;
+	}
+
+	if ((op->len >= 0) && (buf->len < (size_t)op->len)) {
+		LOG_ERR("Too short message for OpCode 0x%08x", opcode);
+		return ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD;
+	} else if ((op->len < 0) && (buf->len != (size_t)(-op->len))) {
+		LOG_ERR("Invalid message size for OpCode 0x%08x", opcode);
+		return ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD;
+	}
+
+	net_buf_simple_save(buf, &state);
+	err = op->func(model, ctx, buf);
+	net_buf_simple_restore(buf, &state);
+
+	if (err) {
+		return ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD;
+	}
+	return ACCESS_STATUS_SUCCESS;
+}
+
+int bt_mesh_model_recv(struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+{
+	int err = ACCESS_STATUS_SUCCESS;
 	uint32_t opcode;
-	int i;
+	uint16_t index;
 
 	LOG_DBG("app_idx 0x%04x src 0x%04x dst 0x%04x", ctx->app_idx, ctx->addr,
 		ctx->recv_dst);
@@ -1035,54 +1078,45 @@ void bt_mesh_model_recv(struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 
 	if (get_opcode(buf, &opcode) < 0) {
 		LOG_WRN("Unable to decode OpCode");
-		return;
+		return ACCESS_STATUS_WRONG_OPCODE;
 	}
 
 	LOG_DBG("OpCode 0x%08x", opcode);
 
-	for (i = 0; i < dev_comp->elem_count; i++) {
-		struct net_buf_simple_state state;
+	if (BT_MESH_ADDR_IS_UNICAST(ctx->recv_dst)) {
+		index = ctx->recv_dst - dev_comp->elem[0].addr;
 
-		op = find_op(&dev_comp->elem[i], opcode, &model);
-		if (!op) {
-			LOG_DBG("No OpCode 0x%08x for elem %d", opcode, i);
-			continue;
+		if (index >= dev_comp->elem_count) {
+			LOG_ERR("Invalid address 0x%02x", ctx->recv_dst);
+			err = ACCESS_STATUS_INVALID_ADDRESS;
+		} else {
+			struct bt_mesh_elem *elem = &dev_comp->elem[index];
+
+			err = element_model_recv(ctx, buf, ctx->recv_dst, elem, opcode);
 		}
+	} else {
+		for (index = 0; index < dev_comp->elem_count; index++) {
+			struct bt_mesh_elem *elem = &dev_comp->elem[index];
 
-		if (!bt_mesh_model_has_key(model, ctx->app_idx)) {
-			continue;
+			(void)element_model_recv(ctx, buf, ctx->recv_dst, elem, opcode);
 		}
-
-		if (!model_has_dst(model, ctx->recv_dst)) {
-			continue;
-		}
-
-		if ((op->len >= 0) && (buf->len < (size_t)op->len)) {
-			LOG_ERR("Too short message for OpCode 0x%08x", opcode);
-			continue;
-		} else if ((op->len < 0) && (buf->len != (size_t)(-op->len))) {
-			LOG_ERR("Invalid message size for OpCode 0x%08x", opcode);
-			continue;
-		}
-
-		/* The callback will likely parse the buffer, so
-		 * store the parsing state in case multiple models
-		 * receive the message.
-		 */
-		net_buf_simple_save(buf, &state);
-		(void)op->func(model, ctx, buf);
-		net_buf_simple_restore(buf, &state);
 	}
 
 	if (IS_ENABLED(CONFIG_BT_MESH_ACCESS_LAYER_MSG) && msg_cb) {
 		msg_cb(opcode, ctx, buf);
 	}
+
+	return err;
 }
 
 int bt_mesh_model_send(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx,
 		       struct net_buf_simple *msg,
 		       const struct bt_mesh_send_cb *cb, void *cb_data)
 {
+	if (IS_ENABLED(CONFIG_BT_MESH_OP_AGG) && bt_mesh_op_agg_accept(ctx)) {
+		return bt_mesh_op_agg_send(model, ctx, msg, cb);
+	}
+
 	if (!bt_mesh_model_has_key(model, ctx->app_idx)) {
 		LOG_ERR("Model not bound to AppKey 0x%04x", ctx->app_idx);
 		return -EINVAL;

subsys/bluetooth/mesh/foundation.h
@@ -97,6 +97,8 @@
 #define OP_SAR_CFG_RX_GET                  BT_MESH_MODEL_OP_2(0x80, 0x6f)
 #define OP_SAR_CFG_RX_SET                  BT_MESH_MODEL_OP_2(0x80, 0x70)
 #define OP_SAR_CFG_RX_STATUS               BT_MESH_MODEL_OP_2(0x80, 0x71)
+#define OP_OPCODES_AGGREGATOR_SEQUENCE     BT_MESH_MODEL_OP_2(0x80, 0x72)
+#define OP_OPCODES_AGGREGATOR_STATUS       BT_MESH_MODEL_OP_2(0x80, 0x73)
 #define OP_LARGE_COMP_DATA_GET             BT_MESH_MODEL_OP_2(0x80, 0x74)
 #define OP_LARGE_COMP_DATA_STATUS          BT_MESH_MODEL_OP_2(0x80, 0x75)
 #define OP_MODELS_METADATA_GET             BT_MESH_MODEL_OP_2(0x80, 0x76)
@@ -130,6 +132,13 @@
 #define STATUS_CANNOT_SET                  0x0f
 #define STATUS_UNSPECIFIED                 0x10
 #define STATUS_INVALID_BINDING             0x11
+
+#define ACCESS_STATUS_SUCCESS                0x00
+#define ACCESS_STATUS_INVALID_ADDRESS        0x01
+#define ACCESS_STATUS_WRONG_KEY              0x02
+#define ACCESS_STATUS_WRONG_OPCODE           0x03
+#define ACCESS_STATUS_MESSAGE_NOT_UNDERSTOOD 0x04
+#define ACCESS_STATUS_RESPONSE_OVERFLOW      0x05
 
 void bt_mesh_model_reset(void);
 

subsys/bluetooth/mesh/op_agg_cli.c
@@ -1,0 +1,210 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/bluetooth/mesh.h>
+
+#include <common/bt_str.h>
+
+#include "access.h"
+#include "foundation.h"
+#include "net.h"
+#include "mesh.h"
+#include "op_agg.h"
+
+#define LOG_LEVEL CONFIG_BT_MESH_MODEL_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(bt_mesh_op_agg_cli);
+
+/** Mesh Opcodes Aggregator Client Model Context */
+static struct bt_mesh_op_agg_cli {
+	/** Composition data model entry pointer. */
+	struct bt_mesh_model *model;
+
+	/* Internal parameters for tracking message responses. */
+	struct bt_mesh_msg_ack_ctx ack_ctx;
+} cli;
+
+static int32_t msg_timeout;
+
+static int handle_status(struct bt_mesh_model *model,
+			 struct bt_mesh_msg_ctx *ctx,
+			 struct net_buf_simple *buf)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+	struct net_buf_simple msg;
+	uint8_t status;
+	uint16_t elem_addr, addr;
+	int err;
+
+	LOG_DBG("net_idx 0x%04x app_idx 0x%04x src 0x%04x len %u: %s",
+		ctx->net_idx, ctx->app_idx, ctx->addr, buf->len,
+		bt_hex(buf->data, buf->len));
+
+	if (!bt_mesh_msg_ack_ctx_match(&cli.ack_ctx,
+				       OP_OPCODES_AGGREGATOR_STATUS, ctx->addr,
+				       NULL)) {
+		LOG_WRN("Unexpected Opcodes Aggregator Status");
+		return -ENOENT;
+	}
+
+	status = net_buf_simple_pull_u8(buf);
+	elem_addr = net_buf_simple_pull_le16(buf);
+
+	while (buf->len > 0) {
+		err = bt_mesh_op_agg_decode_msg(&msg, buf);
+		if (err) {
+			LOG_ERR("Cannot decode aggregated message %d", err);
+			bt_mesh_op_agg_ctx_reinit();
+			return -EINVAL;
+		}
+
+		if (agg->srcs->len < 2) {
+			LOG_ERR("Mismatch in sources address buffer");
+			bt_mesh_op_agg_ctx_reinit();
+			return -ENOENT;
+		}
+
+		addr = net_buf_simple_pull_le16(agg->srcs);
+
+		/* Empty item means unacked msg. */
+		if (!msg.len) {
+			continue;
+		}
+
+		ctx->recv_dst = addr;
+		err = bt_mesh_model_recv(ctx, &msg);
+		if (err) {
+			LOG_ERR("Opcodes Aggregator receive error %d", err);
+			bt_mesh_op_agg_ctx_reinit();
+			return err;
+		}
+	}
+
+	bt_mesh_msg_ack_ctx_rx(&cli.ack_ctx);
+
+	return 0;
+}
+
+const struct bt_mesh_model_op _bt_mesh_op_agg_cli_op[] = {
+	{ OP_OPCODES_AGGREGATOR_STATUS, BT_MESH_LEN_MIN(3), handle_status },
+	BT_MESH_MODEL_OP_END,
+};
+
+static int op_agg_cli_init(struct bt_mesh_model *model)
+{
+	if (!bt_mesh_model_in_primary(model)) {
+		LOG_ERR("Opcodes Aggregator Client only allowed in primary element");
+		return -EINVAL;
+	}
+
+	/* Opcodes Aggregator Client model shall use the device key and
+	 * application keys.
+	 */
+	model->keys[0] = BT_MESH_KEY_DEV_ANY;
+
+	msg_timeout = CONFIG_BT_MESH_OP_AGG_CLI_TIMEOUT;
+	cli.model = model;
+	bt_mesh_msg_ack_ctx_init(&cli.ack_ctx);
+
+	return 0;
+}
+
+int bt_mesh_op_agg_cli_seq_start(uint16_t net_idx, uint16_t app_idx, uint16_t dst,
+				 uint16_t elem_addr)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	if (!BT_MESH_ADDR_IS_UNICAST(elem_addr)) {
+		LOG_ERR("Element address shall be a unicast address");
+		return -EINVAL;
+	}
+
+	if (agg->initialized) {
+		LOG_ERR("Opcodes Aggregator is already configured");
+		return -EALREADY;
+	}
+
+	agg->net_idx = net_idx;
+	agg->app_idx = app_idx;
+	agg->addr = dst;
+	agg->ack = false;
+	agg->rsp_err = 0;
+	agg->initialized = true;
+
+	net_buf_simple_init(agg->srcs, 0);
+	bt_mesh_model_msg_init(agg->sdu, OP_OPCODES_AGGREGATOR_SEQUENCE);
+	net_buf_simple_add_le16(agg->sdu, elem_addr);
+
+	return 0;
+}
+
+int bt_mesh_op_agg_cli_seq_send(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+	struct bt_mesh_msg_ctx ctx = {
+		.net_idx = agg->net_idx,
+		.app_idx = agg->app_idx,
+		.addr = agg->addr,
+	};
+	int err;
+
+	if (!agg->initialized) {
+		LOG_ERR("Opcodes Aggregator not initialized");
+		return -EINVAL;
+	}
+
+	err = bt_mesh_msg_ack_ctx_prepare(&cli.ack_ctx, OP_OPCODES_AGGREGATOR_STATUS, agg->addr,
+					  NULL);
+	if (err) {
+		return err;
+	}
+
+	agg->initialized = false;
+
+	err = bt_mesh_model_send(cli.model, &ctx, agg->sdu, NULL, NULL);
+	if (err) {
+		LOG_ERR("model_send() failed (err %d)", err);
+		bt_mesh_msg_ack_ctx_clear(&cli.ack_ctx);
+		return err;
+	}
+
+	return bt_mesh_msg_ack_ctx_wait(&cli.ack_ctx, K_MSEC(msg_timeout));
+}
+
+void bt_mesh_op_agg_cli_seq_abort(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	agg->initialized = false;
+}
+
+bool bt_mesh_op_agg_cli_seq_is_started(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	return agg->initialized;
+}
+
+size_t bt_mesh_op_agg_cli_seq_tailroom(void)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+
+	return net_buf_simple_tailroom(agg->sdu);
+}
+
+int32_t bt_mesh_op_agg_cli_timeout_get(void)
+{
+	return msg_timeout;
+}
+
+void bt_mesh_op_agg_cli_timeout_set(int32_t timeout)
+{
+	msg_timeout = timeout;
+}
+
+const struct bt_mesh_model_cb _bt_mesh_op_agg_cli_cb = {
+	.init = op_agg_cli_init,
+};

subsys/bluetooth/mesh/op_agg_srv.c
@@ -1,0 +1,127 @@
+/*
+ * Copyright (c) 2022 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/bluetooth/mesh.h>
+
+#include "access.h"
+#include "foundation.h"
+#include "net.h"
+#include "mesh.h"
+#include "transport.h"
+#include "op_agg.h"
+
+#define LOG_LEVEL CONFIG_BT_MESH_MODEL_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(bt_mesh_op_agg_srv);
+
+/** Mesh Opcodes Aggragator Server Model Context */
+static struct bt_mesh_op_agg_srv {
+	/** Composition data model entry pointer. */
+	struct bt_mesh_model *model;
+} srv;
+
+static int handle_sequence(struct bt_mesh_model *model,
+			   struct bt_mesh_msg_ctx *ctx,
+			   struct net_buf_simple *buf)
+{
+	struct op_agg_ctx *agg = bt_mesh_op_agg_ctx_get();
+	struct net_buf_simple_state state;
+	struct net_buf_simple msg;
+	uint16_t elem;
+	uint8_t *status;
+	int err;
+
+	elem = net_buf_simple_pull_le16(buf);
+	ctx->recv_dst = elem;
+
+	bt_mesh_model_msg_init(agg->sdu, OP_OPCODES_AGGREGATOR_STATUS);
+	status = net_buf_simple_add_u8(agg->sdu, 0);
+	net_buf_simple_add_le16(agg->sdu, elem);
+
+	agg->net_idx = ctx->net_idx;
+	agg->app_idx = ctx->app_idx;
+	agg->addr = ctx->addr;
+	agg->initialized = true;
+
+	if (!BT_MESH_ADDR_IS_UNICAST(elem)) {
+		LOG_WRN("Address is not unicast, ignoring.");
+		return -EINVAL;
+	}
+
+	net_buf_simple_save(buf, &state);
+	while (buf->len > 0) {
+		err = bt_mesh_op_agg_decode_msg(&msg, buf);
+		if (err) {
+			LOG_ERR("Unable to parse Opcodes Aggregator Sequence message (err %d)",
+				err);
+			return err;
+		}
+	}
+	net_buf_simple_restore(buf, &state);
+
+	if (!bt_mesh_elem_find(elem)) {
+		*status = ACCESS_STATUS_INVALID_ADDRESS;
+		goto send;
+	}
+
+	while (buf->len > 0) {
+		(void) bt_mesh_op_agg_decode_msg(&msg, buf);
+
+		agg->ack = false;
+		agg->rsp_err = 0;
+		err = bt_mesh_model_recv(ctx, &msg);
+
+		if (agg->rsp_err) {
+			*status = agg->rsp_err;
+			break;
+		}
+
+		if (err) {
+			*status = err;
+			break;
+		}
+
+		if (!agg->ack) {
+			net_buf_simple_add_u8(agg->sdu, 0);
+		}
+	}
+
+send:
+	agg->initialized = false;
+	err = bt_mesh_model_send(model, ctx, agg->sdu, NULL, NULL);
+	if (err) {
+		LOG_ERR("Unable to send Opcodes Aggregator Status");
+		return err;
+	}
+
+	return 0;
+}
+
+const struct bt_mesh_model_op _bt_mesh_op_agg_srv_op[] = {
+	{ OP_OPCODES_AGGREGATOR_SEQUENCE, BT_MESH_LEN_MIN(2), handle_sequence },
+	BT_MESH_MODEL_OP_END,
+};
+
+static int op_agg_srv_init(struct bt_mesh_model *model)
+{
+	if (!bt_mesh_model_in_primary(model)) {
+		LOG_ERR("Opcodes Aggregator Server only allowed in primary element");
+		return -EINVAL;
+	}
+
+	/* Opcodes Aggregator Server model shall use the device key and
+	 * application keys.
+	 */
+	model->keys[0] = BT_MESH_KEY_DEV_ANY;
+
+	srv.model = model;
+
+	return 0;
+}
+
+const struct bt_mesh_model_cb _bt_mesh_op_agg_srv_cb = {
+	.init = op_agg_srv_init,
+};