Balaklaka/zephyr

Tests: Bluetooth: Mesh: BLOB cli mod broadcast API
Add bsim test for broadcast API of the BLOB client model.

Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_broadcast.sh
@@ -1,0 +1,11 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+RunTest mesh_blob \
+	blob_cli_broadcast_basic
+
+RunTest mesh_blob \
+	blob_cli_broadcast_trans

jostapei99/cs736-zephyr

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_broadcast.sh
@@ -1,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+conf=prj_mesh1d1_conf
+RunTest blob_broadcast \
+	blob_cli_broadcast_basic
+
+conf=prj_mesh1d1_conf
+RunTest blob_broadcast \
+	blob_cli_broadcast_trans