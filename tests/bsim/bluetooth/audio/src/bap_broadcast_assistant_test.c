zephyrproject-rtos/zephyr

Bluetooth: Audio: Add BASS server sync test case
Add a test case that tests the scan delegator and
broadcast assistant features when the scan delegator
syncs to a PA without being asked to by the
broadcast assistant.

Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_audio/src/bap_broadcast_assistant_test.c
@@ -26,6 +26,10 @@ static volatile bool g_pa_synced;
 static volatile bool g_state_synced;
 static volatile uint8_t g_src_id;
 static volatile uint32_t g_broadcast_id;
+static volatile uint8_t g_recv_state_count;
+CREATE_FLAG(flag_recv_state_read);
+CREATE_FLAG(flag_recv_state_updated);
+CREATE_FLAG(flag_recv_state_removed);
 
 static volatile bool g_cb;
 static struct bt_conn *g_conn;
@@ -55,6 +59,7 @@ static void bap_broadcast_assistant_discover_cb(struct bt_conn *conn, int err,
 	}
 
 	printk("BASS discover done with %u recv states\n", recv_state_count);
+	g_recv_state_count = recv_state_count;
 	g_discovery_complete = true;
 }
 
@@ -99,6 +104,13 @@ static void bap_broadcast_assistant_recv_state_cb(
 		return;
 	}
 
+	SET_FLAG(flag_recv_state_read);
+
+	if (state == NULL) {
+		/* Empty receive state */
+		return;
+	}
+
 	bt_addr_le_to_str(&state->addr, le_addr, sizeof(le_addr));
 	(void)bin2hex(state->bad_code, BT_BAP_BROADCAST_CODE_SIZE, bad_code,
 		      sizeof(bad_code));
@@ -134,6 +146,8 @@ static void bap_broadcast_assistant_recv_state_cb(
 
 	g_src_id = state->src_id;
 	g_cb = true;
+
+	SET_FLAG(flag_recv_state_updated);
 }
 
 static void bap_broadcast_assistant_recv_state_removed_cb(struct bt_conn *conn, int err,
@@ -146,6 +160,8 @@ static void bap_broadcast_assistant_recv_state_removed_cb(struct bt_conn *conn, 
 
 	printk("BASS recv state %u removed\n", src_id);
 	g_cb = true;
+
+	SET_FLAG(flag_recv_state_removed);
 }
 
 static void bap_broadcast_assistant_scan_start_cb(struct bt_conn *conn, int err)
@@ -311,6 +327,25 @@ static void test_bass_discover(void)
 	printk("Discovery complete\n");
 }
 
+static void test_bass_read_receive_states(void)
+{
+	for (uint8_t i = 0U; i < g_recv_state_count; i++) {
+		int err;
+
+		UNSET_FLAG(flag_recv_state_read);
+		err = bt_bap_broadcast_assistant_read_recv_state(g_conn, i);
+		if (err != 0) {
+			FAIL("Failed to read receive state with idx %u: %d\n",
+			     i, err);
+			return;
+		}
+
+		WAIT_FOR_FLAG(flag_recv_state_read);
+	}
+
+	printk("Receive state read complete\n");
+}
+
 static void test_bass_scan_start(void)
 {
 	int err;
@@ -453,7 +488,7 @@ static void test_bass_remove_source(void)
 	printk("Source removed\n");
 }
 
-static void test_main(void)
+static int common_init(void)
 {
 	int err;
 
@@ -461,7 +496,7 @@ static void test_main(void)
 
 	if (err != 0) {
 		FAIL("Bluetooth enable failed (err %d)\n", err);
-		return;
+		return err;
 	}
 
 	bt_conn_cb_register(&conn_callbacks);
@@ -473,7 +508,7 @@ static void test_main(void)
 	err = bt_le_scan_start(BT_LE_SCAN_PASSIVE, device_found);
 	if (err != 0) {
 		FAIL("Scanning failed to start (err %d)\n", err);
-		return;
+		return err;
 	}
 
 	printk("Scanning successfully started\n");
@@ -482,6 +517,21 @@ static void test_main(void)
 
 	test_exchange_mtu();
 	test_bass_discover();
+	test_bass_read_receive_states();
+
+	return 0;
+}
+
+static void test_main_client_sync(void)
+{
+	int err;
+
+	err = common_init();
+	if (err != 0) {
+		FAIL("Bluetooth enable failed (err %d)\n", err);
+		return;
+	}
+
 	test_bass_scan_start();
 	test_bass_scan_stop();
 	test_bass_create_pa_sync();
@@ -490,15 +540,64 @@ static void test_main(void)
 	test_bass_broadcast_code();
 	test_bass_remove_source();
 
-	PASS("BAP broadcast assistant Passed\n");
+	PASS("BAP Broadcast Assistant Client Sync Passed\n");
+}
+
+static void test_main_server_sync_client_rem(void)
+{
+	int err;
+
+	err = common_init();
+	if (err != 0) {
+		FAIL("Bluetooth enable failed (err %d)\n", err);
+		return;
+	}
+
+	WAIT_FOR_FLAG(flag_recv_state_updated);
+
+	test_bass_broadcast_code();
+	test_bass_remove_source();
+
+	PASS("BAP Broadcast Assistant Server Sync Passed\n");
+}
+
+static void test_main_server_sync_server_rem(void)
+{
+	int err;
+
+	err = common_init();
+	if (err != 0) {
+		FAIL("Bluetooth enable failed (err %d)\n", err);
+		return;
+	}
+
+	WAIT_FOR_FLAG(flag_recv_state_updated);
+
+	test_bass_broadcast_code();
+
+	WAIT_FOR_FLAG(flag_recv_state_removed);
+
+	PASS("BAP Broadcast Assistant Server Sync Passed\n");
 }
 
 static const struct bst_test_instance test_bass[] = {
 	{
-		.test_id = "bap_broadcast_assistant",
+		.test_id = "bap_broadcast_assistant_client_sync",
 		.test_post_init_f = test_init,
 		.test_tick_f = test_tick,
-		.test_main_f = test_main
+		.test_main_f = test_main_client_sync,
+	},
+	{
+		.test_id = "bap_broadcast_assistant_server_sync_client_rem",
+		.test_post_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = test_main_server_sync_client_rem,
+	},
+	{
+		.test_id = "bap_broadcast_assistant_server_sync_server_rem",
+		.test_post_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = test_main_server_sync_server_rem,
 	},
 	BSTEST_END_MARKER
 };

tests/bluetooth/bsim_bt/bsim_test_audio/test_scripts/bass_server_sync_client_rem.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020-2022 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: Apache-2.0
+
+SIMULATION_ID="bass_server_sync_client_rem"
+VERBOSITY_LEVEL=2
+PROCESS_IDS=""; EXIT_CODE=0
+
+function Execute(){
+  if [ ! -f $1 ]; then
+    echo -e "  \e[91m`pwd`/`basename $1` cannot be found (did you forget to\
+ compile it?)\e[39m"
+    exit 1
+  fi
+  timeout 20 $@ & PROCESS_IDS="$PROCESS_IDS $!"
+}
+
+: "${BSIM_OUT_PATH:?BSIM_OUT_PATH must be defined}"
+
+#Give a default value to BOARD if it does not have one yet:
+BOARD="${BOARD:-nrf52_bsim}"
+
+cd ${BSIM_OUT_PATH}/bin
+
+printf "\n\n======== Running BASS Server Sync Client Remove =========\n\n"
+
+Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 \
+  -testid=bap_scan_delegator_server_sync_client_rem -rs=24
+
+Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 \
+  -testid=bap_broadcast_assistant_server_sync_client_rem -rs=46
+
+  Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=2 -testid=bass_broadcaster -rs=69
+
+# Simulation time should be larger than the WAIT_TIME in common.h
+Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -D=3 \
+  -sim_length=60e6 $@
+
+for PROCESS_ID in $PROCESS_IDS; do
+  wait $PROCESS_ID || let "EXIT_CODE=$?"
+done
+
+exit $EXIT_CODE #the last exit code != 0

tests/bluetooth/bsim_bt/bsim_test_audio/test_scripts/bass_server_sync_server_rem.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020-2022 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: Apache-2.0
+
+SIMULATION_ID="bass_server_sync_server_rem"
+VERBOSITY_LEVEL=2
+PROCESS_IDS=""; EXIT_CODE=0
+
+function Execute(){
+  if [ ! -f $1 ]; then
+    echo -e "  \e[91m`pwd`/`basename $1` cannot be found (did you forget to\
+ compile it?)\e[39m"
+    exit 1
+  fi
+  timeout 20 $@ & PROCESS_IDS="$PROCESS_IDS $!"
+}
+
+: "${BSIM_OUT_PATH:?BSIM_OUT_PATH must be defined}"
+
+#Give a default value to BOARD if it does not have one yet:
+BOARD="${BOARD:-nrf52_bsim}"
+
+cd ${BSIM_OUT_PATH}/bin
+
+printf "\n\n======== Running BASS Server Sync Server Remove =========\n\n"
+
+Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 \
+  -testid=bap_scan_delegator_server_sync_server_rem -rs=24
+
+Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 \
+  -testid=bap_broadcast_assistant_server_sync_server_rem -rs=46
+
+  Execute ./bs_${BOARD}_tests_bluetooth_bsim_bt_bsim_test_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=2 -testid=bass_broadcaster -rs=69
+
+# Simulation time should be larger than the WAIT_TIME in common.h
+Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -D=3 \
+  -sim_length=60e6 $@
+
+for PROCESS_ID in $PROCESS_IDS; do
+  wait $PROCESS_ID || let "EXIT_CODE=$?"
+done
+
+exit $EXIT_CODE #the last exit code != 0

jostapei99/cs736-zephyr

tests/bsim/bluetooth/audio/src/bap_broadcast_assistant_test.c
@@ -24,6 +24,10 @@ static volatile bool g_pa_synced;
 static volatile bool g_state_synced;
 static volatile uint8_t g_src_id;
 static volatile uint32_t g_broadcast_id;
+static volatile uint8_t g_recv_state_count;
+CREATE_FLAG(flag_recv_state_read);
+CREATE_FLAG(flag_recv_state_updated);
+CREATE_FLAG(flag_recv_state_removed);
 
 static volatile bool g_cb;
 
@@ -52,6 +56,7 @@ static void bap_broadcast_assistant_discover_cb(struct bt_conn *conn, int err,
 	}
 
 	printk("BASS discover done with %u recv states\n", recv_state_count);
+	g_recv_state_count = recv_state_count;
 	g_discovery_complete = true;
 }
 
@@ -96,6 +101,13 @@ static void bap_broadcast_assistant_recv_state_cb(
 		return;
 	}
 
+	SET_FLAG(flag_recv_state_read);
+
+	if (state == NULL) {
+		/* Empty receive state */
+		return;
+	}
+
 	bt_addr_le_to_str(&state->addr, le_addr, sizeof(le_addr));
 	(void)bin2hex(state->bad_code, BT_AUDIO_BROADCAST_CODE_SIZE, bad_code,
 		      sizeof(bad_code));
@@ -131,6 +143,8 @@ static void bap_broadcast_assistant_recv_state_cb(
 
 	g_src_id = state->src_id;
 	g_cb = true;
+
+	SET_FLAG(flag_recv_state_updated);
 }
 
 static void bap_broadcast_assistant_recv_state_removed_cb(struct bt_conn *conn, int err,
@@ -143,6 +157,8 @@ static void bap_broadcast_assistant_recv_state_removed_cb(struct bt_conn *conn, 
 
 	printk("BASS recv state %u removed\n", src_id);
 	g_cb = true;
+
+	SET_FLAG(flag_recv_state_removed);
 }
 
 static void bap_broadcast_assistant_scan_start_cb(struct bt_conn *conn, int err)
@@ -287,6 +303,25 @@ static void test_bass_discover(void)
 	printk("Discovery complete\n");
 }
 
+static void test_bass_read_receive_states(void)
+{
+	for (uint8_t i = 0U; i < g_recv_state_count; i++) {
+		int err;
+
+		UNSET_FLAG(flag_recv_state_read);
+		err = bt_bap_broadcast_assistant_read_recv_state(default_conn, i);
+		if (err != 0) {
+			FAIL("Failed to read receive state with idx %u: %d\n",
+			     i, err);
+			return;
+		}
+
+		WAIT_FOR_FLAG(flag_recv_state_read);
+	}
+
+	printk("Receive state read complete\n");
+}
+
 static void test_bass_scan_start(void)
 {
 	int err;
@@ -428,7 +463,7 @@ static void test_bass_remove_source(void)
 	printk("Source removed\n");
 }
 
-static void test_main(void)
+static int common_init(void)
 {
 	int err;
 
@@ -436,7 +471,7 @@ static void test_main(void)
 
 	if (err != 0) {
 		FAIL("Bluetooth enable failed (err %d)\n", err);
-		return;
+		return err;
 	}
 
 	bt_gatt_cb_register(&gatt_callbacks);
@@ -447,7 +482,7 @@ static void test_main(void)
 	err = bt_le_scan_start(BT_LE_SCAN_PASSIVE, device_found);
 	if (err != 0) {
 		FAIL("Scanning failed to start (err %d)\n", err);
-		return;
+		return err;
 	}
 
 	printk("Scanning successfully started\n");
@@ -456,6 +491,21 @@ static void test_main(void)
 
 	test_exchange_mtu();
 	test_bass_discover();
+	test_bass_read_receive_states();
+
+	return 0;
+}
+
+static void test_main_client_sync(void)
+{
+	int err;
+
+	err = common_init();
+	if (err != 0) {
+		FAIL("Bluetooth enable failed (err %d)\n", err);
+		return;
+	}
+
 	test_bass_scan_start();
 	test_bass_scan_stop();
 	test_bass_create_pa_sync();
@@ -464,15 +514,64 @@ static void test_main(void)
 	test_bass_broadcast_code();
 	test_bass_remove_source();
 
-	PASS("BAP broadcast assistant Passed\n");
+	PASS("BAP Broadcast Assistant Client Sync Passed\n");
+}
+
+static void test_main_server_sync_client_rem(void)
+{
+	int err;
+
+	err = common_init();
+	if (err != 0) {
+		FAIL("Bluetooth enable failed (err %d)\n", err);
+		return;
+	}
+
+	WAIT_FOR_FLAG(flag_recv_state_updated);
+
+	test_bass_broadcast_code();
+	test_bass_remove_source();
+
+	PASS("BAP Broadcast Assistant Server Sync Passed\n");
+}
+
+static void test_main_server_sync_server_rem(void)
+{
+	int err;
+
+	err = common_init();
+	if (err != 0) {
+		FAIL("Bluetooth enable failed (err %d)\n", err);
+		return;
+	}
+
+	WAIT_FOR_FLAG(flag_recv_state_updated);
+
+	test_bass_broadcast_code();
+
+	WAIT_FOR_FLAG(flag_recv_state_removed);
+
+	PASS("BAP Broadcast Assistant Server Sync Passed\n");
 }
 
 static const struct bst_test_instance test_bass[] = {
 	{
-		.test_id = "bap_broadcast_assistant",
+		.test_id = "bap_broadcast_assistant_client_sync",
 		.test_post_init_f = test_init,
 		.test_tick_f = test_tick,
-		.test_main_f = test_main
+		.test_main_f = test_main_client_sync,
+	},
+	{
+		.test_id = "bap_broadcast_assistant_server_sync_client_rem",
+		.test_post_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = test_main_server_sync_client_rem,
+	},
+	{
+		.test_id = "bap_broadcast_assistant_server_sync_server_rem",
+		.test_post_init_f = test_init,
+		.test_tick_f = test_tick,
+		.test_main_f = test_main_server_sync_server_rem,
 	},
 	BSTEST_END_MARKER
 };

tests/bsim/bluetooth/audio/test_scripts/bass_server_sync_client_rem.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020-2022 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: Apache-2.0
+
+SIMULATION_ID="bass_server_sync_client_rem"
+VERBOSITY_LEVEL=2
+PROCESS_IDS=""; EXIT_CODE=0
+
+function Execute(){
+  if [ ! -f $1 ]; then
+    echo -e "  \e[91m`pwd`/`basename $1` cannot be found (did you forget to\
+ compile it?)\e[39m"
+    exit 1
+  fi
+  timeout 20 $@ & PROCESS_IDS="$PROCESS_IDS $!"
+}
+
+: "${BSIM_OUT_PATH:?BSIM_OUT_PATH must be defined}"
+
+#Give a default value to BOARD if it does not have one yet:
+BOARD="${BOARD:-nrf52_bsim}"
+
+cd ${BSIM_OUT_PATH}/bin
+
+printf "\n\n======== Running BASS Server Sync Client Remove =========\n\n"
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 \
+  -testid=bap_scan_delegator_server_sync_client_rem -rs=24
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 \
+  -testid=bap_broadcast_assistant_server_sync_client_rem -rs=46
+
+  Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=2 -testid=bass_broadcaster -rs=69
+
+# Simulation time should be larger than the WAIT_TIME in common.h
+Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -D=3 \
+  -sim_length=60e6 $@
+
+for PROCESS_ID in $PROCESS_IDS; do
+  wait $PROCESS_ID || let "EXIT_CODE=$?"
+done
+
+exit $EXIT_CODE #the last exit code != 0

tests/bsim/bluetooth/audio/test_scripts/bass_server_sync_server_rem.sh
@@ -1,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020-2022 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: Apache-2.0
+
+SIMULATION_ID="bass_server_sync_server_rem"
+VERBOSITY_LEVEL=2
+PROCESS_IDS=""; EXIT_CODE=0
+
+function Execute(){
+  if [ ! -f $1 ]; then
+    echo -e "  \e[91m`pwd`/`basename $1` cannot be found (did you forget to\
+ compile it?)\e[39m"
+    exit 1
+  fi
+  timeout 20 $@ & PROCESS_IDS="$PROCESS_IDS $!"
+}
+
+: "${BSIM_OUT_PATH:?BSIM_OUT_PATH must be defined}"
+
+#Give a default value to BOARD if it does not have one yet:
+BOARD="${BOARD:-nrf52_bsim}"
+
+cd ${BSIM_OUT_PATH}/bin
+
+printf "\n\n======== Running BASS Server Sync Server Remove =========\n\n"
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 \
+  -testid=bap_scan_delegator_server_sync_server_rem -rs=24
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 \
+  -testid=bap_broadcast_assistant_server_sync_server_rem -rs=46
+
+  Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=2 -testid=bass_broadcaster -rs=69
+
+# Simulation time should be larger than the WAIT_TIME in common.h
+Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -D=3 \
+  -sim_length=60e6 $@
+
+for PROCESS_ID in $PROCESS_IDS; do
+  wait $PROCESS_ID || let "EXIT_CODE=$?"
+done
+
+exit $EXIT_CODE #the last exit code != 0