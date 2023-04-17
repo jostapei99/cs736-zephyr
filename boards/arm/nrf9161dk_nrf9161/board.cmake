eivindj-nordic/zephyr

boards: arm: add support for nRF9161DK
Add board support for nRF9161DK.

Signed-off-by: Eivind Jølsgard <eivind.jolsgard@nordicsemi.no>

boards/arm/nrf9161dk_nrf9161/board.cmake
@@ -1,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_NRF9161DK_NRF9161_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file "${CMAKE_BINARY_DIR}/tfm_merged.hex")
+endif()
+
+board_runner_args(jlink "--device=nRF9161_xxAA" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)

jostapei99/cs736-zephyr

boards/arm/nrf9161dk_nrf9161/board.cmake
@@ -1,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_NRF9161DK_NRF9161_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file "${CMAKE_BINARY_DIR}/tfm_merged.hex")
+endif()
+
+# TODO: change to nRF9161_xxAA when such device is available in JLink
+board_runner_args(jlink "--device=nRF9160_xxAA" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)