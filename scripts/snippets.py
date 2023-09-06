nordicjm/zephyr

twister: Add support for required snippets
Adds support for twister to require using snippets on tests

Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

scripts/snippets.py
@@ -238,6 +238,22 @@ def process_snippets(args: argparse.Namespace) -> Snippets:
 
     return snippets
 
+def process_snippets_inter(requested_snippets, snippet_roots) -> Snippets:
+    '''Process snippet.yml files under each *snippet_root*
+    by recursive search. Return a Snippets object describing
+    the results of the search.
+    '''
+    # This will contain information about all the snippets
+    # we discover in each snippet_root element.
+    snippets = Snippets(requested=requested_snippets)
+
+    # Process each path in snippet_root in order, adjusting
+    # snippets as needed for each one.
+    for root in snippet_roots:
+        process_snippets_in(root, snippets)
+
+    return snippets
+
 def process_snippets_in(root_dir: Path, snippets: Snippets) -> None:
     '''Process snippet.yml files in *root_dir*,
     updating *snippets* as needed.'''

jostapei99/cs736-zephyr

scripts/snippets.py
@@ -238,6 +238,22 @@ def process_snippets(args: argparse.Namespace) -> Snippets:
 
     return snippets
 
+def find_snippets_in_roots(requested_snippets, snippet_roots) -> Snippets:
+    '''Process snippet.yml files under each *snippet_root*
+    by recursive search. Return a Snippets object describing
+    the results of the search.
+    '''
+    # This will contain information about all the snippets
+    # we discover in each snippet_root element.
+    snippets = Snippets(requested=requested_snippets)
+
+    # Process each path in snippet_root in order, adjusting
+    # snippets as needed for each one.
+    for root in snippet_roots:
+        process_snippets_in(root, snippets)
+
+    return snippets
+
 def process_snippets_in(root_dir: Path, snippets: Snippets) -> None:
     '''Process snippet.yml files in *root_dir*,
     updating *snippets* as needed.'''