jostapei99/cs736-zephyr

commit 3de5d6474d820199c59e74dbcffecf8d5947cb9e

Bluetooth: Controller: Remove redudant header file includes
Remove redundant header file includes, introduce radio used
resources header file.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5340.h
@@ -337,56 +337,6 @@
 #endif /* !CONFIG_BT_CTLR_TIFS_HW */
 #endif /* !CONFIG_BT_CTLR_RADIO_ENABLE_FAST */
 
-#if !defined(CONFIG_BT_CTLR_TIFS_HW)
-#if defined(CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER)
-#undef EVENT_TIMER_ID
-#define EVENT_TIMER_ID 0
-#define SW_SWITCH_TIMER EVENT_TIMER
-#define SW_SWITCH_TIMER_EVTS_COMP_BASE 3
-#define SW_SWITCH_TIMER_EVTS_COMP_S2_BASE 5
-#undef HAL_EVENT_TIMER_SAMPLE_CC_OFFSET
-#define HAL_EVENT_TIMER_SAMPLE_CC_OFFSET 2
-#undef HAL_EVENT_TIMER_SAMPLE_TASK
-#define HAL_EVENT_TIMER_SAMPLE_TASK NRF_TIMER_TASK_CAPTURE2
-
-#else /* !CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER */
-#define SW_SWITCH_TIMER NRF_TIMER1
-#define SW_SWITCH_TIMER_EVTS_COMP_BASE 0
-#define SW_SWITCH_TIMER_EVTS_COMP_S2_BASE 2
-
-/* Wrapper for EVENTS_END event generated by Radio peripheral at the very end of the transmission
- * or reception of a PDU on air. In case of regular PDU it is generated when last bit of CRC is
- * received or transmitted.
- *
- * When direction finding is enabled a PDU may include Constant Tone Extension at its end. For PDU
- * including CTE EVENTS_PHYEND event is generated at very end of a PDU, after CTE is received or
- * transmitted. In case there is no CTE in a PDU the EVENTS_PHYEND event is generated in the same
- * instant as EVENTS_END event.
- */
-#undef NRF_RADIO_TXRX_END_EVENT
-#define NRF_RADIO_TXRX_END_EVENT EVENTS_PHYEND
-
-/* Wrapper for RADIO_SHORTS mask connecting EVENTS_PHYEND to EVENTS_DISABLE.
- * This is a mask for SOC that has Direction Finding Extension in a Radio peripheral.
- * It enables shortcut for EVENTS_PHYEND event generated at very end to Radio EVENTS_DISABLE event.
- * In case there is a CTE in a PDU then EVENTS_PHYEND event is generated after the CTE.
- * If there is no CTE, it is generated in the same instant as EVENTS_END.
- */
-#undef NRF_RADIO_SHORTS_PDU_END_DISABLE
-#define NRF_RADIO_SHORTS_PDU_END_DISABLE RADIO_SHORTS_PHYEND_DISABLE_Msk
-
-#if defined(CONFIG_BT_CTLR_DF_PHYEND_OFFSET_COMPENSATION_ENABLE)
-/* Allocate 2 adjacent channels for PHYEND delay compensation. Use the same channels as for
- * PHY CODED S2. The CTEINLINE may not be enabled for PHY CODED so PHYEND event is generated
- * at the same instant as END event. Hence the channels are uesed interchangeably.
- * That saves from use of another timer.
- */
-#define SW_SWITCH_TIMER_EVTS_COMP_PHYEND_DELAY_COMPENSATION_BASE 2
-#endif /* CONFIG_BT_CTLR_DF_PHYEND_OFFSET_COMPENSATION_ENABLE */
-
-#endif /* !CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER */
-#endif /* !CONFIG_BT_CTLR_TIFS_HW */
-
 /* nRF5340 supports +3dBm Tx Power using high voltage request, define +3dBm
  * value for Controller use.
  */

subsys/bluetooth/controller/hal/cntr.h
@@ -4,7 +4,6 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#include <stdint.h>
 
 void cntr_init(void);
 uint32_t cntr_start(void);

subsys/bluetooth/controller/hal/debug.h
@@ -5,7 +5,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
- #include "common/assert.h"
+#include "common/assert.h"
 
 #ifdef CONFIG_BT_CTLR_ASSERT_HANDLER
 void bt_ctlr_assert_handle(char *file, uint32_t line);

subsys/bluetooth/controller/ll_sw/lll_chan.c
@@ -5,6 +5,7 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "hal/ccm.h"
 #include "hal/radio.h"

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/cntr.c
@@ -5,8 +5,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/sys/dlist.h>
-
 #include <hal/nrf_rtc.h>
 
 #include "hal/cntr.h"

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/ecb.c
@@ -7,8 +7,6 @@
 
 #include <string.h>
 
-#include <zephyr/sys/dlist.h>
-
 #include <hal/nrf_ecb.h>
 
 #include "util/mem.h"

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/nrfx_glue/bt_ctlr_used_resources.h
@@ -3,6 +3,8 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
+#include "../radio/radio_nrf5_resources.h"
 #include "../radio/radio_nrf5_fem.h"
 
 #ifdef DPPI_PRESENT

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio.c
@@ -5,13 +5,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/sys/dlist.h>
 #include <zephyr/toolchain.h>
 #include <zephyr/dt-bindings/gpio/gpio.h>
 #include <soc.h>
 
 #include <hal/nrf_rtc.h>
 #include <hal/nrf_timer.h>
+#include <hal/nrf_radio.h>
 #include <hal/nrf_ccm.h>
 #include <hal/nrf_aar.h>
 

commit 1c030d857cf2a0aa10f198378508e6bdd19b6532

Bluetooth: Controller: nRF53: Fix sw switch single timer id regression
Fix regression in sw switch single timer id use for nRF53x
series SoC.

Regression introduced in commit 3de5d6474d82 ("Bluetooth:
Controller: Remove redudant header file includes").

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5340.h
@@ -5,6 +5,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+/* Override EVENT_TIMER_ID from 4 to 0, as nRF5340 does not have 4 timer
+ * instances.
+ */
+#if defined(CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER)
+#undef EVENT_TIMER_ID
+#define EVENT_TIMER_ID 0
+
+#undef EVENT_TIMER
+#define EVENT_TIMER    _CONCAT(NRF_TIMER, EVENT_TIMER_ID)
+
+#undef SW_SWITCH_TIMER
+#define SW_SWITCH_TIMER EVENT_TIMER
+#endif /* CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER */
+
 /* NRF Radio HW timing constants
  * - provided in US and NS (for higher granularity)
  * - based on empirical measurements and sniffer logs

tests/bluetooth/init/testcase.yaml
@@ -122,6 +122,19 @@ tests:
     integration_platforms:
       - nrf52840dk_nrf52840
       - nrf52dk_nrf52832
+  bluetooth.init.test_ctlr_sw_switch_single_timer:
+    extra_args:
+      - CONF_FILE=prj_ctlr.conf
+      - CONFIG_BT_CTLR_ADVANCED_FEATURES=y
+      - CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER=y
+    platform_allow:
+      - nrf5340dk_nrf5340_cpunet
+      - nrf52840dk_nrf52840
+      - nrf52dk_nrf52832
+    integration_platforms:
+      - nrf5340dk_nrf5340_cpunet
+      - nrf52840dk_nrf52840
+      - nrf52dk_nrf52832
   bluetooth.init.test_ctlr_ticker:
     extra_args:
       - CONF_FILE=prj_ctlr_ticker.conf