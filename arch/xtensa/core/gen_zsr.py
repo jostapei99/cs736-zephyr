ceolin/zephyr

xtensa: mmu: allocate scratch registers for MMU
When MMU is enabled, we need some scratch registers to preload
page table entries. So update gen_zsr.py to that.

Signed-off-by: Daniel Leung <daniel.leung@intel.com>
Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

arch/xtensa/core/gen_zsr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2022 Intel corporation
 # SPDX-License-Identifier: Apache-2.0
-import sys
+import argparse
 import re
 
 # Scratch register allocator.  Zephyr uses multiple Xtensa SRs as
@@ -11,10 +11,26 @@ import re
 # -dM") core-isa.h file for the current architecture and assigns
 # registers to usages.
 
-NEEDED = ("ALLOCA", "CPU", "FLUSH")
+def parse_args():
+    parser = argparse.ArgumentParser(allow_abbrev=False)
 
-coreisa = sys.argv[1]
-outfile = sys.argv[2]
+    parser.add_argument("--mmu", action="store_true",
+                        help="Enable scratch registers for MMU usage")
+    parser.add_argument("coreisa",
+                        help="Path to preprocessed core-isa.h")
+    parser.add_argument("outfile",
+                        help="Output file")
+
+    return parser.parse_args()
+
+args = parse_args()
+
+NEEDED = ["ALLOCA", "CPU", "FLUSH"]
+if args.mmu:
+    NEEDED += ["MMU_0", "MMU_1", "DBLEXC"]
+
+coreisa = args.coreisa
+outfile = args.outfile
 
 syms = {}
 

jostapei99/cs736-zephyr

arch/xtensa/core/gen_zsr.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2022 Intel corporation
 # SPDX-License-Identifier: Apache-2.0
-import sys
+import argparse
 import re
 
 # Scratch register allocator.  Zephyr uses multiple Xtensa SRs as
@@ -11,10 +11,26 @@ import re
 # -dM") core-isa.h file for the current architecture and assigns
 # registers to usages.
 
-NEEDED = ("A0SAVE", "CPU", "FLUSH")
+def parse_args():
+    parser = argparse.ArgumentParser(allow_abbrev=False)
 
-coreisa = sys.argv[1]
-outfile = sys.argv[2]
+    parser.add_argument("--mmu", action="store_true",
+                        help="Enable scratch registers for MMU usage")
+    parser.add_argument("coreisa",
+                        help="Path to preprocessed core-isa.h")
+    parser.add_argument("outfile",
+                        help="Output file")
+
+    return parser.parse_args()
+
+args = parse_args()
+
+NEEDED = ["A0SAVE", "CPU", "FLUSH"]
+if args.mmu:
+    NEEDED += ["MMU_0", "MMU_1", "DBLEXC"]
+
+coreisa = args.coreisa
+outfile = args.outfile
 
 syms = {}
 