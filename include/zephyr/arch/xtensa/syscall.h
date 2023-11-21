ceolin/zephyr

xtensa: userspace: Supports tls on userspace
Use thread local storage to check whether or not a thread is running
in user mode. This allows to use threadptr to properly support tls.

Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

include/zephyr/arch/xtensa/syscall.h
@@ -207,8 +207,17 @@ static inline bool arch_is_user_context(void)
 		"rur.THREADPTR %0\n\t"
 		: "=a" (thread)
 	);
+#ifdef CONFIG_THREAD_LOCAL_STORAGE
+	extern __thread uint8_t is_user_mode;
 
+	if (!thread) {
+		return false;
+	}
+
+	return is_user_mode != 0;
+#else
 	return !!thread;
+#endif
 }
 
 #undef SYSINL

jostapei99/cs736-zephyr

include/zephyr/arch/xtensa/syscall.h
@@ -207,8 +207,17 @@ static inline bool arch_is_user_context(void)
 		"rur.THREADPTR %0\n\t"
 		: "=a" (thread)
 	);
+#ifdef CONFIG_THREAD_LOCAL_STORAGE
+	extern __thread uint32_t is_user_mode;
 
+	if (!thread) {
+		return false;
+	}
+
+	return is_user_mode != 0;
+#else
 	return !!thread;
+#endif
 }
 
 #undef SYSINL