jostapei99/cs736-zephyr

commit e27cd5c5d3e6993ca63da0a13646c19d4a611b85

modem: modem_cellular: CID 338115
incorrect size passed to strncpy()

Signed-off-by: Bjarki Arge Andreasen <bjarki@arge-andreasen.me>

drivers/modem/modem_cellular.c
@@ -313,7 +313,7 @@ static void modem_cellular_chat_on_cgmm(struct modem_chat *chat, char **argv, ui
 		return;
 	}
 
-	strncpy(data->model_id, argv[1], sizeof(data->model_id));
+	strncpy(data->model_id, argv[1], sizeof(data->model_id) - 1);
 }
 
 static void modem_cellular_chat_on_cgmi(struct modem_chat *chat, char **argv, uint16_t argc,

commit 52df2fa97a489e66a8d0564da84b42c587d5ce19

modem: modem_cellular: increase model_id size
Fixes an issue introduced with `e27cd5c5`

Signed-off-by: Lucas Denefle <lucas.denefle@converge.io>

drivers/modem/modem_cellular.c
@@ -27,7 +27,7 @@ LOG_MODULE_REGISTER(modem_cellular, CONFIG_MODEM_LOG_LEVEL);
 	K_MSEC(CONFIG_MODEM_CELLULAR_PERIODIC_SCRIPT_MS)
 
 #define MODEM_CELLULAR_DATA_IMEI_LEN         (15)
-#define MODEM_CELLULAR_DATA_MODEL_ID_LEN     (64)
+#define MODEM_CELLULAR_DATA_MODEL_ID_LEN     (65)
 #define MODEM_CELLULAR_DATA_IMSI_LEN         (22)
 #define MODEM_CELLULAR_DATA_ICCID_LEN        (22)
 #define MODEM_CELLULAR_DATA_MANUFACTURER_LEN (65)