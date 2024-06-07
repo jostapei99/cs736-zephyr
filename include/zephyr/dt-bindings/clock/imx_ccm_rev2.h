zephyrproject-rtos/zephyr

drivers: clock_control: mcux_ccm_rev2: Add ENET_1G clock
Add ENET_1G clock value to the RT11XX CCM version.
Implemented enabling ENET_1G clock and getting its frequency.

Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

include/zephyr/dt-bindings/clock/imx_ccm_rev2.h
@@ -91,6 +91,8 @@
 /* ENET */
 #define IMX_CCM_ENET_CLK	0x3000UL
 #define IMX_CCM_ENET_PLL	0x3001UL
+#define IMX_CCM_ENET1G_CLK	0x3002UL
+#define IMX_CCM_ENET1G_PLL	0x3003UL
 
 /* FLEXSPI */
 #define IMX_CCM_FLEXSPI_CLK	0x4000UL

jostapei99/cs736-zephyr

include/zephyr/dt-bindings/clock/imx_ccm_rev2.h
@@ -92,6 +92,8 @@
 /* ENET */
 #define IMX_CCM_ENET_CLK               0x1200UL
 #define IMX_CCM_ENET_PLL               0x1201UL
+#define IMX_CCM_ENET1G_CLK             0x1202UL
+#define IMX_CCM_ENET1G_PLL             0x1203UL
 
 /* FLEXSPI */
 #define IMX_CCM_FLEXSPI_CLK            0x1300UL