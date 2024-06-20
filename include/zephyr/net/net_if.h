kz-space/zephyr

net: wifi: split wifi interface into STA and uAP
Split wifi interface into station mode and soft-AP mode, as there may be
station and soft-AP two interfaces that work concurrently.

Signed-off-by: Maochen Wang <maochen.wang@nxp.com>

include/zephyr/net/net_if.h
@@ -3014,6 +3014,20 @@ bool net_if_is_wifi(struct net_if *iface);
 struct net_if *net_if_get_first_wifi(void);
 
 /**
+ * @brief Get Wi-Fi network station interface.
+ *
+ * @return Pointer to network interface, NULL if not found.
+ */
+struct net_if *net_if_get_wifi_sta(void);
+
+/**
+ * @brief Get first Wi-Fi network Soft-AP interface.
+ *
+ * @return Pointer to network interface, NULL if not found.
+ */
+struct net_if *net_if_get_wifi_uap(void);
+
+/**
  * @brief Get network interface name.
  *
  * @details If interface name support is not enabled, empty string is returned.

subsys/net/ip/net_if.c
@@ -22,6 +22,9 @@ LOG_MODULE_REGISTER(net_if, CONFIG_NET_IF_LOG_LEVEL);
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/ethernet.h>
+#ifdef CONFIG_WIFI_NM
+#include <zephyr/net/wifi_nm.h>
+#endif
 #include <zephyr/net/offloaded_netdev.h>
 #include <zephyr/net/virtual.h>
 #include <zephyr/net/socket.h>
@@ -5562,6 +5565,40 @@ struct net_if *net_if_get_first_wifi(void)
 {
 	STRUCT_SECTION_FOREACH(net_if, iface) {
 		if (net_if_is_wifi(iface)) {
+			return iface;
+		}
+	}
+	return NULL;
+}
+
+struct net_if *net_if_get_wifi_sta(void)
+{
+	struct ethernet_context *eth_ctx = NULL;
+
+	STRUCT_SECTION_FOREACH(net_if, iface) {
+		eth_ctx = net_if_l2_data(iface);
+		if (net_if_is_wifi(iface)
+#ifdef CONFIG_WIFI_NM
+			&& (wifi_nm_get_type_iface(iface) == (1 << WIFI_TYPE_STA))
+#endif
+			) {
+			return iface;
+		}
+	}
+	return NULL;
+}
+
+struct net_if *net_if_get_wifi_uap(void)
+{
+	struct ethernet_context *eth_ctx = NULL;
+
+	STRUCT_SECTION_FOREACH(net_if, iface) {
+		eth_ctx = net_if_l2_data(iface);
+		if (net_if_is_wifi(iface)
+#ifdef CONFIG_WIFI_NM
+			&& (wifi_nm_get_type_iface(iface) == (1 << WIFI_TYPE_SAP))
+#endif
+			) {
 			return iface;
 		}
 	}

subsys/net/l2/wifi/wifi_shell.c
@@ -590,7 +590,7 @@ static int __wifi_args_to_params(const struct shell *sh, size_t argc, char *argv
 static int cmd_wifi_connect(const struct shell *sh, size_t argc,
 			    char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_sta();
 	struct wifi_connect_req_params cnx_params = { 0 };
 	int ret;
 
@@ -617,7 +617,7 @@ static int cmd_wifi_connect(const struct shell *sh, size_t argc,
 static int cmd_wifi_disconnect(const struct shell *sh, size_t argc,
 			       char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_sta();
 	int status;
 
 	context.disconnecting = true;
@@ -1246,7 +1246,7 @@ static int cmd_wifi_twt_teardown_all(const struct shell *sh, size_t argc,
 static int cmd_wifi_ap_enable(const struct shell *sh, size_t argc,
 			      char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_uap();
 	static struct wifi_connect_req_params cnx_params;
 	int ret;
 
@@ -1273,7 +1273,7 @@ static int cmd_wifi_ap_enable(const struct shell *sh, size_t argc,
 static int cmd_wifi_ap_disable(const struct shell *sh, size_t argc,
 			       char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_uap();
 	int ret;
 
 	ret = net_mgmt(NET_REQUEST_WIFI_AP_DISABLE, iface, NULL, 0);

jostapei99/cs736-zephyr

include/zephyr/net/net_if.h
@@ -3064,6 +3064,20 @@ bool net_if_is_wifi(struct net_if *iface);
 struct net_if *net_if_get_first_wifi(void);
 
 /**
+ * @brief Get Wi-Fi network station interface.
+ *
+ * @return Pointer to network interface, NULL if not found.
+ */
+struct net_if *net_if_get_wifi_sta(void);
+
+/**
+ * @brief Get first Wi-Fi network Soft-AP interface.
+ *
+ * @return Pointer to network interface, NULL if not found.
+ */
+struct net_if *net_if_get_wifi_sap(void);
+
+/**
  * @brief Get network interface name.
  *
  * @details If interface name support is not enabled, empty string is returned.

subsys/net/ip/net_if.c
@@ -23,6 +23,9 @@ LOG_MODULE_REGISTER(net_if, CONFIG_NET_IF_LOG_LEVEL);
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/ethernet.h>
+#ifdef CONFIG_WIFI_NM
+#include <zephyr/net/wifi_nm.h>
+#endif
 #include <zephyr/net/offloaded_netdev.h>
 #include <zephyr/net/virtual.h>
 #include <zephyr/net/socket.h>
@@ -5675,6 +5678,40 @@ struct net_if *net_if_get_first_wifi(void)
 {
 	STRUCT_SECTION_FOREACH(net_if, iface) {
 		if (net_if_is_wifi(iface)) {
+			return iface;
+		}
+	}
+	return NULL;
+}
+
+struct net_if *net_if_get_wifi_sta(void)
+{
+	struct ethernet_context *eth_ctx = NULL;
+
+	STRUCT_SECTION_FOREACH(net_if, iface) {
+		eth_ctx = net_if_l2_data(iface);
+		if (net_if_is_wifi(iface)
+#ifdef CONFIG_WIFI_NM
+			&& (wifi_nm_get_type_iface(iface) == (1 << WIFI_TYPE_STA))
+#endif
+			) {
+			return iface;
+		}
+	}
+	return NULL;
+}
+
+struct net_if *net_if_get_wifi_sap(void)
+{
+	struct ethernet_context *eth_ctx = NULL;
+
+	STRUCT_SECTION_FOREACH(net_if, iface) {
+		eth_ctx = net_if_l2_data(iface);
+		if (net_if_is_wifi(iface)
+#ifdef CONFIG_WIFI_NM
+			&& (wifi_nm_get_type_iface(iface) == (1 << WIFI_TYPE_SAP))
+#endif
+			) {
 			return iface;
 		}
 	}

subsys/net/l2/wifi/wifi_shell.c
@@ -607,7 +607,7 @@ static int __wifi_args_to_params(const struct shell *sh, size_t argc, char *argv
 static int cmd_wifi_connect(const struct shell *sh, size_t argc,
 			    char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_sta();
 	struct wifi_connect_req_params cnx_params = { 0 };
 	int ret;
 
@@ -634,7 +634,7 @@ static int cmd_wifi_connect(const struct shell *sh, size_t argc,
 static int cmd_wifi_disconnect(const struct shell *sh, size_t argc,
 			       char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_sta();
 	int status;
 
 	context.disconnecting = true;
@@ -1263,7 +1263,7 @@ static int cmd_wifi_twt_teardown_all(const struct shell *sh, size_t argc,
 static int cmd_wifi_ap_enable(const struct shell *sh, size_t argc,
 			      char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_sap();
 	static struct wifi_connect_req_params cnx_params;
 	int ret;
 
@@ -1290,7 +1290,7 @@ static int cmd_wifi_ap_enable(const struct shell *sh, size_t argc,
 static int cmd_wifi_ap_disable(const struct shell *sh, size_t argc,
 			       char *argv[])
 {
-	struct net_if *iface = net_if_get_first_wifi();
+	struct net_if *iface = net_if_get_wifi_sap();
 	int ret;
 
 	ret = net_mgmt(NET_REQUEST_WIFI_AP_DISABLE, iface, NULL, 0);