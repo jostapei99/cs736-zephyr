jostapei99/cs736-zephyr

commit 3675f2cbac1dc6d5984c2bc3bd82b339cda962f9

drivers: esp_at: implement bind() and recvfrom() for UDP sockets
Implement bind() and recvfrom() for UDP sockets. This is achived by
setting remote field in net_pkt which in return makes recvfrom() fill
in *src_addr. This is only implemented for passiv mode and CIPDINFO needs
to be enabled. Also set net_if to non-dormant when enabling AP mode to
make binding to a port and address possible.

Signed-off-by: John Johnson <john.filip.johnson@gmail.com>

drivers/wifi/esp_at/esp_offload.c
@@ -20,16 +20,6 @@ LOG_MODULE_REGISTER(wifi_esp_at_offload, CONFIG_WIFI_LOG_LEVEL);
 
 #include "esp.h"
 
-static int esp_bind(struct net_context *context, const struct sockaddr *addr,
-		    socklen_t addrlen)
-{
-	if (IS_ENABLED(CONFIG_NET_IPV4) && addr->sa_family == AF_INET) {
-		return 0;
-	}
-
-	return -EAFNOSUPPORT;
-}
-
 static int esp_listen(struct net_context *context, int backlog)
 {
 	return -ENOTSUP;
@@ -43,7 +33,7 @@ static int _sock_connect(struct esp_data *dev, struct esp_socket *sock)
 	struct sockaddr dst;
 	int ret;
 
-	if (!esp_flags_are_set(dev, EDF_STA_CONNECTED)) {
+	if (!esp_flags_are_set(dev, EDF_STA_CONNECTED | EDF_AP_ENABLED)) {
 		return -ENETUNREACH;
 	}
 
@@ -62,9 +52,9 @@ static int _sock_connect(struct esp_data *dev, struct esp_socket *sock)
 			 ntohs(net_sin(&dst)->sin_port));
 	} else {
 		snprintk(connect_msg, sizeof(connect_msg),
-			 "AT+CIPSTART=%d,\"UDP\",\"%s\",%d",
+			 "AT+CIPSTART=%d,\"UDP\",\"%s\",%d,%d",
 			 sock->link_id, addr_str,
-			 ntohs(net_sin(&dst)->sin_port));
+			 ntohs(net_sin(&dst)->sin_port), ntohs(net_sin(&dst)->sin_port));
 	}
 
 	LOG_DBG("link %d, ip_proto %s, addr %s", sock->link_id,
@@ -104,6 +94,40 @@ void esp_connect_work(struct k_work *work)
 		sock->connect_cb(sock->context, ret, sock->conn_user_data);
 	}
 	k_mutex_unlock(&sock->lock);
+}
+
+static int esp_bind(struct net_context *context, const struct sockaddr *addr,
+		    socklen_t addrlen)
+{
+	struct esp_socket *sock;
+	struct esp_data *dev;
+
+	sock = (struct esp_socket *)context->offload_context;
+	dev = esp_socket_to_dev(sock);
+
+	if (esp_socket_ip_proto(sock) == IPPROTO_TCP) {
+		return 0;
+	}
+
+	if (IS_ENABLED(CONFIG_NET_IPV4) && addr->sa_family == AF_INET) {
+		LOG_DBG("link %d", sock->link_id);
+
+		if (esp_socket_connected(sock)) {
+			return -EISCONN;
+		}
+
+		k_mutex_lock(&sock->lock, K_FOREVER);
+		sock->dst = *addr;
+		sock->connect_cb = NULL;
+		sock->conn_user_data = NULL;
+		k_mutex_unlock(&sock->lock);
+
+		_sock_connect(dev, sock);
+
+		return 0;
+	}
+
+	return -EAFNOSUPPORT;
 }
 
 static int esp_connect(struct net_context *context,
@@ -204,7 +228,7 @@ static int _sock_send(struct esp_socket *sock, struct net_pkt *pkt)
 	};
 	struct sockaddr dst;
 
-	if (!esp_flags_are_set(dev, EDF_STA_CONNECTED)) {
+	if (!esp_flags_are_set(dev, EDF_STA_CONNECTED | EDF_AP_ENABLED)) {
 		return -ENETUNREACH;
 	}
 
@@ -360,7 +384,7 @@ static int esp_sendto(struct net_pkt *pkt,
 
 	LOG_DBG("link %d, timeout %d", sock->link_id, timeout);
 
-	if (!esp_flags_are_set(dev, EDF_STA_CONNECTED)) {
+	if (!esp_flags_are_set(dev, EDF_STA_CONNECTED | EDF_AP_ENABLED)) {
 		return -ENETUNREACH;
 	}
 
@@ -386,11 +410,8 @@ static int esp_sendto(struct net_pkt *pkt,
 			if (ret < 0) {
 				return ret;
 			}
-		} else if (dst_addr && memcmp(dst_addr, &sock->dst, addrlen)) {
-			/* This might be unexpected behaviour but the ESP
-			 * doesn't support changing endpoint.
-			 */
-			return -EISCONN;
+		} else if (esp_socket_type(sock) == SOCK_DGRAM) {
+			memcpy(&sock->dst, dst_addr, addrlen);
 		}
 	}
 
@@ -406,11 +427,17 @@ static int esp_send(struct net_pkt *pkt,
 }
 
 #define CIPRECVDATA_CMD_MIN_LEN (sizeof("+CIPRECVDATA,L:") - 1)
+
+#if defined(CONFIG_WIFI_ESP_AT_CIPDINFO_USE)
+#define CIPRECVDATA_CMD_MAX_LEN (sizeof("+CIPRECVDATA,LLLL,\"255.255.255.255\",65535:") - 1)
+#else
 #define CIPRECVDATA_CMD_MAX_LEN (sizeof("+CIPRECVDATA,LLLL:") - 1)
+#endif
 
 static int cmd_ciprecvdata_parse(struct esp_socket *sock,
 				 struct net_buf *buf, uint16_t len,
-				 int *data_offset, int *data_len)
+				 int *data_offset, int *data_len, char *ip_str,
+				 int *port)
 {
 	char cmd_buf[CIPRECVDATA_CMD_MAX_LEN + 1];
 	char *endptr;
@@ -427,6 +454,23 @@ static int cmd_ciprecvdata_parse(struct esp_socket *sock,
 	cmd_buf[match_len] = 0;
 
 	*data_len = strtol(&cmd_buf[len], &endptr, 10);
+
+#if defined(CONFIG_WIFI_ESP_AT_CIPDINFO_USE)
+	char *strstart = endptr + 1;
+	char *strend = strchr(strstart, ',');
+
+	if (strstart == NULL || strend == NULL) {
+		return -EAGAIN;
+	}
+
+	memcpy(ip_str, strstart, strend - strstart);
+	ip_str[strend - strstart] = '\0';
+	*port = strtol(strend + 1, &endptr, 10);
+#else
+	ARG_UNUSED(ip_str);
+	ARG_UNUSED(port);
+#endif
+
 	if (endptr == &cmd_buf[len] ||
 	    (*endptr == 0 && match_len >= CIPRECVDATA_CMD_MAX_LEN) ||
 	    *data_len > CIPRECVDATA_MAX_LEN) {
@@ -461,8 +505,16 @@ MODEM_CMD_DIRECT_DEFINE(on_cmd_ciprecvdata)
 	int data_offset, data_len;
 	int err;
 
+#if defined(CONFIG_WIFI_ESP_AT_CIPDINFO_USE)
+	char raw_remote_ip[INET_ADDRSTRLEN + 3] = {0};
+	int port = 0;
+
 	err = cmd_ciprecvdata_parse(sock, data->rx_buf, len, &data_offset,
-				    &data_len);
+				    &data_len, raw_remote_ip, &port);
+#else
+	err = cmd_ciprecvdata_parse(sock, data->rx_buf, len, &data_offset,
+				    &data_len, NULL, NULL);
+#endif
 	if (err) {
 		if (err == -EAGAIN) {
 			return -EAGAIN;
@@ -471,6 +523,31 @@ MODEM_CMD_DIRECT_DEFINE(on_cmd_ciprecvdata)
 		return err;
 	}
 
+#if defined(CONFIG_WIFI_ESP_AT_CIPDINFO_USE)
+	struct sockaddr_in *recv_addr =
+			(struct sockaddr_in *) &sock->context->remote;
+
+	recv_addr->sin_port = ntohs(port);
+	recv_addr->sin_family = AF_INET;
+
+	/* IP addr comes within quotation marks, which is disliked by
+	 * conv function. So we remove them by subtraction 2 from
+	 * raw_remote_ip length and index from &raw_remote_ip[1].
+	 */
+	char remote_ip_addr[INET_ADDRSTRLEN];
+	size_t remote_ip_str_len;
+
+	remote_ip_str_len = MIN(sizeof(remote_ip_addr) - 1,
+				strlen(raw_remote_ip) - 2);
+	strncpy(remote_ip_addr, &raw_remote_ip[1], remote_ip_str_len);
+	remote_ip_addr[remote_ip_str_len] = '\0';
+
+	if (net_addr_pton(AF_INET, remote_ip_addr, &recv_addr->sin_addr) < 0) {
+		LOG_ERR("Invalid src addr %s", remote_ip_addr);
+		err = -EIO;
+		return err;
+	}
+#endif
 	esp_socket_rx(sock, data->rx_buf, data_offset, data_len);
 
 	return data_offset + data_len;

drivers/wifi/esp_at/esp_socket.c
@@ -140,6 +140,11 @@ static struct net_pkt *esp_socket_prepare_pkt(struct esp_socket *sock,
 	net_pkt_set_context(pkt, sock->context);
 	net_pkt_cursor_init(pkt);
 
+#if defined(CONFIG_WIFI_ESP_AT_CIPDINFO_USE)
+	memcpy(&pkt->remote, &sock->context->remote, sizeof(pkt->remote));
+	pkt->family = sock->dst.sa_family;
+#endif
+
 	return pkt;
 }
 

drivers/wifi/esp_at/Kconfig.esp_at
@@ -161,6 +161,11 @@ config WIFI_ESP_AT_DNS_USE
 	  Fetch DNS servers from ESP chip with AT+CIPDNS? command and apply that
 	  list to system DNS resolver.
 
+config WIFI_ESP_AT_CIPDINFO_USE
+	bool "Use CIPDINFO to get peer ip and port"
+	help
+	  Enable AT+CIPDINFO got get peer ip-address and port.
+
 config WIFI_ESP_AT_FETCH_VERSION
 	bool "Fetch and log ESP-AT firmware version"
 	default y

drivers/wifi/esp_at/esp.c
@@ -451,7 +451,9 @@ static void esp_mgmt_disconnect_work(struct k_work *work)
 #if defined(CONFIG_NET_NATIVE_IPV4)
 	net_if_ipv4_addr_rm(dev->net_iface, &dev->ip);
 #endif
-	net_if_dormant_on(dev->net_iface);
+	if (!esp_flags_are_set(dev, EDF_AP_ENABLED)) {
+		net_if_dormant_on(dev->net_iface);
+	}
 	wifi_mgmt_raise_disconnect_result_event(dev->net_iface, 0);
 }
 
@@ -1074,12 +1076,18 @@ static int esp_mgmt_ap_enable(const struct device *dev,
 
 	ret = esp_cmd_send(data, NULL, 0, cmd, ESP_CMD_TIMEOUT);
 
+	net_if_dormant_off(data->net_iface);
+
 	return ret;
 }
 
 static int esp_mgmt_ap_disable(const struct device *dev)
 {
 	struct esp_data *data = dev->data;
+
+	if (!esp_flags_are_set(data, EDF_STA_CONNECTED)) {
+		net_if_dormant_on(data->net_iface);
+	}
 
 	return esp_mode_flags_clear(data, EDF_AP_ENABLED);
 }
@@ -1127,6 +1135,9 @@ static void esp_init_work(struct k_work *work)
 #endif
 #if defined(CONFIG_WIFI_ESP_AT_PASSIVE_MODE)
 		SETUP_CMD_NOHANDLE("AT+CIPRECVMODE=1"),
+#endif
+#if defined(CONFIG_WIFI_ESP_AT_CIPDINFO_USE)
+		SETUP_CMD_NOHANDLE("AT+CIPDINFO=1"),
 #endif
 		SETUP_CMD("AT+"_CIPSTAMAC"?", "+"_CIPSTAMAC":",
 			  on_cmd_cipstamac, 1U, ""),

commit 2ddc100f6d41e2180a499c9b0cc702309b06aa59

drivers: wifi: esp_at: fix UDP socket setup
Right now AT+CIPSTART command is called with both "remote port" and "local
port" being set to the same number. This means that for outgoing UDP
traffic (e.g. when resolving DNS or when reaching out some application
server with CoAP over UDP) always the same outgoing port is used. Such
behavior is wrong, since by default a random outgoing port should be used.

Reusing the same port confuses server implementation that is reached out,
since especially in context of DTLS over UDP, outgoing port defines TLS
context/session to be used. Such servers often ignore TLS packets from new
sessions (e.g. after device reboot) and result in failed DTLS connection
attempts.

Commit 3675f2cbac1d ("drivers: esp_at: implement bind() and recvfrom() for
UDP sockets") added support for "server-side listening" for incoming
traffic on UDP sockets, which introduced broken behavior of using the same
remote and local port.

In esp_bind() implementation assign newly intorduced 'src' member, instead
of reusing 'dst'. Don't call AT+CIPSTART yet at this stage, as in case of
connect() Zephyr syscall esp_bind() (via bind_default() helper function) is
called implicitly to assign random generated local port, so remote port is
yet to be assigned. Check in esp_recv() whether socket was already
connected (i.e. esp_connect() was called) and if not, invoke
AT+CIPSTART (via _sock_connect()) to start listening on "server-side" UDP
socket.

This patch fixes broken behavior of always reusing the same local port for
outgoing UDP traffic. Instead, randomly generated (by Zephyr net_context
subsys) local port is used. Additionally bind() and recvfrom()
implementation (to handle server-side UDP sockets) is improved, so that
binding to 0.0.0.0 (on any interface) is possible.

Fixes: 3675f2cbac1d ("drivers: esp_at: implement bind() and recvfrom() for
  UDP sockets")
Fixes: a7edf0b251d7 ("drivers: wifi: esp_at: do not connect socket on
  bind(INADDR_ANY)")

drivers/wifi/esp_at/esp.h
@@ -175,6 +175,7 @@ struct esp_socket {
 	atomic_t flags;
 
 	/* socket info */
+	struct sockaddr src;
 	struct sockaddr dst;
 
 	/* sem */

drivers/wifi/esp_at/esp_offload.c
@@ -27,9 +27,14 @@ static int esp_listen(struct net_context *context, int backlog)
 
 static int _sock_connect(struct esp_data *dev, struct esp_socket *sock)
 {
-	char connect_msg[sizeof("AT+CIPSTART=000,\"TCP\",\"\",65535,7200") +
-			 NET_IPV4_ADDR_LEN];
-	char addr_str[NET_IPV4_ADDR_LEN];
+	/* Calculate the largest possible AT command length based on both TCP and UDP variants. */
+	char connect_msg[MAX(sizeof("AT+CIPSTART=000,\"TCP\",\"\",65535,7200") +
+				NET_IPV4_ADDR_LEN,
+			     sizeof("AT+CIPSTART=000,\"UDP\",\"\",65535,65535,0,\"\"") +
+				2 * NET_IPV4_ADDR_LEN)];
+	char dst_addr_str[NET_IPV4_ADDR_LEN];
+	char src_addr_str[NET_IPV4_ADDR_LEN];
+	struct sockaddr src;
 	struct sockaddr dst;
 	int ret;
 
@@ -38,28 +43,44 @@ static int _sock_connect(struct esp_data *dev, struct esp_socket *sock)
 	}
 
 	k_mutex_lock(&sock->lock, K_FOREVER);
+	src = sock->src;
 	dst = sock->dst;
 	k_mutex_unlock(&sock->lock);
 
-	net_addr_ntop(dst.sa_family,
-		      &net_sin(&dst)->sin_addr,
-		      addr_str, sizeof(addr_str));
+	if (dst.sa_family == AF_INET) {
+		net_addr_ntop(dst.sa_family,
+			      &net_sin(&dst)->sin_addr,
+			      dst_addr_str, sizeof(dst_addr_str));
+	} else {
+		strcpy(dst_addr_str, "0.0.0.0");
+	}
 
 	if (esp_socket_ip_proto(sock) == IPPROTO_TCP) {
 		snprintk(connect_msg, sizeof(connect_msg),
 			 "AT+CIPSTART=%d,\"TCP\",\"%s\",%d,7200",
-			 sock->link_id, addr_str,
+			 sock->link_id, dst_addr_str,
 			 ntohs(net_sin(&dst)->sin_port));
 	} else {
-		snprintk(connect_msg, sizeof(connect_msg),
-			 "AT+CIPSTART=%d,\"UDP\",\"%s\",%d,%d",
-			 sock->link_id, addr_str,
-			 ntohs(net_sin(&dst)->sin_port), ntohs(net_sin(&dst)->sin_port));
+		if (src.sa_family == AF_INET && net_sin(&src)->sin_port != 0) {
+			net_addr_ntop(src.sa_family,
+				      &net_sin(&src)->sin_addr,
+				      src_addr_str, sizeof(src_addr_str));
+			snprintk(connect_msg, sizeof(connect_msg),
+				 "AT+CIPSTART=%d,\"UDP\",\"%s\",%d,%d,0,\"%s\"",
+				 sock->link_id, dst_addr_str,
+				 ntohs(net_sin(&dst)->sin_port), ntohs(net_sin(&src)->sin_port),
+				 src_addr_str);
+		} else {
+			snprintk(connect_msg, sizeof(connect_msg),
+				 "AT+CIPSTART=%d,\"UDP\",\"%s\",%d",
+				 sock->link_id, dst_addr_str,
+				 ntohs(net_sin(&dst)->sin_port));
+		}
 	}
 
 	LOG_DBG("link %d, ip_proto %s, addr %s", sock->link_id,
 		esp_socket_ip_proto(sock) == IPPROTO_TCP ? "TCP" : "UDP",
-		addr_str);
+		dst_addr_str);
 
 	ret = esp_cmd_send(dev, NULL, 0, connect_msg, ESP_CMD_TIMEOUT);
 	if (ret == 0) {
@@ -110,25 +131,15 @@ static int esp_bind(struct net_context *context, const struct sockaddr *addr,
 	}
 
 	if (IS_ENABLED(CONFIG_NET_IPV4) && addr->sa_family == AF_INET) {
-		struct sockaddr_in *addr4 = (struct sockaddr_in *)addr;
-
 		LOG_DBG("link %d", sock->link_id);
-
-		if (addr4->sin_addr.s_addr == INADDR_ANY) {
-			return 0;
-		}
 
 		if (esp_socket_connected(sock)) {
 			return -EISCONN;
 		}
 
 		k_mutex_lock(&sock->lock, K_FOREVER);
-		sock->dst = *addr;
-		sock->connect_cb = NULL;
-		sock->conn_user_data = NULL;
+		sock->src = *addr;
 		k_mutex_unlock(&sock->lock);
-
-		_sock_connect(dev, sock);
 
 		return 0;
 	}
@@ -616,10 +627,22 @@ static int esp_recv(struct net_context *context,
 		    void *user_data)
 {
 	struct esp_socket *sock = context->offload_context;
+	struct esp_data *dev = esp_socket_to_dev(sock);
 	int ret;
 
 	LOG_DBG("link_id %d, timeout %d, cb %p, data %p",
 		sock->link_id, timeout, cb, user_data);
+
+	/*
+	 * UDP "listening" socket needs to be bound using AT+CIPSTART before any
+	 * traffic can be received.
+	 */
+	if (!esp_socket_connected(sock) &&
+	    esp_socket_ip_proto(sock) == IPPROTO_UDP &&
+	    sock->src.sa_family == AF_INET &&
+	    net_sin(&sock->src)->sin_port != 0) {
+		_sock_connect(dev, sock);
+	}
 
 	k_mutex_lock(&sock->lock, K_FOREVER);
 	sock->recv_cb = cb;

drivers/wifi/esp_at/esp_socket.c
@@ -32,6 +32,8 @@ struct esp_socket *esp_socket_get(struct esp_data *data,
 
 			sock->connect_cb = NULL;
 			sock->recv_cb = NULL;
+			memset(&sock->src, 0x0, sizeof(sock->src));
+			memset(&sock->dst, 0x0, sizeof(sock->dst));
 
 			atomic_inc(&sock->refcount);
 
@@ -142,7 +144,7 @@ static struct net_pkt *esp_socket_prepare_pkt(struct esp_socket *sock,
 
 #if defined(CONFIG_WIFI_ESP_AT_CIPDINFO_USE)
 	memcpy(&pkt->remote, &sock->context->remote, sizeof(pkt->remote));
-	pkt->family = sock->dst.sa_family;
+	pkt->family = sock->src.sa_family;
 #endif
 
 	return pkt;