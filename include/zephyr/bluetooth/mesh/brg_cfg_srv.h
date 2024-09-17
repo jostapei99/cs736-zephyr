zephyrproject-rtos/zephyr

Bluetooth: Mesh: Bridge Configuration Client/Server API
Adding documentation and function calles for the API's
in Bridge Configuration Client model and Bridge
Configuration Server model.

Signed-off-by: Ingar Kulbrandstad <ingar.kulbrandstad@nordicsemi.no>

include/zephyr/bluetooth/mesh/brg_cfg_srv.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** @file
+ *  @brief Bluetooth Mesh Bridge Configuration Server Model APIs.
+ */
+#ifndef BT_MESH_BRG_CFG_SRV_H__
+#define BT_MESH_BRG_CFG_SRV_H__
+
+#include <zephyr/bluetooth/mesh.h>
+#include <zephyr/bluetooth/mesh/brg_cfg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup bt_mesh_brg_cfg_srv Bridge Configuration Server Model
+ * @ingroup bt_mesh
+ * @{
+ *  @brief API for the Bluetooth Mesh Bridge Configuration Server model
+ */
+
+/**
+ *
+ *  @brief Bridge Configuration Server model Composition Data entry.
+ */
+#define BT_MESH_MODEL_BRG_CFG_SRV                                              \
+	BT_MESH_MODEL_CB(BT_MESH_MODEL_ID_BRG_CFG_SRV, bt_mesh_brg_cfg_srv_op,     \
+			 NULL, NULL, &bt_mesh_brg_cfg_srv_cb)
+
+/** @cond INTERNAL_HIDDEN */
+extern const struct bt_mesh_model_op bt_mesh_brg_cfg_srv_op[];
+extern const struct bt_mesh_model_cb bt_mesh_brg_cfg_srv_cb;
+/** @endcond */
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BT_MESH_BRG_CFG_SRV_H__ */

jostapei99/cs736-zephyr

include/zephyr/bluetooth/mesh/brg_cfg_srv.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** @file
+ *  @brief Bluetooth Mesh Bridge Configuration Server Model APIs.
+ */
+#ifndef ZEPHYR_INCLUDE_BLUETOOTH_MESH_BRG_CFG_SRV_H__
+#define ZEPHYR_INCLUDE_BLUETOOTH_MESH_BRG_CFG_SRV_H__
+
+#include <zephyr/bluetooth/mesh/brg_cfg.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @defgroup bt_mesh_brg_cfg_srv Bridge Configuration Server Model
+ * @ingroup bt_mesh
+ * @{
+ *  @brief API for the Bluetooth Mesh Bridge Configuration Server model
+ */
+
+/**
+ *
+ *  @brief Bridge Configuration Server model Composition Data entry.
+ */
+#define BT_MESH_MODEL_BRG_CFG_SRV                                              \
+	BT_MESH_MODEL_CB(BT_MESH_MODEL_ID_BRG_CFG_SRV, bt_mesh_brg_cfg_srv_op,     \
+			 NULL, NULL, &bt_mesh_brg_cfg_srv_cb)
+
+/** @cond INTERNAL_HIDDEN */
+extern const struct bt_mesh_model_op bt_mesh_brg_cfg_srv_op[];
+extern const struct bt_mesh_model_cb bt_mesh_brg_cfg_srv_cb;
+/** @endcond */
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ZEPHYR_INCLUDE_BLUETOOTH_MESH_BRG_CFG_SRV_H__ */