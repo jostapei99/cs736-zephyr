zephyrproject-rtos/zephyr

bluetooth: host: CS support for CS Test
Adds HCI support for the LE CS Test command.

Signed-off-by: Olivier Lesage <olivier.lesage@nordicsemi.no>

include/zephyr/bluetooth/hci_types.h
@@ -2435,6 +2435,152 @@ struct bt_hci_cp_le_read_remote_fae_table {
 	uint16_t handle;
 } __packed;
 
+
+#define BT_HCI_OP_LE_CS_TEST BT_OP(BT_OGF_LE, 0x0095) /* 0x2095 */
+
+#define BT_HCI_OP_LE_CS_TEST_MAIN_MODE_1 0x1
+#define BT_HCI_OP_LE_CS_TEST_MAIN_MODE_2 0x2
+#define BT_HCI_OP_LE_CS_TEST_MAIN_MODE_3 0x3
+
+#define BT_HCI_OP_LE_CS_TEST_SUB_MODE_1 0x1
+#define BT_HCI_OP_LE_CS_TEST_SUB_MODE_2 0x2
+#define BT_HCI_OP_LE_CS_TEST_SUB_MODE_3 0x3
+#define BT_HCI_OP_LE_CS_TEST_SUB_MODE_UNUSED 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_ROLE 0x0
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_ROLE 0x1
+
+#define BT_HCI_OP_LE_CS_TEST_RTT_TYPE_AA_ONLY     0x0
+#define BT_HCI_OP_LE_CS_TEST_RTT_TYPE_32BIT_SOUND 0x1
+#define BT_HCI_OP_LE_CS_TEST_RTT_TYPE_96BIT_SOUND 0x2
+#define BT_HCI_OP_LE_CS_TEST_RTT_TYPE_32BIT_RAND  0x3
+#define BT_HCI_OP_LE_CS_TEST_RTT_TYPE_64BIT_RAND  0x4
+#define BT_HCI_OP_LE_CS_TEST_RTT_TYPE_96BIT_RAND  0x5
+#define BT_HCI_OP_LE_CS_TEST_RTT_TYPE_128BIT_RAND 0x6
+
+#define BT_HCI_OP_LE_CS_TEST_CS_SYNC_1M     0x1
+#define BT_HCI_OP_LE_CS_TEST_CS_SYNC_2M     0x2
+#define BT_HCI_OP_LE_CS_TEST_CS_SYNC_2M_2BT 0x3
+
+#define BT_HCI_OP_LE_CS_TEST_MAXIMIZE_TX_POWER 0x7E
+#define BT_HCI_OP_LE_CS_TEST_MINIMIZE_TX_POWER 0x7F
+
+#define BT_HCI_OP_LE_CS_TEST_ACI_0 0x0
+#define BT_HCI_OP_LE_CS_TEST_ACI_1 0x1
+#define BT_HCI_OP_LE_CS_TEST_ACI_2 0x2
+#define BT_HCI_OP_LE_CS_TEST_ACI_3 0x3
+#define BT_HCI_OP_LE_CS_TEST_ACI_4 0x4
+#define BT_HCI_OP_LE_CS_TEST_ACI_5 0x5
+#define BT_HCI_OP_LE_CS_TEST_ACI_6 0x6
+#define BT_HCI_OP_LE_CS_TEST_ACI_7 0x7
+
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_18 0x0
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_21 0x1
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_24 0x2
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_27 0x3
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_30 0x4
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_NOT_USED 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_18 0x0
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_21 0x1
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_24 0x2
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_27 0x3
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_30 0x4
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_NOT_USED 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_0_MASK BIT(0)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_2_MASK BIT(2)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_3_MASK BIT(3)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_4_MASK BIT(4)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_5_MASK BIT(5)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_6_MASK BIT(6)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_7_MASK BIT(7)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_8_MASK BIT(8)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_10_MASK BIT(10)
+
+#define BT_HCI_OP_LE_CS_TEST_CHSEL_TYPE_3B 0x0
+#define BT_HCI_OP_LE_CS_TEST_CHSEL_TYPE_3C 0x1
+
+#define BT_HCI_OP_LE_CS_TEST_CH3C_SHAPE_HAT 0x0
+#define BT_HCI_OP_LE_CS_TEST_CH3C_SHAPE_X   0x1
+
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_NONE 0x0
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_INIT 0x1
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_REFL 0x2
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_BOTH 0x3
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_REPEAT 0x4
+
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_00 0x0
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_01 0x1
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_02 0x2
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_03 0x3
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_04 0x4
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_05 0x5
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_06 0x6
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_07 0x7
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_08 0x8
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_09 0x9
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_10 0xA
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_11 0xB
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_12 0xC
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_13 0xD
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_14 0xE
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_15 0xF
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_16 0x10
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_17 0x11
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_18 0x12
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_19 0x13
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_20 0x14
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_21 0x15
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_22 0x16
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_23 0x17
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_LOOP 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_2_POSITION_NOT_PRESENT 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_VAL_0011 0x0
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_VAL_1100 0x1
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_VAL_LOOP 0x2
+
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_PRBS9    0x00
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_11110000 0x01
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_10101010 0x02
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_PRBS15   0x03
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_11111111 0x04
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_00000000 0x05
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_00001111 0x06
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_01010101 0x07
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_USER     0x80
+
+struct bt_hci_op_le_cs_test {
+	uint8_t main_mode_type;
+	uint8_t sub_mode_type;
+	uint8_t main_mode_repetition;
+	uint8_t mode_0_steps;
+	uint8_t role;
+	uint8_t rtt_type;
+	uint8_t cs_sync_phy;
+	uint8_t cs_sync_antenna_selection;
+	uint8_t subevent_len[3];
+	uint16_t subevent_interval;
+	uint8_t max_num_subevents;
+	uint8_t transmit_power_level;
+	uint8_t t_ip1_time;
+	uint8_t t_ip2_time;
+	uint8_t t_fcs_time;
+	uint8_t t_pm_time;
+	uint8_t t_sw_time;
+	uint8_t tone_antenna_config_selection;
+	uint8_t reserved;
+	uint8_t snr_control_initiator;
+	uint8_t snr_control_reflector;
+	uint16_t drbg_nonce;
+	uint8_t channel_map_repetition;
+	uint16_t override_config;
+	uint8_t override_parameters_length;
+	uint8_t override_parameters_data[];
+} __packed;
+
 /* Event definitions */
 
 #define BT_HCI_EVT_UNKNOWN                      0x00

subsys/bluetooth/shell/cs.c
@@ -133,6 +133,68 @@ static int cmd_read_remote_fae_table(const struct shell *sh, size_t argc, char *
 
 	return 0;
 }
+
+static int cmd_cs_test_simple(const struct shell *sh, size_t argc, char *argv[])
+{
+	int err = 0;
+	struct bt_cs_test_param params;
+
+	params.main_mode = BT_CS_TEST_MAIN_MODE_1;
+	params.sub_mode = BT_HCI_OP_LE_CS_TEST_SUB_MODE_UNUSED;
+	params.main_mode_repetition = 0;
+	params.mode_0_steps = 0x1;
+
+	params.role = shell_strtoul(argv[1], 16, &err);
+
+	if (err) {
+		shell_help(sh);
+		shell_error(sh, "Could not parse input 1, Role selection");
+		return SHELL_CMD_HELP_PRINTED;
+	}
+
+	if (params.role != BT_CS_TEST_ROLE_INITIATOR &&
+			params.role != BT_CS_TEST_ROLE_REFLECTOR) {
+		shell_help(sh);
+		shell_error(sh, "Role selection input invalid");
+		return SHELL_CMD_HELP_PRINTED;
+	}
+
+	params.rtt_type = BT_CS_TEST_RTT_AA_ONLY;
+	params.cs_sync_phy = BT_CS_TEST_CS_SYNC_LE_1M_PHY;
+	params.cs_sync_antenna_selection = BT_CS_TEST_CS_SYNC_ANTENNA_SELECTION_OPT_ONE;
+	params.subevent_len = 10000;
+	params.subevent_interval = 0;
+	params.max_num_subevents = 0;
+	params.transmit_power_level = BT_HCI_OP_LE_CS_TEST_MAXIMIZE_TX_POWER;
+	params.t_ip1_time = 80;
+	params.t_ip2_time = 80;
+	params.t_fcs_time = 120;
+	params.t_pm_time = 20;
+	params.t_sw_time = 0;
+	params.tone_antenna_config_selection = BT_CS_TEST_TONE_ANTENNA_CONFIGURATION_INDEX_ONE;
+	params.initiator_snr_control = BT_CS_TEST_INITIATOR_SNR_CONTROL_NOT_USED;
+	params.reflector_snr_control = BT_CS_TEST_REFLECTOR_SNR_CONTROL_NOT_USED;
+	params.drbg_nonce = 0x1234;
+	params.override_config = 0;
+	params.override_config_0.channel_map_repetition = 1;
+	memset(params.override_config_0.not_set.channel_map, 0,
+		sizeof(params.override_config_0.not_set.channel_map));
+	params.override_config_0.not_set.channel_map[1] = 0xFF;
+	params.override_config_0.not_set.channel_selection_type =
+		BT_CS_TEST_OVERRIDE_0_CHSEL_ALG_3B;
+	params.override_config_0.not_set.ch3c_shape =
+		BT_CS_TEST_OVERRIDE_0_CHSEL_ALG_3C_HAT_SHAPE;
+	params.override_config_0.not_set.ch3c_jump = 0x2;
+
+	err = bt_cs_start_test(&params);
+	if (err) {
+		shell_error(sh, "bt_cs_start_test returned error %d", err);
+		return -ENOEXEC;
+	}
+
+	return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	cs_cmds,
 	SHELL_CMD_ARG(
@@ -148,6 +210,10 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 		read_remote_fae_table, NULL,
 		"<None>",
 		cmd_read_remote_fae_table, 1, 0),
+	SHELL_CMD_ARG(
+		start_simple_cs_test, NULL,
+		"<Role selection (initiator, reflector): 0, 1>",
+		cmd_cs_test_simple, 2, 0),
 	SHELL_SUBCMD_SET_END);
 
 static int cmd_cs(const struct shell *sh, size_t argc, char **argv)

jostapei99/cs736-zephyr

include/zephyr/bluetooth/hci_types.h
@@ -2436,6 +2436,152 @@ struct bt_hci_cp_le_read_remote_fae_table {
 	uint16_t handle;
 } __packed;
 
+
+#define BT_HCI_OP_LE_CS_TEST BT_OP(BT_OGF_LE, 0x0095) /* 0x2095 */
+
+#define BT_HCI_OP_LE_CS_MAIN_MODE_1 0x1
+#define BT_HCI_OP_LE_CS_MAIN_MODE_2 0x2
+#define BT_HCI_OP_LE_CS_MAIN_MODE_3 0x3
+
+#define BT_HCI_OP_LE_CS_SUB_MODE_1 0x1
+#define BT_HCI_OP_LE_CS_SUB_MODE_2 0x2
+#define BT_HCI_OP_LE_CS_SUB_MODE_3 0x3
+#define BT_HCI_OP_LE_CS_SUB_MODE_UNUSED 0xFF
+
+#define BT_HCI_OP_LE_CS_INITIATOR_ROLE 0x0
+#define BT_HCI_OP_LE_CS_REFLECTOR_ROLE 0x1
+
+#define BT_HCI_OP_LE_CS_RTT_TYPE_AA_ONLY     0x0
+#define BT_HCI_OP_LE_CS_RTT_TYPE_32BIT_SOUND 0x1
+#define BT_HCI_OP_LE_CS_RTT_TYPE_96BIT_SOUND 0x2
+#define BT_HCI_OP_LE_CS_RTT_TYPE_32BIT_RAND  0x3
+#define BT_HCI_OP_LE_CS_RTT_TYPE_64BIT_RAND  0x4
+#define BT_HCI_OP_LE_CS_RTT_TYPE_96BIT_RAND  0x5
+#define BT_HCI_OP_LE_CS_RTT_TYPE_128BIT_RAND 0x6
+
+#define BT_HCI_OP_LE_CS_CS_SYNC_1M     0x1
+#define BT_HCI_OP_LE_CS_CS_SYNC_2M     0x2
+#define BT_HCI_OP_LE_CS_CS_SYNC_2M_2BT 0x3
+
+#define BT_HCI_OP_LE_CS_TEST_MAXIMIZE_TX_POWER 0x7E
+#define BT_HCI_OP_LE_CS_TEST_MINIMIZE_TX_POWER 0x7F
+
+#define BT_HCI_OP_LE_CS_TEST_ACI_0 0x0
+#define BT_HCI_OP_LE_CS_TEST_ACI_1 0x1
+#define BT_HCI_OP_LE_CS_TEST_ACI_2 0x2
+#define BT_HCI_OP_LE_CS_TEST_ACI_3 0x3
+#define BT_HCI_OP_LE_CS_TEST_ACI_4 0x4
+#define BT_HCI_OP_LE_CS_TEST_ACI_5 0x5
+#define BT_HCI_OP_LE_CS_TEST_ACI_6 0x6
+#define BT_HCI_OP_LE_CS_TEST_ACI_7 0x7
+
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_18 0x0
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_21 0x1
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_24 0x2
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_27 0x3
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_30 0x4
+#define BT_HCI_OP_LE_CS_TEST_INITIATOR_SNR_NOT_USED 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_18 0x0
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_21 0x1
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_24 0x2
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_27 0x3
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_30 0x4
+#define BT_HCI_OP_LE_CS_TEST_REFLECTOR_SNR_NOT_USED 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_0_MASK BIT(0)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_2_MASK BIT(2)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_3_MASK BIT(3)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_4_MASK BIT(4)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_5_MASK BIT(5)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_6_MASK BIT(6)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_7_MASK BIT(7)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_8_MASK BIT(8)
+#define BT_HCI_OP_LE_CS_TEST_OVERRIDE_CONFIG_10_MASK BIT(10)
+
+#define BT_HCI_OP_LE_CS_TEST_CHSEL_TYPE_3B 0x0
+#define BT_HCI_OP_LE_CS_TEST_CHSEL_TYPE_3C 0x1
+
+#define BT_HCI_OP_LE_CS_TEST_CH3C_SHAPE_HAT 0x0
+#define BT_HCI_OP_LE_CS_TEST_CH3C_SHAPE_X   0x1
+
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_NONE 0x0
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_INIT 0x1
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_REFL 0x2
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_BOTH 0x3
+#define BT_HCI_OP_LE_CS_TEST_TONE_EXT_REPEAT 0x4
+
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_00 0x0
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_01 0x1
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_02 0x2
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_03 0x3
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_04 0x4
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_05 0x5
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_06 0x6
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_07 0x7
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_08 0x8
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_09 0x9
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_10 0xA
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_11 0xB
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_12 0xC
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_13 0xD
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_14 0xE
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_15 0xF
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_16 0x10
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_17 0x11
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_18 0x12
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_19 0x13
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_20 0x14
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_21 0x15
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_22 0x16
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_23 0x17
+#define BT_HCI_OP_LE_CS_TEST_AP_INDEX_LOOP 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_2_POSITION_NOT_PRESENT 0xFF
+
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_VAL_0011 0x0
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_VAL_1100 0x1
+#define BT_HCI_OP_LE_CS_TEST_SS_MARKER_VAL_LOOP 0x2
+
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_PRBS9    0x00
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_11110000 0x01
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_10101010 0x02
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_PRBS15   0x03
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_11111111 0x04
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_00000000 0x05
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_00001111 0x06
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_01010101 0x07
+#define BT_HCI_OP_LE_CS_TEST_PAYLOAD_USER     0x80
+
+struct bt_hci_op_le_cs_test {
+	uint8_t main_mode_type;
+	uint8_t sub_mode_type;
+	uint8_t main_mode_repetition;
+	uint8_t mode_0_steps;
+	uint8_t role;
+	uint8_t rtt_type;
+	uint8_t cs_sync_phy;
+	uint8_t cs_sync_antenna_selection;
+	uint8_t subevent_len[3];
+	uint16_t subevent_interval;
+	uint8_t max_num_subevents;
+	uint8_t transmit_power_level;
+	uint8_t t_ip1_time;
+	uint8_t t_ip2_time;
+	uint8_t t_fcs_time;
+	uint8_t t_pm_time;
+	uint8_t t_sw_time;
+	uint8_t tone_antenna_config_selection;
+	uint8_t reserved;
+	uint8_t snr_control_initiator;
+	uint8_t snr_control_reflector;
+	uint16_t drbg_nonce;
+	uint8_t channel_map_repetition;
+	uint16_t override_config;
+	uint8_t override_parameters_length;
+	uint8_t override_parameters_data[];
+} __packed;
+
 /* Event definitions */
 
 #define BT_HCI_EVT_UNKNOWN                      0x00

subsys/bluetooth/shell/cs.c
@@ -133,6 +133,67 @@ static int cmd_read_remote_fae_table(const struct shell *sh, size_t argc, char *
 
 	return 0;
 }
+
+static int cmd_cs_test_simple(const struct shell *sh, size_t argc, char *argv[])
+{
+	int err = 0;
+	struct bt_cs_test_param params;
+
+	params.main_mode = BT_CS_TEST_MAIN_MODE_1;
+	params.sub_mode = BT_CS_TEST_SUB_MODE_UNUSED;
+	params.main_mode_repetition = 0;
+	params.mode_0_steps = 0x1;
+
+	params.role = shell_strtoul(argv[1], 16, &err);
+
+	if (err) {
+		shell_help(sh);
+		shell_error(sh, "Could not parse input 1, Role selection");
+		return SHELL_CMD_HELP_PRINTED;
+	}
+
+	if (params.role != BT_CS_TEST_ROLE_INITIATOR && params.role != BT_CS_TEST_ROLE_REFLECTOR) {
+		shell_help(sh);
+		shell_error(sh, "Role selection input invalid");
+		return SHELL_CMD_HELP_PRINTED;
+	}
+
+	params.rtt_type = BT_CS_TEST_RTT_AA_ONLY;
+	params.cs_sync_phy = BT_CS_TEST_CS_SYNC_LE_1M_PHY;
+	params.cs_sync_antenna_selection = BT_CS_TEST_CS_SYNC_ANTENNA_SELECTION_ONE;
+	params.subevent_len = 10000;
+	params.subevent_interval = 0;
+	params.max_num_subevents = 0;
+	params.transmit_power_level = BT_HCI_OP_LE_CS_TEST_MAXIMIZE_TX_POWER;
+	params.t_ip1_time = 80;
+	params.t_ip2_time = 80;
+	params.t_fcs_time = 120;
+	params.t_pm_time = 20;
+	params.t_sw_time = 0;
+	params.tone_antenna_config_selection = BT_CS_TEST_TONE_ANTENNA_CONFIGURATION_INDEX_ONE;
+	params.initiator_snr_control = BT_CS_TEST_INITIATOR_SNR_CONTROL_NOT_USED;
+	params.reflector_snr_control = BT_CS_TEST_REFLECTOR_SNR_CONTROL_NOT_USED;
+	params.drbg_nonce = 0x1234;
+	params.override_config = 0;
+	params.override_config_0.channel_map_repetition = 1;
+	memset(params.override_config_0.not_set.channel_map, 0,
+		sizeof(params.override_config_0.not_set.channel_map));
+	params.override_config_0.not_set.channel_map[1] = 0xFF;
+	params.override_config_0.not_set.channel_selection_type =
+		BT_CS_TEST_OVERRIDE_0_CHSEL_ALG_3B;
+	params.override_config_0.not_set.ch3c_shape =
+		BT_CS_TEST_OVERRIDE_0_CHSEL_ALG_3C_HAT_SHAPE;
+	params.override_config_0.not_set.ch3c_jump = 0x2;
+
+	err = bt_cs_start_test(&params);
+	if (err) {
+		shell_error(sh, "bt_cs_start_test returned error %d", err);
+		return -ENOEXEC;
+	}
+
+	return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(
 	cs_cmds,
 	SHELL_CMD_ARG(
@@ -148,6 +209,10 @@ SHELL_STATIC_SUBCMD_SET_CREATE(
 		read_remote_fae_table, NULL,
 		"<None>",
 		cmd_read_remote_fae_table, 1, 0),
+	SHELL_CMD_ARG(
+		start_simple_cs_test, NULL,
+		"<Role selection (initiator, reflector): 0, 1>",
+		cmd_cs_test_simple, 2, 0),
 	SHELL_SUBCMD_SET_END);
 
 static int cmd_cs(const struct shell *sh, size_t argc, char **argv)