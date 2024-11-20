ravi-sharma-adi/zephyr

tests: posix: common: separate posix xsi streams to standalone test
posix.common contains testsuites that can be separated into smaller
groups of tests. This change moves stropts into a singular
testsuite at tests/posix/xsi_streams app directory.

Signed-off-by: Marvin Ouma <pancakesdeath@protonmail.com>

tests/posix/xopen_streams/src/main.c
@@ -7,7 +7,7 @@
 #include <stropts.h>
 #include <errno.h>
 
-ZTEST(stropts, test_putmsg)
+ZTEST(posix_xopen_streams, test_putmsg)
 {
 	const struct strbuf *ctrl = NULL;
 	const struct strbuf *data = NULL;
@@ -18,7 +18,7 @@ ZTEST(stropts, test_putmsg)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_fdetach)
+ZTEST(posix_xopen_streams, test_fdetach)
 {
 	char *path = NULL;
 	int ret = fdetach(path);
@@ -27,7 +27,7 @@ ZTEST(stropts, test_fdetach)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_fattach)
+ZTEST(posix_xopen_streams, test_fattach)
 {
 	char *path = NULL;
 	int fd = -1;
@@ -37,7 +37,7 @@ ZTEST(stropts, test_fattach)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_getmsg)
+ZTEST(posix_xopen_streams, test_getmsg)
 {
 	struct strbuf *ctrl = NULL;
 	struct strbuf *data = NULL;
@@ -48,7 +48,7 @@ ZTEST(stropts, test_getmsg)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_getpmsg)
+ZTEST(posix_xopen_streams, test_getpmsg)
 {
 	struct strbuf *ctrl = NULL;
 	struct strbuf *data = NULL;
@@ -59,7 +59,7 @@ ZTEST(stropts, test_getpmsg)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_isastream)
+ZTEST(posix_xopen_streams, test_isastream)
 {
 	int fd = -1;
 	int ret = isastream(fd);
@@ -68,4 +68,4 @@ ZTEST(stropts, test_isastream)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST_SUITE(stropts, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(posix_xopen_streams, NULL, NULL, NULL, NULL, NULL);

jostapei99/cs736-zephyr

tests/posix/xsi_streams/src/main.c
@@ -7,7 +7,7 @@
 #include <stropts.h>
 #include <errno.h>
 
-ZTEST(stropts, test_putmsg)
+ZTEST(xsi_streams, test_putmsg)
 {
 	const struct strbuf *ctrl = NULL;
 	const struct strbuf *data = NULL;
@@ -18,7 +18,7 @@ ZTEST(stropts, test_putmsg)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_fdetach)
+ZTEST(xsi_streams, test_fdetach)
 {
 	char *path = NULL;
 	int ret = fdetach(path);
@@ -27,7 +27,7 @@ ZTEST(stropts, test_fdetach)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_fattach)
+ZTEST(xsi_streams, test_fattach)
 {
 	char *path = NULL;
 	int fd = -1;
@@ -37,7 +37,7 @@ ZTEST(stropts, test_fattach)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_getmsg)
+ZTEST(xsi_streams, test_getmsg)
 {
 	struct strbuf *ctrl = NULL;
 	struct strbuf *data = NULL;
@@ -48,7 +48,7 @@ ZTEST(stropts, test_getmsg)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_getpmsg)
+ZTEST(xsi_streams, test_getpmsg)
 {
 	struct strbuf *ctrl = NULL;
 	struct strbuf *data = NULL;
@@ -59,7 +59,7 @@ ZTEST(stropts, test_getpmsg)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST(stropts, test_isastream)
+ZTEST(xsi_streams, test_isastream)
 {
 	int fd = -1;
 	int ret = isastream(fd);
@@ -68,4 +68,4 @@ ZTEST(stropts, test_isastream)
 	zassert_equal(errno, ENOSYS, "Expected errno ENOSYS, got %d", errno);
 }
 
-ZTEST_SUITE(stropts, NULL, NULL, NULL, NULL, NULL);
+ZTEST_SUITE(xsi_streams, NULL, NULL, NULL, NULL, NULL);