Johnsonyu0926/zephyr

drivers: video: controls: use the same CID numbers as Linux
Refers to the CID numbers found in the Linux Kernel so that each CID
of Zephyr matches the CIDs of Linux. The new CIDs introduced in
Zephyr can then follow the same number and naming found on Linux.

No renaming of the CID macro names is done yet.

Signed-off-by: Josuah Demangeon <me@josuah.net>

include/zephyr/drivers/video-controls.h
@@ -62,9 +62,9 @@ extern "C" {
  * @{
  */
 /** Mirror the picture horizontally */
-#define VIDEO_CID_HFLIP			(VIDEO_CTRL_CLASS_BASE + 0)
+#define VIDEO_CID_HFLIP			(VIDEO_CTRL_CLASS_BASE + 20)
 /** Mirror the picture vertically */
-#define VIDEO_CID_VFLIP			(VIDEO_CTRL_CLASS_BASE + 1)
+#define VIDEO_CID_VFLIP			(VIDEO_CTRL_CLASS_BASE + 21)
 /**
  * @}
  */
@@ -73,15 +73,15 @@ extern "C" {
  * @name Camera class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CTRL_CLASS_CAMERA_BASE + 0)
-#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CTRL_CLASS_CAMERA_BASE + 1)
-#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CTRL_CLASS_CAMERA_BASE + 2)
-#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CTRL_CLASS_CAMERA_BASE + 3)
-#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CTRL_CLASS_CAMERA_BASE + 4)
-#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CTRL_CLASS_CAMERA_BASE + 5)
-#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CTRL_CLASS_CAMERA_BASE + 6)
-#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CTRL_CLASS_CAMERA_BASE + 7)
-#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CTRL_CLASS_CAMERA_BASE + 8)
+#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CTRL_CLASS_BASE + 17)
+#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CTRL_CLASS_BASE + 19)
+#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CTRL_CLASS_CAMERA_BASE + 13)
+#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CTRL_CLASS_BASE + 0)
+#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CTRL_CLASS_BASE + 2)
+#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CTRL_CLASS_BASE + 26)
+#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CTRL_CLASS_BASE + 1)
+#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CTRL_CLASS_IMAGE_PROC_BASE + 3)
+#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CTRL_CLASS_JPEG_BASE + 3)
 /**
  * @}
  */

jostapei99/cs736-zephyr

include/zephyr/drivers/video-controls.h
@@ -56,13 +56,13 @@ extern "C" {
  * @{
  */
 /** Mirror the picture horizontally */
-#define VIDEO_CID_HFLIP			(VIDEO_CID_BASE + 0)
+#define VIDEO_CID_HFLIP			(VIDEO_CID_BASE + 20)
 /** Mirror the picture vertically */
-#define VIDEO_CID_VFLIP			(VIDEO_CID_BASE + 1)
+#define VIDEO_CID_VFLIP			(VIDEO_CID_BASE + 21)
 /** Power line frequency (enum) filter to avoid flicker */
-#define VIDEO_CID_POWER_LINE_FREQUENCY (VIDEO_CTRL_CLASS_GENERIC + 2)
+#define VIDEO_CID_POWER_LINE_FREQUENCY	(VIDEO_CID_BASE + 24)
 /** Pixel rate (pixels/second) in the device's pixel array. This control is read-only. */
-#define VIDEO_CID_PIXEL_RATE           (VIDEO_CTRL_CLASS_GENERIC + 3)
+#define VIDEO_CID_PIXEL_RATE		(VIDEO_CID_IMAGE_PROC_CLASS_BASE + 2)
 enum video_power_line_frequency {
 	VIDEO_CID_POWER_LINE_FREQUENCY_DISABLED = 0,
 	VIDEO_CID_POWER_LINE_FREQUENCY_50HZ = 1,
@@ -77,16 +77,16 @@ enum video_power_line_frequency {
  * @name Camera class control IDs
  * @{
  */
-#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CID_CAMERA_CLASS_BASE + 0)
-#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CID_CAMERA_CLASS_BASE + 1)
-#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CID_CAMERA_CLASS_BASE + 2)
-#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CID_CAMERA_CLASS_BASE + 3)
-#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CID_CAMERA_CLASS_BASE + 4)
-#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CID_CAMERA_CLASS_BASE + 5)
-#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CID_CAMERA_CLASS_BASE + 6)
-#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CID_CAMERA_CLASS_BASE + 7)
-#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CID_CAMERA_CLASS_BASE + 8)
-#define VIDEO_CID_CAMERA_HUE		(VIDEO_CID_CAMERA_CLASS_BASE + 9)
+#define VIDEO_CID_CAMERA_EXPOSURE	(VIDEO_CID_BASE + 17)
+#define VIDEO_CID_CAMERA_GAIN		(VIDEO_CID_BASE + 19)
+#define VIDEO_CID_CAMERA_ZOOM		(VIDEO_CID_CAMERA_CLASS_BASE + 13)
+#define VIDEO_CID_CAMERA_BRIGHTNESS	(VIDEO_CID_BASE + 0)
+#define VIDEO_CID_CAMERA_SATURATION	(VIDEO_CID_BASE + 2)
+#define VIDEO_CID_CAMERA_WHITE_BAL	(VIDEO_CID_BASE + 26)
+#define VIDEO_CID_CAMERA_CONTRAST	(VIDEO_CID_BASE + 1)
+#define VIDEO_CID_CAMERA_COLORBAR	(VIDEO_CID_IMAGE_PROC_CLASS_BASE + 3)
+#define VIDEO_CID_CAMERA_HUE		(VIDEO_CID_BASE + 3)
+#define VIDEO_CID_CAMERA_QUALITY	(VIDEO_CID_JPEG_CLASS_BASE + 3)
 /**
  * @}
  */