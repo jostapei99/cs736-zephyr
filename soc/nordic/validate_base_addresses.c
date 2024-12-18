ravi-sharma-adi/zephyr

dts: common: nordic: nrf54h20: define BICR node
BICR (Board Information Configuration Registers) are located within the
application UICR region (ref. MRAM mapping, table 38).

Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

soc/nordic/validate_base_addresses.c
@@ -330,6 +330,7 @@ CHECK_DT_REG(uart136, NRF_UARTE136);
 CHECK_DT_REG(uart137, NRF_UARTE137);
 CHECK_DT_REG(uicr, NRF_UICR);
 CHECK_DT_REG(cpuapp_uicr, NRF_APPLICATION_UICR);
+CHECK_DT_REG(bicr, NRF_APPLICATION_BICR_NS);
 CHECK_DT_REG(cpurad_uicr, NRF_RADIOCORE_UICR);
 CHECK_DT_REG(usbd, NRF_USBD);
 CHECK_DT_REG(usbhs, NRF_USBHS);

jostapei99/cs736-zephyr

soc/nordic/validate_base_addresses.c
@@ -330,6 +330,7 @@ CHECK_DT_REG(uart136, NRF_UARTE136);
 CHECK_DT_REG(uart137, NRF_UARTE137);
 CHECK_DT_REG(uicr, NRF_UICR);
 CHECK_DT_REG(cpuapp_uicr, NRF_APPLICATION_UICR);
+CHECK_DT_REG(bicr, NRF_APPLICATION_BICR);
 CHECK_DT_REG(cpurad_uicr, NRF_RADIOCORE_UICR);
 CHECK_DT_REG(usbd, NRF_USBD);
 CHECK_DT_REG(usbhs, NRF_USBHS);