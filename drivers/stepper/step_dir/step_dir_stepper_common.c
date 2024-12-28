zephyrproject-rtos/zephyr

drivers: stepper: Add timing source for step dir stepper
Adds a timing source api which is used by the step-dir stepper common code.
This allows the reusable common code to configure different timing sources,
since the initial delayable work implementation was inacurate for higher
maximum velocities.

Signed-off-by: Fabian Blatz <fabianblatz@gmail.com>

drivers/stepper/step_dir/step_dir_stepper_common.c
@@ -47,20 +47,70 @@ static inline int step_dir_stepper_perform_step(const struct device *dev)
 	return 0;
 }
 
+static void stepper_trigger_callback(const struct device *dev, enum stepper_event event)
+{
+	struct step_dir_stepper_common_data *data = dev->data;
+
+	if (!data->callback) {
+		LOG_WRN_ONCE("No callback set");
+		return;
+	}
+
+	if (!k_is_in_isr()) {
+		data->callback(dev, event, data->event_cb_user_data);
+		return;
+	}
+
+#ifdef CONFIG_STEP_DIR_STEPPER_GENERATE_ISR_SAFE_EVENTS
+	/* Dispatch to msgq instead of raising directly */
+	int ret = k_msgq_put(&data->event_msgq, &event, K_NO_WAIT);
+
+	if (ret != 0) {
+		LOG_WRN("Failed to put event in msgq: %d", ret);
+	}
+
+	ret = k_work_submit(&data->event_callback_work);
+	if (ret < 0) {
+		LOG_ERR("Failed to submit work item: %d", ret);
+	}
+#else
+	LOG_WRN_ONCE("Event callback called from ISR context without ISR safe events enabled");
+#endif
+}
+
+#ifdef CONFIG_STEP_DIR_STEPPER_GENERATE_ISR_SAFE_EVENTS
+static void stepper_work_event_handler(struct k_work *work)
+{
+	struct step_dir_stepper_common_data *data =
+		CONTAINER_OF(work, struct step_dir_stepper_common_data, event_callback_work);
+	enum stepper_event event;
+	int ret;
+
+	ret = k_msgq_get(&data->event_msgq, &event, K_NO_WAIT);
+	if (ret != 0) {
+		return;
+	}
+
+	/* Run the callback */
+	if (data->callback != NULL) {
+		data->callback(data->dev, event, data->event_cb_user_data);
+	}
+
+	/* If there are more pending events, resubmit this work item to handle them */
+	if (k_msgq_num_used_get(&data->event_msgq) > 0) {
+		k_work_submit(work);
+	}
+}
+#endif /* CONFIG_STEP_DIR_STEPPER_GENERATE_ISR_SAFE_EVENTS */
+
 static void update_remaining_steps(struct step_dir_stepper_common_data *data)
 {
 	if (data->step_count > 0) {
 		data->step_count--;
-		(void)k_work_reschedule(&data->stepper_dwork, K_USEC(data->delay_in_us));
 	} else if (data->step_count < 0) {
 		data->step_count++;
-		(void)k_work_reschedule(&data->stepper_dwork, K_USEC(data->delay_in_us));
 	} else {
-		if (!data->callback) {
-			LOG_WRN_ONCE("No callback set");
-			return;
-		}
-		data->callback(data->dev, STEPPER_EVENT_STEPS_COMPLETED, data->event_cb_user_data);
+		stepper_trigger_callback(data->dev, STEPPER_EVENT_STEPS_COMPLETED);
 	}
 }
 
@@ -80,34 +130,41 @@ static void update_direction_from_step_count(const struct device *dev)
 static void position_mode_task(const struct device *dev)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	if (data->step_count) {
 		(void)step_dir_stepper_perform_step(dev);
 	}
+
 	update_remaining_steps(dev->data);
+
+	if (config->timing_source->needs_reschedule(dev) && data->step_count != 0) {
+		(void)config->timing_source->start(dev);
+	}
 }
 
 static void velocity_mode_task(const struct device *dev)
 {
-	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	(void)step_dir_stepper_perform_step(dev);
-	(void)k_work_reschedule(&data->stepper_dwork, K_USEC(data->delay_in_us));
+
+	if (config->timing_source->needs_reschedule(dev)) {
+		(void)config->timing_source->start(dev);
+	}
 }
 
-static void stepper_work_step_handler(struct k_work *work)
+void stepper_handle_timing_signal(const struct device *dev)
 {
-	struct k_work_delayable *dwork = k_work_delayable_from_work(work);
-	struct step_dir_stepper_common_data *data =
-		CONTAINER_OF(dwork, struct step_dir_stepper_common_data, stepper_dwork);
+	struct step_dir_stepper_common_data *data = dev->data;
 
 	K_SPINLOCK(&data->lock) {
 		switch (data->run_mode) {
 		case STEPPER_RUN_MODE_POSITION:
-			position_mode_task(data->dev);
+			position_mode_task(dev);
 			break;
 		case STEPPER_RUN_MODE_VELOCITY:
-			velocity_mode_task(data->dev);
+			velocity_mode_task(dev);
 			break;
 		default:
 			LOG_WRN("Unsupported run mode: %d", data->run_mode);
@@ -119,7 +176,6 @@ static void stepper_work_step_handler(struct k_work *work)
 int step_dir_stepper_common_init(const struct device *dev)
 {
 	const struct step_dir_stepper_common_config *config = dev->config;
-	struct step_dir_stepper_common_data *data = dev->data;
 	int ret;
 
 	if (!gpio_is_ready_dt(&config->step_pin) || !gpio_is_ready_dt(&config->dir_pin)) {
@@ -139,7 +195,21 @@ int step_dir_stepper_common_init(const struct device *dev)
 		return ret;
 	}
 
-	k_work_init_delayable(&data->stepper_dwork, stepper_work_step_handler);
+	if (config->timing_source->init) {
+		ret = config->timing_source->init(dev);
+		if (ret < 0) {
+			LOG_ERR("Failed to initialize timing source: %d", ret);
+			return ret;
+		}
+	}
+
+#ifdef CONFIG_STEP_DIR_STEPPER_GENERATE_ISR_SAFE_EVENTS
+	struct step_dir_stepper_common_data *data = dev->data;
+
+	k_msgq_init(&data->event_msgq, data->event_msgq_buffer, sizeof(enum stepper_event),
+		    CONFIG_STEP_DIR_STEPPER_EVENT_QUEUE_LEN);
+	k_work_init(&data->event_callback_work, stepper_work_event_handler);
+#endif
 
 	return 0;
 }
@@ -147,8 +217,9 @@ int step_dir_stepper_common_init(const struct device *dev)
 int step_dir_stepper_common_move_by(const struct device *dev, const int32_t micro_steps)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
-	if (data->delay_in_us == 0) {
+	if (data->max_velocity == 0) {
 		LOG_ERR("Velocity not set or invalid velocity set");
 		return -EINVAL;
 	}
@@ -156,8 +227,9 @@ int step_dir_stepper_common_move_by(const struct device *dev, const int32_t micr
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_POSITION;
 		data->step_count = micro_steps;
+		config->timing_source->update(dev, data->max_velocity);
 		update_direction_from_step_count(dev);
-		(void)k_work_reschedule(&data->stepper_dwork, K_NO_WAIT);
+		config->timing_source->start(dev);
 	}
 
 	return 0;
@@ -166,6 +238,7 @@ int step_dir_stepper_common_move_by(const struct device *dev, const int32_t micr
 int step_dir_stepper_common_set_max_velocity(const struct device *dev, const uint32_t velocity)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	if (velocity == 0) {
 		LOG_ERR("Velocity cannot be zero");
@@ -178,7 +251,8 @@ int step_dir_stepper_common_set_max_velocity(const struct device *dev, const uin
 	}
 
 	K_SPINLOCK(&data->lock) {
-		data->delay_in_us = USEC_PER_SEC / velocity;
+		data->max_velocity = velocity;
+		config->timing_source->update(dev, velocity);
 	}
 
 	return 0;
@@ -209,8 +283,9 @@ int step_dir_stepper_common_get_actual_position(const struct device *dev, int32_
 int step_dir_stepper_common_move_to(const struct device *dev, const int32_t value)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
-	if (data->delay_in_us == 0) {
+	if (data->max_velocity == 0) {
 		LOG_ERR("Velocity not set or invalid velocity set");
 		return -EINVAL;
 	}
@@ -218,8 +293,9 @@ int step_dir_stepper_common_move_to(const struct device *dev, const int32_t valu
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_POSITION;
 		data->step_count = value - data->actual_position;
+		config->timing_source->update(dev, data->max_velocity);
 		update_direction_from_step_count(dev);
-		(void)k_work_reschedule(&data->stepper_dwork, K_NO_WAIT);
+		config->timing_source->start(dev);
 	}
 
 	return 0;
@@ -227,9 +303,9 @@ int step_dir_stepper_common_move_to(const struct device *dev, const int32_t valu
 
 int step_dir_stepper_common_is_moving(const struct device *dev, bool *is_moving)
 {
-	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
-	*is_moving = k_work_delayable_is_pending(&data->stepper_dwork);
+	*is_moving = config->timing_source->is_running(dev);
 	return 0;
 }
 
@@ -237,15 +313,17 @@ int step_dir_stepper_common_run(const struct device *dev, const enum stepper_dir
 				const uint32_t velocity)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_VELOCITY;
 		data->direction = direction;
+		data->max_velocity = velocity;
+		config->timing_source->update(dev, velocity);
 		if (velocity != 0) {
-			data->delay_in_us = USEC_PER_SEC / velocity;
-			(void)k_work_reschedule(&data->stepper_dwork, K_NO_WAIT);
+			config->timing_source->start(dev);
 		} else {
-			(void)k_work_cancel_delayable(&data->stepper_dwork);
+			config->timing_source->stop(dev);
 		}
 	}
 

drivers/stepper/step_dir/step_dir_stepper_common.h
@@ -17,6 +17,9 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/stepper.h>
+#include <zephyr/drivers/counter.h>
+
+#include "step_dir_stepper_timing_source.h"
 
 /**
  * @brief Common step direction stepper config.
@@ -27,19 +30,27 @@ struct step_dir_stepper_common_config {
 	const struct gpio_dt_spec step_pin;
 	const struct gpio_dt_spec dir_pin;
 	bool dual_edge;
+	const struct stepper_timing_source_api *timing_source;
+	const struct device *counter;
 };
 
 /**
  * @brief Initialize common step direction stepper config from devicetree instance.
+ *        If the counter property is set, the timing source will be set to the counter timing
+ * source.
  *
  * @param node_id The devicetree node identifier.
  */
 #define STEP_DIR_STEPPER_DT_COMMON_CONFIG_INIT(node_id)                                            \
 	{                                                                                          \
 		.step_pin = GPIO_DT_SPEC_GET(node_id, step_gpios),                                 \
-		.dir_pin = GPIO_DT_SPEC_GET(node_id, dir_gpios),	                           \
+		.dir_pin = GPIO_DT_SPEC_GET(node_id, dir_gpios),                                   \
 		.dual_edge = DT_PROP_OR(node_id, dual_edge_step, false),                           \
-	}
+		.counter = DEVICE_DT_GET_OR_NULL(DT_PHANDLE(node_id, counter)),                    \
+		.timing_source = COND_CODE_1(DT_NODE_HAS_PROP(node_id, counter),                   \
+						(&step_counter_timing_source_api),                 \
+						(&step_work_timing_source_api)),    \
+		}
 
 /**
  * @brief Initialize common step direction stepper config from devicetree instance.
@@ -58,12 +69,24 @@ struct step_dir_stepper_common_data {
 	struct k_spinlock lock;
 	enum stepper_direction direction;
 	enum stepper_run_mode run_mode;
-	struct k_work_delayable stepper_dwork;
 	int32_t actual_position;
-	uint32_t delay_in_us;
+	uint32_t max_velocity;
 	int32_t step_count;
 	stepper_event_callback_t callback;
 	void *event_cb_user_data;
+
+	struct k_work_delayable stepper_dwork;
+
+#ifdef CONFIG_STEP_DIR_STEPPER_COUNTER_TIMING
+	struct counter_top_cfg counter_top_cfg;
+#endif /* CONFIG_STEP_DIR_STEPPER_COUNTER_TIMING */
+
+#ifdef CONFIG_STEP_DIR_STEPPER_GENERATE_ISR_SAFE_EVENTS
+	struct k_work event_callback_work;
+	struct k_msgq event_msgq;
+	uint8_t event_msgq_buffer[CONFIG_STEP_DIR_STEPPER_EVENT_QUEUE_LEN *
+				  sizeof(enum stepper_event)];
+#endif /* CONFIG_STEP_DIR_STEPPER_GENERATE_ISR_SAFE_EVENTS */
 };
 
 /**
@@ -185,6 +208,12 @@ int step_dir_stepper_common_run(const struct device *dev, const enum stepper_dir
  */
 int step_dir_stepper_common_set_event_callback(const struct device *dev,
 					       stepper_event_callback_t callback, void *user_data);
+
+/**
+ * @brief Handle a timing signal and update the stepper position.
+ * @param dev Pointer to the device structure.
+ */
+void stepper_handle_timing_signal(const struct device *dev);
 
 /** @} */
 

jostapei99/cs736-zephyr

drivers/stepper/step_dir/step_dir_stepper_common.c
@@ -47,20 +47,70 @@ static inline int step_dir_stepper_perform_step(const struct device *dev)
 	return 0;
 }
 
+static void stepper_trigger_callback(const struct device *dev, enum stepper_event event)
+{
+	struct step_dir_stepper_common_data *data = dev->data;
+
+	if (!data->callback) {
+		LOG_WRN_ONCE("No callback set");
+		return;
+	}
+
+	if (!k_is_in_isr()) {
+		data->callback(dev, event, data->event_cb_user_data);
+		return;
+	}
+
+#ifdef CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS
+	/* Dispatch to msgq instead of raising directly */
+	int ret = k_msgq_put(&data->event_msgq, &event, K_NO_WAIT);
+
+	if (ret != 0) {
+		LOG_WRN("Failed to put event in msgq: %d", ret);
+	}
+
+	ret = k_work_submit(&data->event_callback_work);
+	if (ret < 0) {
+		LOG_ERR("Failed to submit work item: %d", ret);
+	}
+#else
+	LOG_WRN_ONCE("Event callback called from ISR context without ISR safe events enabled");
+#endif /* CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS */
+}
+
+#ifdef CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS
+static void stepper_work_event_handler(struct k_work *work)
+{
+	struct step_dir_stepper_common_data *data =
+		CONTAINER_OF(work, struct step_dir_stepper_common_data, event_callback_work);
+	enum stepper_event event;
+	int ret;
+
+	ret = k_msgq_get(&data->event_msgq, &event, K_NO_WAIT);
+	if (ret != 0) {
+		return;
+	}
+
+	/* Run the callback */
+	if (data->callback != NULL) {
+		data->callback(data->dev, event, data->event_cb_user_data);
+	}
+
+	/* If there are more pending events, resubmit this work item to handle them */
+	if (k_msgq_num_used_get(&data->event_msgq) > 0) {
+		k_work_submit(work);
+	}
+}
+#endif /* CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS */
+
 static void update_remaining_steps(struct step_dir_stepper_common_data *data)
 {
 	if (data->step_count > 0) {
 		data->step_count--;
-		(void)k_work_reschedule(&data->stepper_dwork, K_USEC(data->delay_in_us));
 	} else if (data->step_count < 0) {
 		data->step_count++;
-		(void)k_work_reschedule(&data->stepper_dwork, K_USEC(data->delay_in_us));
 	} else {
-		if (!data->callback) {
-			LOG_WRN_ONCE("No callback set");
-			return;
-		}
-		data->callback(data->dev, STEPPER_EVENT_STEPS_COMPLETED, data->event_cb_user_data);
+		stepper_trigger_callback(data->dev, STEPPER_EVENT_STEPS_COMPLETED);
 	}
 }
 
@@ -80,34 +130,41 @@ static void update_direction_from_step_count(const struct device *dev)
 static void position_mode_task(const struct device *dev)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	if (data->step_count) {
 		(void)step_dir_stepper_perform_step(dev);
 	}
+
 	update_remaining_steps(dev->data);
+
+	if (config->timing_source->needs_reschedule(dev) && data->step_count != 0) {
+		(void)config->timing_source->start(dev);
+	}
 }
 
 static void velocity_mode_task(const struct device *dev)
 {
-	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	(void)step_dir_stepper_perform_step(dev);
-	(void)k_work_reschedule(&data->stepper_dwork, K_USEC(data->delay_in_us));
+
+	if (config->timing_source->needs_reschedule(dev)) {
+		(void)config->timing_source->start(dev);
+	}
 }
 
-static void stepper_work_step_handler(struct k_work *work)
+void stepper_handle_timing_signal(const struct device *dev)
 {
-	struct k_work_delayable *dwork = k_work_delayable_from_work(work);
-	struct step_dir_stepper_common_data *data =
-		CONTAINER_OF(dwork, struct step_dir_stepper_common_data, stepper_dwork);
+	struct step_dir_stepper_common_data *data = dev->data;
 
 	K_SPINLOCK(&data->lock) {
 		switch (data->run_mode) {
 		case STEPPER_RUN_MODE_POSITION:
-			position_mode_task(data->dev);
+			position_mode_task(dev);
 			break;
 		case STEPPER_RUN_MODE_VELOCITY:
-			velocity_mode_task(data->dev);
+			velocity_mode_task(dev);
 			break;
 		default:
 			LOG_WRN("Unsupported run mode: %d", data->run_mode);
@@ -119,7 +176,6 @@ static void stepper_work_step_handler(struct k_work *work)
 int step_dir_stepper_common_init(const struct device *dev)
 {
 	const struct step_dir_stepper_common_config *config = dev->config;
-	struct step_dir_stepper_common_data *data = dev->data;
 	int ret;
 
 	if (!gpio_is_ready_dt(&config->step_pin) || !gpio_is_ready_dt(&config->dir_pin)) {
@@ -139,7 +195,21 @@ int step_dir_stepper_common_init(const struct device *dev)
 		return ret;
 	}
 
-	k_work_init_delayable(&data->stepper_dwork, stepper_work_step_handler);
+	if (config->timing_source->init) {
+		ret = config->timing_source->init(dev);
+		if (ret < 0) {
+			LOG_ERR("Failed to initialize timing source: %d", ret);
+			return ret;
+		}
+	}
+
+#ifdef CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS
+	struct step_dir_stepper_common_data *data = dev->data;
+
+	k_msgq_init(&data->event_msgq, data->event_msgq_buffer, sizeof(enum stepper_event),
+		    CONFIG_STEPPER_STEP_DIR_EVENT_QUEUE_LEN);
+	k_work_init(&data->event_callback_work, stepper_work_event_handler);
+#endif /* CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS */
 
 	return 0;
 }
@@ -147,8 +217,9 @@ int step_dir_stepper_common_init(const struct device *dev)
 int step_dir_stepper_common_move_by(const struct device *dev, const int32_t micro_steps)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
-	if (data->delay_in_us == 0) {
+	if (data->max_velocity == 0) {
 		LOG_ERR("Velocity not set or invalid velocity set");
 		return -EINVAL;
 	}
@@ -156,8 +227,9 @@ int step_dir_stepper_common_move_by(const struct device *dev, const int32_t micr
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_POSITION;
 		data->step_count = micro_steps;
+		config->timing_source->update(dev, data->max_velocity);
 		update_direction_from_step_count(dev);
-		(void)k_work_reschedule(&data->stepper_dwork, K_NO_WAIT);
+		config->timing_source->start(dev);
 	}
 
 	return 0;
@@ -166,6 +238,7 @@ int step_dir_stepper_common_move_by(const struct device *dev, const int32_t micr
 int step_dir_stepper_common_set_max_velocity(const struct device *dev, const uint32_t velocity)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	if (velocity == 0) {
 		LOG_ERR("Velocity cannot be zero");
@@ -178,7 +251,8 @@ int step_dir_stepper_common_set_max_velocity(const struct device *dev, const uin
 	}
 
 	K_SPINLOCK(&data->lock) {
-		data->delay_in_us = USEC_PER_SEC / velocity;
+		data->max_velocity = velocity;
+		config->timing_source->update(dev, velocity);
 	}
 
 	return 0;
@@ -209,8 +283,9 @@ int step_dir_stepper_common_get_actual_position(const struct device *dev, int32_
 int step_dir_stepper_common_move_to(const struct device *dev, const int32_t value)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
-	if (data->delay_in_us == 0) {
+	if (data->max_velocity == 0) {
 		LOG_ERR("Velocity not set or invalid velocity set");
 		return -EINVAL;
 	}
@@ -218,8 +293,9 @@ int step_dir_stepper_common_move_to(const struct device *dev, const int32_t valu
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_POSITION;
 		data->step_count = value - data->actual_position;
+		config->timing_source->update(dev, data->max_velocity);
 		update_direction_from_step_count(dev);
-		(void)k_work_reschedule(&data->stepper_dwork, K_NO_WAIT);
+		config->timing_source->start(dev);
 	}
 
 	return 0;
@@ -227,9 +303,9 @@ int step_dir_stepper_common_move_to(const struct device *dev, const int32_t valu
 
 int step_dir_stepper_common_is_moving(const struct device *dev, bool *is_moving)
 {
-	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
-	*is_moving = k_work_delayable_is_pending(&data->stepper_dwork);
+	*is_moving = config->timing_source->is_running(dev);
 	return 0;
 }
 
@@ -237,15 +313,17 @@ int step_dir_stepper_common_run(const struct device *dev, const enum stepper_dir
 				const uint32_t velocity)
 {
 	struct step_dir_stepper_common_data *data = dev->data;
+	const struct step_dir_stepper_common_config *config = dev->config;
 
 	K_SPINLOCK(&data->lock) {
 		data->run_mode = STEPPER_RUN_MODE_VELOCITY;
 		data->direction = direction;
+		data->max_velocity = velocity;
+		config->timing_source->update(dev, velocity);
 		if (velocity != 0) {
-			data->delay_in_us = USEC_PER_SEC / velocity;
-			(void)k_work_reschedule(&data->stepper_dwork, K_NO_WAIT);
+			config->timing_source->start(dev);
 		} else {
-			(void)k_work_cancel_delayable(&data->stepper_dwork);
+			config->timing_source->stop(dev);
 		}
 	}
 

drivers/stepper/step_dir/step_dir_stepper_common.h
@@ -17,6 +17,9 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/stepper.h>
+#include <zephyr/drivers/counter.h>
+
+#include "step_dir_stepper_timing_source.h"
 
 /**
  * @brief Common step direction stepper config.
@@ -27,18 +30,26 @@ struct step_dir_stepper_common_config {
 	const struct gpio_dt_spec step_pin;
 	const struct gpio_dt_spec dir_pin;
 	bool dual_edge;
+	const struct stepper_timing_source_api *timing_source;
+	const struct device *counter;
 };
 
 /**
  * @brief Initialize common step direction stepper config from devicetree instance.
+ *        If the counter property is set, the timing source will be set to the counter timing
+ *        source.
  *
  * @param node_id The devicetree node identifier.
  */
 #define STEP_DIR_STEPPER_DT_COMMON_CONFIG_INIT(node_id)                                            \
 	{                                                                                          \
 		.step_pin = GPIO_DT_SPEC_GET(node_id, step_gpios),                                 \
-		.dir_pin = GPIO_DT_SPEC_GET(node_id, dir_gpios),	                           \
-		.dual_edge = DT_PROP_OR(node_id, dual_edge_step, false),                           \
+		.dir_pin = GPIO_DT_SPEC_GET(node_id, dir_gpios),                                   \
+		.dual_edge = DT_PROP(node_id, dual_edge_step),                                     \
+		.counter = DEVICE_DT_GET_OR_NULL(DT_PHANDLE(node_id, counter)),                    \
+		.timing_source = COND_CODE_1(DT_NODE_HAS_PROP(node_id, counter),                   \
+						(&step_counter_timing_source_api),                 \
+						(&step_work_timing_source_api)),                   \
 	}
 
 /**
@@ -58,12 +69,25 @@ struct step_dir_stepper_common_data {
 	struct k_spinlock lock;
 	enum stepper_direction direction;
 	enum stepper_run_mode run_mode;
-	struct k_work_delayable stepper_dwork;
 	int32_t actual_position;
-	uint32_t delay_in_us;
+	uint32_t max_velocity;
 	int32_t step_count;
 	stepper_event_callback_t callback;
 	void *event_cb_user_data;
+
+	struct k_work_delayable stepper_dwork;
+
+#ifdef CONFIG_STEP_DIR_STEPPER_COUNTER_TIMING
+	struct counter_top_cfg counter_top_cfg;
+	bool counter_running;
+#endif /* CONFIG_STEP_DIR_STEPPER_COUNTER_TIMING */
+
+#ifdef CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS
+	struct k_work event_callback_work;
+	struct k_msgq event_msgq;
+	uint8_t event_msgq_buffer[CONFIG_STEPPER_STEP_DIR_EVENT_QUEUE_LEN *
+				  sizeof(enum stepper_event)];
+#endif /* CONFIG_STEPPER_STEP_DIR_GENERATE_ISR_SAFE_EVENTS */
 };
 
 /**
@@ -185,6 +209,12 @@ int step_dir_stepper_common_run(const struct device *dev, const enum stepper_dir
  */
 int step_dir_stepper_common_set_event_callback(const struct device *dev,
 					       stepper_event_callback_t callback, void *user_data);
+
+/**
+ * @brief Handle a timing signal and update the stepper position.
+ * @param dev Pointer to the device structure.
+ */
+void stepper_handle_timing_signal(const struct device *dev);
 
 /** @} */
 