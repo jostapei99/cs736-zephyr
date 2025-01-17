silverlee35/z_example

clock: driver/clock_control: Add sai clock support for syscon.
Add sai clock support for syscon.

Signed-off-by: Qiang Zhang <qiang.zhang_6@nxp.com>

include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -97,4 +97,7 @@
 #define MCUX_LPUART3_CLK MCUX_LPC_CLK_ID(0x13, 0x03)
 #define MCUX_LPUART4_CLK MCUX_LPC_CLK_ID(0x13, 0x04)
 
+#define MCUX_SAI0_CLK			MCUX_LPC_CLK_ID(0x13, 0x00)
+#define MCUX_SAI1_CLK			MCUX_LPC_CLK_ID(0x13, 0x01)
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */

jostapei99/cs736-zephyr

include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -115,4 +115,7 @@
 #define MCUX_XSPI1_CLK MCUX_LPC_CLK_ID(0x15, 0x01)
 #define MCUX_XSPI2_CLK MCUX_LPC_CLK_ID(0x15, 0x02)
 
+#define MCUX_SAI0_CLK MCUX_LPC_CLK_ID(0x16, 0x00)
+#define MCUX_SAI1_CLK MCUX_LPC_CLK_ID(0x16, 0x01)
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */