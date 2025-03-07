zephyrproject-rtos/zephyr

Bluetooth: SDP: Ignore the unexpected response
The response should not be handled if there is not a request has been
sent.

Ignore the response if the `session->param` is `NULL`.

Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

subsys/bluetooth/host/classic/sdp.c
@@ -2117,6 +2117,11 @@ static int sdp_client_receive(struct bt_l2cap_chan *chan, struct net_buf *buf)
 		return 0;
 	}
 
+	if (session->param == NULL) {
+		LOG_ERR("No request in progress");
+		return 0;
+	}
+
 	switch (hdr->op_code) {
 	case BT_SDP_SVC_SEARCH_RSP:
 		return sdp_client_receive_ss(session, buf);

jostapei99/cs736-zephyr

subsys/bluetooth/host/classic/sdp.c
@@ -2124,6 +2124,11 @@ static int sdp_client_receive(struct bt_l2cap_chan *chan, struct net_buf *buf)
 		return 0;
 	}
 
+	if (session->param == NULL) {
+		LOG_WRN("No request in progress");
+		return 0;
+	}
+
 	switch (hdr->op_code) {
 	case BT_SDP_SVC_SEARCH_RSP:
 		return sdp_client_receive_ss(session, buf);