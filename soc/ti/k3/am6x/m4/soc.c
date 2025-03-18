zephyrproject-rtos/zephyr

soc: ti_k3: specify soc-specific ctrl partitions
The first two partitions in padconfig mmr regions need to be unlocked for
the pinctrl driver to be able to write. However, the base addresses for the
the registers can be different across SoCs and domains. Besides, currently
this is only done for M4 and that too not in the local (M4's) view.

This patch introduces a file specifying all ctrl partition base addresses
using ifdef directives for different SoCs and variants, and unlocking them
before the kernel and drivers initialize.

Signed-off-by: Amneesh Singh <a-singh7@ti.com>

soc/ti/k3/am6x/m4/soc.c
@@ -4,18 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <common/ctrl_partitions.h>
 #include <zephyr/arch/cpu.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
 #include <soc.h>
 
-#define ADDR_TRANSLATE_RAT_BASE_ADDR		(0x044200000u)
-#define PINCTRL_BASE_ADDR			(0x4080000u)
-#define KICK0_UNLOCK_VAL			(0x68EF3490U)
-#define KICK1_UNLOCK_VAL			(0xD172BC5AU)
-#define CSL_MCU_PADCONFIG_LOCK0_KICK0_OFFSET	(0x1008)
-#define CSL_MCU_PADCONFIG_LOCK1_KICK0_OFFSET	(0x5008)
+#define ADDR_TRANSLATE_RAT_BASE_ADDR (0x044200000u)
 
 static struct address_trans_region_config am6x_region_config[] = {
 	{
@@ -44,27 +40,10 @@ static struct address_trans_region_config am6x_region_config[] = {
  */
 };
 
-static void am6x_mmr_unlock(void)
-{
-	uint32_t baseAddr = PINCTRL_BASE_ADDR;
-	uintptr_t kickAddr;
-
-	/* Lock 0 */
-	kickAddr = baseAddr + CSL_MCU_PADCONFIG_LOCK0_KICK0_OFFSET;
-	sys_write32(KICK0_UNLOCK_VAL, kickAddr);   /* KICK 0 */
-	kickAddr = kickAddr + sizeof(uint32_t *);
-	sys_write32(KICK1_UNLOCK_VAL, kickAddr);   /* KICK 1 */
-
-	/* Lock 1 */
-	kickAddr = baseAddr + CSL_MCU_PADCONFIG_LOCK1_KICK0_OFFSET;
-	sys_write32(KICK0_UNLOCK_VAL, kickAddr);   /* KICK 0 */
-	kickAddr = kickAddr + sizeof(uint32_t *);
-	sys_write32(KICK1_UNLOCK_VAL, kickAddr);   /* KICK 1 */
-}
-
 void soc_early_init_hook(void)
 {
 	sys_mm_drv_ti_rat_init(am6x_region_config, ADDR_TRANSLATE_RAT_BASE_ADDR,
-		ARRAY_SIZE(am6x_region_config));
-	am6x_mmr_unlock();
+			       ARRAY_SIZE(am6x_region_config));
+
+	k3_unlock_all_ctrl_partitions();
 }

jostapei99/cs736-zephyr

soc/ti/k3/am6x/m4/soc.c
@@ -4,18 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <common/ctrl_partitions.h>
 #include <zephyr/arch/cpu.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
 #include <soc.h>
 
-#define ADDR_TRANSLATE_RAT_BASE_ADDR		(0x44200000u)
-#define PINCTRL_BASE_ADDR			(0x04080000u)
-#define KICK0_UNLOCK_VAL			(0x68EF3490U)
-#define KICK1_UNLOCK_VAL			(0xD172BC5AU)
-#define CSL_MCU_PADCONFIG_LOCK0_KICK0_OFFSET	(0x1008)
-#define CSL_MCU_PADCONFIG_LOCK1_KICK0_OFFSET	(0x5008)
+#define ADDR_TRANSLATE_RAT_BASE_ADDR (0x44200000u)
 
 static struct address_trans_region_config am6x_region_config[] = {
 	{
@@ -38,27 +34,10 @@ static struct address_trans_region_config am6x_region_config[] = {
  */
 };
 
-static void am6x_mmr_unlock(void)
-{
-	uint32_t baseAddr = PINCTRL_BASE_ADDR;
-	uintptr_t kickAddr;
-
-	/* Lock 0 */
-	kickAddr = baseAddr + CSL_MCU_PADCONFIG_LOCK0_KICK0_OFFSET;
-	sys_write32(KICK0_UNLOCK_VAL, kickAddr);   /* KICK 0 */
-	kickAddr = kickAddr + sizeof(uint32_t *);
-	sys_write32(KICK1_UNLOCK_VAL, kickAddr);   /* KICK 1 */
-
-	/* Lock 1 */
-	kickAddr = baseAddr + CSL_MCU_PADCONFIG_LOCK1_KICK0_OFFSET;
-	sys_write32(KICK0_UNLOCK_VAL, kickAddr);   /* KICK 0 */
-	kickAddr = kickAddr + sizeof(uint32_t *);
-	sys_write32(KICK1_UNLOCK_VAL, kickAddr);   /* KICK 1 */
-}
-
 void soc_early_init_hook(void)
 {
 	sys_mm_drv_ti_rat_init(am6x_region_config, ADDR_TRANSLATE_RAT_BASE_ADDR,
-		ARRAY_SIZE(am6x_region_config));
-	am6x_mmr_unlock();
+			       ARRAY_SIZE(am6x_region_config));
+
+	k3_unlock_all_ctrl_partitions();
 }