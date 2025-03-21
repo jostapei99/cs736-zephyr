jostapei99/cs736-zephyr

commit 2991c8d199f7fefe62e6ecc735f2be0fa4a39bcb

drivers: clock_control: add calibration for h7 pllx_hsi
This will calibrate the HSI's PLLX clocks if enabled
The value rcc_hsicalibartion_default is 0x40U for h7/h7rs.

Signed-off-by: Fabrice DJIATSA <fabrice.djiatsa-ext@st.com>

drivers/clock_control/clock_stm32_ll_h7.c
@@ -675,9 +675,18 @@ static void set_up_fixed_clock_sources(void)
 	}
 
 	if (IS_ENABLED(STM32_HSI_ENABLED)) {
-		/* Enable HSI oscillator */
-		LL_RCC_HSI_Enable();
-		while (LL_RCC_HSI_IsReady() != 1) {
+		if (IS_ENABLED(STM32_PLL_SRC_HSI) || IS_ENABLED(STM32_PLL2_SRC_HSI) ||
++		    IS_ENABLED(STM32_PLL3_SRC_HSI)) {
+			/* HSI calibration */
+			LL_RCC_HSI_SetCalibTrimming(RCC_HSICALIBRATION_DEFAULT);
+		}
+		/* Enable HSI if not enabled */
+		if (LL_RCC_HSI_IsReady() != 1) {
+			/* Enable HSI oscillator */
+			LL_RCC_HSI_Enable();
+			while (LL_RCC_HSI_IsReady() != 1) {
+			/* Wait for HSI ready */
+			}
 		}
 		/* HSI divider configuration */
 		LL_RCC_HSI_SetDivider(hsi_divider(STM32_HSI_DIVISOR));

commit 40b6b449f1e131f57b1d451de2e701029888f8a5

drivers: clock_control: fix typo in STM32H7* clock init
Correct typo in STM32H7 HSI clock calibration directives introduced
in commit 2991c8d199f7 ("drivers: clock_control: add calibration for
h7 pllx_hsi") that currently has no functionnal impact but is worth
to be fixed for consistency.

No functional change,

Fixes: 2991c8d199f7fefe62e6ecc735f2be0fa4a39bcb
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

drivers/clock_control/clock_stm32_ll_h7.c
@@ -687,7 +687,7 @@ static void set_up_fixed_clock_sources(void)
 
 	if (IS_ENABLED(STM32_HSI_ENABLED)) {
 		if (IS_ENABLED(STM32_PLL_SRC_HSI) || IS_ENABLED(STM32_PLL2_SRC_HSI) ||
-+		    IS_ENABLED(STM32_PLL3_SRC_HSI)) {
+		    IS_ENABLED(STM32_PLL3_SRC_HSI)) {
 			/* HSI calibration */
 			LL_RCC_HSI_SetCalibTrimming(RCC_HSICALIBRATION_DEFAULT);
 		}