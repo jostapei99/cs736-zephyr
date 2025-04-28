zephyrproject-rtos/zephyr

drivers: net: ethernet: change ETHERNET_LINK_*BASE_T
change ETHERNET_LINK_*BASE_T to ETHERNET_LINK_*BASE.

Signed-off-by: Fin Maaß <f.maass@vogl-electronic.com>

drivers/ethernet/dsa/dsa_ksz8xxx.c
@@ -1114,8 +1114,8 @@ static enum ethernet_hw_caps dsa_port_get_capabilities(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	return ETHERNET_DSA_SLAVE_PORT | ETHERNET_LINK_10BASE_T |
-		ETHERNET_LINK_100BASE_T;
+	return ETHERNET_DSA_SLAVE_PORT | ETHERNET_LINK_10BASE |
+		ETHERNET_LINK_100BASE;
 }
 
 const struct ethernet_api dsa_eth_api_funcs = {

jostapei99/cs736-zephyr

drivers/ethernet/dsa/dsa_ksz8xxx.c
@@ -1114,8 +1114,8 @@ static enum ethernet_hw_caps dsa_port_get_capabilities(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 
-	return ETHERNET_DSA_USER_PORT | ETHERNET_LINK_10BASE_T |
-		ETHERNET_LINK_100BASE_T;
+	return ETHERNET_DSA_USER_PORT | ETHERNET_LINK_10BASE |
+		ETHERNET_LINK_100BASE;
 }
 
 const struct ethernet_api dsa_eth_api_funcs = {