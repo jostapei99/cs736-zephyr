zephyrproject-rtos/zephyr

fs: littlefs: define default disk version in Kconfig
This makes it possible to set the disk version when Devicetree is not
used for configuration.

Signed-off-by: Xudong Zheng <7pkvm5aw@slicealias.com>

subsys/fs/littlefs_fs.c
@@ -793,7 +793,10 @@ static int littlefs_init_cfg(struct fs_littlefs *fs, int flags)
 	uint32_t disk_version = lcp->disk_version;
 
 	if (disk_version == 0) {
-		disk_version = LFS_DISK_VERSION;
+		disk_version = CONFIG_FS_LITTLEFS_DISK_VERSION;
+		if (disk_version == 0) {
+			disk_version = LFS_DISK_VERSION;
+		}
 	}
 #endif /* CONFIG_FS_LITTLEFS_MULTIVERSION */
 

jostapei99/cs736-zephyr

subsys/fs/littlefs_fs.c
@@ -793,7 +793,10 @@ static int littlefs_init_cfg(struct fs_littlefs *fs, int flags)
 	uint32_t disk_version = lcp->disk_version;
 
 	if (disk_version == 0) {
-		disk_version = LFS_DISK_VERSION;
+		disk_version = CONFIG_FS_LITTLEFS_DISK_VERSION_NUMBER;
+		if (disk_version == 0) {
+			disk_version = LFS_DISK_VERSION;
+		}
 	}
 #endif /* CONFIG_FS_LITTLEFS_DISK_VERSION */
 