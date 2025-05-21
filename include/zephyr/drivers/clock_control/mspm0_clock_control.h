zephyrproject-rtos/zephyr

drivers: clock: ti: Add initial support TI MSPM0 clock module
Add initial support TI MSPM0 clock module

Signed-off-by: Saravanan Sekar <saravanan@linumiz.com>
Signed-off-by: Jackson Farley <j-farley@ti.com>

include/zephyr/drivers/clock_control/mspm0_clock_control.h
@@ -1,0 +1,18 @@
+/*
+ * Copyright (c) 2025 Texas Instruments Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_MSPM0_CLOCK_CONTROL
+#define ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_MSPM0_CLOCK_CONTROL
+
+#include <zephyr/dt-bindings/clock/mspm0_clock.h>
+
+struct mspm0_sys_clock {
+	uint32_t bus;
+};
+
+#define MSPM0_CLOCK_SUBSYS_FN(index) {.bus = DT_INST_CLOCKS_CELL(index, bus)}
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_MSPM0_CLOCK_CONTROL */

jostapei99/cs736-zephyr

include/zephyr/drivers/clock_control/mspm0_clock_control.h
@@ -1,0 +1,18 @@
+/*
+ * Copyright (c) 2025 Texas Instruments Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_MSPM0_CLOCK_CONTROL
+#define ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_MSPM0_CLOCK_CONTROL
+
+#include <zephyr/dt-bindings/clock/mspm0_clock.h>
+
+struct mspm0_sys_clock {
+	uint32_t clk;
+};
+
+#define MSPM0_CLOCK_SUBSYS_FN(index) {.clk = DT_INST_CLOCKS_CELL(index, clk)}
+
+#endif /* ZEPHYR_INCLUDE_DRIVERS_CLOCK_CONTROL_MSPM0_CLOCK_CONTROL */