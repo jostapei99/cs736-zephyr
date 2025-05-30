zephyrproject-rtos/zephyr

drivers: stepper: adi_tmc: tmc51xx uart support
Added support for tmc51xx uart support alongside the
existing spi support. The tmc51xx driver is now consolidated
into a dedicated folder.

Signed-off-by: Dipak Shetty <shetty.dipak@gmx.com>

drivers/stepper/adi_tmc/tmc51xx/tmc51xx.h
@@ -1,0 +1,65 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Dipak Shetty
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_STEPPER_ADI_TMC51XX_H
+#define ZEPHYR_DRIVERS_STEPPER_ADI_TMC51XX_H
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/stepper/stepper_trinamic.h>
+
+#include <adi_tmc_bus.h>
+
+#define DT_DRV_COMPAT adi_tmc51xx
+
+/* Check for supported bus types */
+#define TMC51XX_BUS_SPI  DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+#define TMC51XX_BUS_UART DT_ANY_INST_ON_BUS_STATUS_OKAY(uart)
+
+/* Common configuration structure for TMC51xx */
+struct tmc51xx_config {
+	union tmc_bus bus;
+	const struct tmc_bus_io *bus_io;
+	uint8_t comm_type;
+	const uint32_t gconf;
+	const uint32_t clock_frequency;
+	const uint16_t default_micro_step_res;
+	const int8_t sg_threshold;
+	const bool is_sg_enabled;
+	const uint32_t sg_velocity_check_interval_ms;
+	const uint32_t sg_threshold_velocity;
+#ifdef CONFIG_STEPPER_ADI_TMC51XX_RAMP_GEN
+	const struct tmc_ramp_generator_data default_ramp_config;
+#endif
+#if TMC51XX_BUS_UART
+	const struct gpio_dt_spec sw_sel_gpio;
+	uint8_t uart_slave_addr;
+#endif
+#if TMC51XX_BUS_SPI
+	struct gpio_dt_spec diag0_gpio;
+#endif
+};
+
+struct tmc51xx_data {
+	struct k_sem sem;
+	struct k_work_delayable stallguard_dwork;
+	struct k_work_delayable rampstat_callback_dwork;
+	struct gpio_callback diag0_cb;
+	const struct device *stepper;
+	stepper_event_callback_t callback;
+	void *event_cb_user_data;
+};
+
+#if TMC51XX_BUS_SPI
+/* SPI bus I/O operations for TMC51xx devices */
+extern const struct tmc_bus_io tmc51xx_spi_bus_io;
+#endif
+
+#if TMC51XX_BUS_UART
+/* UART bus I/O operations for TMC51xx devices */
+extern const struct tmc_bus_io tmc51xx_uart_bus_io;
+#endif
+
+#endif /* ZEPHYR_DRIVERS_STEPPER_ADI_TMC51XX_H */

drivers/stepper/adi_tmc/tmc51xx/tmc51xx_uart.c
@@ -1,0 +1,63 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Dipak Shetty
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+
+#include "tmc51xx.h"
+#include <adi_tmc_bus.h>
+#include <adi_tmc_uart.h>
+
+#if TMC51XX_BUS_UART
+LOG_MODULE_DECLARE(tmc51xx, CONFIG_STEPPER_LOG_LEVEL);
+
+static int tmc51xx_bus_check_uart(const union tmc_bus *bus, uint8_t comm_type)
+{
+	if (comm_type != TMC_COMM_UART) {
+		return -ENOTSUP;
+	}
+	return device_is_ready(bus->uart) ? 0 : -ENODEV;
+}
+
+static int tmc51xx_reg_write_uart(const struct device *dev, const uint8_t reg_addr,
+				  const uint32_t reg_val)
+{
+	const struct tmc51xx_config *config = dev->config;
+	int err;
+
+	/* Route to the adi_tmc_uart.h implementation */
+	err = tmc_uart_write_register(config->bus.uart, config->uart_slave_addr, reg_addr, reg_val);
+
+	if (err < 0) {
+		LOG_ERR("Failed to write register 0x%x with value 0x%x", reg_addr, reg_val);
+	}
+	/* Wait for the write to complete */
+	k_sleep(K_MSEC(1));
+	return err;
+}
+
+static int tmc51xx_reg_read_uart(const struct device *dev, const uint8_t reg_addr,
+				 uint32_t *reg_val)
+{
+	const struct tmc51xx_config *config = dev->config;
+	int err;
+
+	/* Route to the adi_tmc_uart.h implementation */
+	err = tmc_uart_read_register(config->bus.uart, config->uart_slave_addr, reg_addr, reg_val);
+
+	if (err < 0) {
+		LOG_ERR("Failed to read register 0x%x", reg_addr);
+	}
+	/* Wait for the read to complete */
+	k_sleep(K_MSEC(1));
+	return err;
+}
+
+const struct tmc_bus_io tmc51xx_uart_bus_io = {
+	.check = tmc51xx_bus_check_uart,
+	.read = tmc51xx_reg_read_uart,
+	.write = tmc51xx_reg_write_uart,
+};
+#endif /* TMC51XX_BUS_UART */

jostapei99/cs736-zephyr

drivers/stepper/adi_tmc/tmc51xx/tmc51xx.h
@@ -1,0 +1,65 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Dipak Shetty
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_STEPPER_ADI_TMC51XX_H
+#define ZEPHYR_DRIVERS_STEPPER_ADI_TMC51XX_H
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/stepper/stepper_trinamic.h>
+
+#include <adi_tmc_bus.h>
+
+#define DT_DRV_COMPAT adi_tmc51xx
+
+/* Check for supported bus types */
+#define TMC51XX_BUS_SPI  DT_ANY_INST_ON_BUS_STATUS_OKAY(spi)
+#define TMC51XX_BUS_UART DT_ANY_INST_ON_BUS_STATUS_OKAY(uart)
+
+/* Common configuration structure for TMC51xx */
+struct tmc51xx_config {
+	union tmc_bus bus;
+	const struct tmc_bus_io *bus_io;
+	uint8_t comm_type;
+	const uint32_t gconf;
+	const uint32_t clock_frequency;
+	const uint16_t default_micro_step_res;
+	const int8_t sg_threshold;
+	const bool is_sg_enabled;
+	const uint32_t sg_velocity_check_interval_ms;
+	const uint32_t sg_threshold_velocity;
+#ifdef CONFIG_STEPPER_ADI_TMC51XX_RAMP_GEN
+	const struct tmc_ramp_generator_data default_ramp_config;
+#endif
+#if TMC51XX_BUS_UART
+	const struct gpio_dt_spec sw_sel_gpio;
+	uint8_t uart_addr;
+#endif
+#if TMC51XX_BUS_SPI
+	struct gpio_dt_spec diag0_gpio;
+#endif
+};
+
+struct tmc51xx_data {
+	struct k_sem sem;
+	struct k_work_delayable stallguard_dwork;
+	struct k_work_delayable rampstat_callback_dwork;
+	struct gpio_callback diag0_cb;
+	const struct device *stepper;
+	stepper_event_callback_t callback;
+	void *event_cb_user_data;
+};
+
+#if TMC51XX_BUS_SPI
+/* SPI bus I/O operations for TMC51xx devices */
+extern const struct tmc_bus_io tmc51xx_spi_bus_io;
+#endif
+
+#if TMC51XX_BUS_UART
+/* UART bus I/O operations for TMC51xx devices */
+extern const struct tmc_bus_io tmc51xx_uart_bus_io;
+#endif
+
+#endif /* ZEPHYR_DRIVERS_STEPPER_ADI_TMC51XX_H */

drivers/stepper/adi_tmc/tmc51xx/tmc51xx_uart.c
@@ -1,0 +1,63 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Dipak Shetty
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+
+#include "tmc51xx.h"
+#include <adi_tmc_bus.h>
+#include <adi_tmc_uart.h>
+
+#if TMC51XX_BUS_UART
+LOG_MODULE_DECLARE(tmc51xx, CONFIG_STEPPER_LOG_LEVEL);
+
+static int tmc51xx_bus_check_uart(const union tmc_bus *bus, uint8_t comm_type)
+{
+	if (comm_type != TMC_COMM_UART) {
+		return -ENOTSUP;
+	}
+	return device_is_ready(bus->uart) ? 0 : -ENODEV;
+}
+
+static int tmc51xx_reg_write_uart(const struct device *dev, const uint8_t reg_addr,
+				  const uint32_t reg_val)
+{
+	const struct tmc51xx_config *config = dev->config;
+	int err;
+
+	/* Route to the adi_tmc_uart.h implementation */
+	err = tmc_uart_write_register(config->bus.uart, config->uart_addr, reg_addr, reg_val);
+
+	if (err < 0) {
+		LOG_ERR("Failed to write register 0x%x with value 0x%x", reg_addr, reg_val);
+	}
+	/* Wait for the write to complete */
+	k_sleep(K_MSEC(1));
+	return err;
+}
+
+static int tmc51xx_reg_read_uart(const struct device *dev, const uint8_t reg_addr,
+				 uint32_t *reg_val)
+{
+	const struct tmc51xx_config *config = dev->config;
+	int err;
+
+	/* Route to the adi_tmc_uart.h implementation */
+	err = tmc_uart_read_register(config->bus.uart, config->uart_addr, reg_addr, reg_val);
+
+	if (err < 0) {
+		LOG_ERR("Failed to read register 0x%x", reg_addr);
+	}
+	/* Wait for the read to complete */
+	k_sleep(K_MSEC(1));
+	return err;
+}
+
+const struct tmc_bus_io tmc51xx_uart_bus_io = {
+	.check = tmc51xx_bus_check_uart,
+	.read = tmc51xx_reg_read_uart,
+	.write = tmc51xx_reg_write_uart,
+};
+#endif /* TMC51XX_BUS_UART */