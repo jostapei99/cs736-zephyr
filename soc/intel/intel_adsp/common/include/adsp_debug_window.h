jostapei99/cs736-zephyr

commit 7f24d16356635a0978e004e371bf8a9420537d7e

soc: intel_adsp: add a debug window slot descriptor
Add a debug window slot descriptor for the debug window area in the
first page, where descriptors themselves occupy the first few
hundreds of bytes. This area begins 1024 bytes after the beginning of
the debug window until the end of the page and is therefore 3 * 1024
bytes large. Descriptor 15 will be used to describe it and it will be
used for the GDB stub.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

soc/intel/intel_adsp/common/include/adsp_debug_window.h
@@ -40,12 +40,15 @@
 #define ADSP_DW_PAGE_SIZE		0x1000
 #define ADSP_DW_SLOT_SIZE		ADSP_DW_PAGE_SIZE
 #define ADSP_DW_SLOT_COUNT		15
+#define ADSP_DW_DESC_COUNT		(ADSP_DW_SLOT_COUNT + 1)
 
 /* debug window slots usage, mutually exclusive options can reuse slots */
 #define ADSP_DW_SLOT_NUM_SHELL		0
 #define ADSP_DW_SLOT_NUM_MTRACE		0
 #define ADSP_DW_SLOT_NUM_TRACE		1
 #define ADSP_DW_SLOT_NUM_TELEMETRY	1
+/* this uses remaining space in the first page after descriptors */
+#define ADSP_DW_SLOT_NUM_GDB		(ADSP_DW_DESC_COUNT - 1)
 
 /* debug log slot types */
 #define ADSP_DW_SLOT_UNUSED		0x00000000
@@ -69,9 +72,9 @@ struct adsp_dw_desc {
 } __packed;
 
 struct adsp_debug_window {
-	struct adsp_dw_desc descs[ADSP_DW_SLOT_COUNT];
-	uint8_t reserved[ADSP_DW_SLOT_SIZE - ADSP_DW_SLOT_COUNT * sizeof(struct adsp_dw_desc)];
-	uint8_t slots[ADSP_DW_SLOT_COUNT][ADSP_DW_SLOT_SIZE];
+	struct adsp_dw_desc descs[ADSP_DW_DESC_COUNT];
+	uint8_t reserved[ADSP_DW_SLOT_SIZE - ADSP_DW_DESC_COUNT * sizeof(struct adsp_dw_desc)];
+	uint8_t slots[ADSP_DW_DESC_COUNT][ADSP_DW_SLOT_SIZE];
 } __packed;
 
 #define WIN2_MBASE DT_REG_ADDR(DT_PHANDLE(DT_NODELABEL(mem_window2), memory))

commit 91fe249413f4a026ae4327f8b1a575b6ef997ba1

soc: intel_adsp: fix debug window number
Fixes: commit 7f24d1635663 ("soc: intel_adsp: add a debug window slot
descriptor") - restore the maximum debug slot number. The added slot
resides in page 0, not at the end of the array. The bug didn't have
any run-time implications - no access to those high index value slots
is performed, all platforms so far have 3 slots at most, so this is a
pure correctness fix.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

soc/intel/intel_adsp/common/include/adsp_debug_window.h
@@ -74,7 +74,7 @@ struct adsp_dw_desc {
 struct adsp_debug_window {
 	struct adsp_dw_desc descs[ADSP_DW_DESC_COUNT];
 	uint8_t reserved[ADSP_DW_SLOT_SIZE - ADSP_DW_DESC_COUNT * sizeof(struct adsp_dw_desc)];
-	uint8_t slots[ADSP_DW_DESC_COUNT][ADSP_DW_SLOT_SIZE];
+	uint8_t slots[ADSP_DW_SLOT_COUNT][ADSP_DW_SLOT_SIZE];
 } __packed;
 
 #define WIN2_MBASE DT_REG_ADDR(DT_PHANDLE(DT_NODELABEL(mem_window2), memory))