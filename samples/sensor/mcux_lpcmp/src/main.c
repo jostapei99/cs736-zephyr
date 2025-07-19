zephyrproject-rtos/zephyr

boards: frdm_mcxa166, frdm_mcxa276: add lpcmp support
1. enable sensor/nxp,lpcmp support
2. verified samples/sensor/mcux_lpcmp
3. update the mcux_lpcmp to support different port

Signed-off-by: Peter Wang <chaoyi.wang@nxp.com>

samples/sensor/mcux_lpcmp/src/main.c
@@ -17,8 +17,8 @@ struct lpcmp_attr {
 };
 
 static const struct lpcmp_attr attrs[] = {
-	{.attr = SENSOR_ATTR_MCUX_LPCMP_POSITIVE_MUX_INPUT, .val = 0},
-	{.attr = SENSOR_ATTR_MCUX_LPCMP_NEGATIVE_MUX_INPUT, .val = 7},
+	{.attr = SENSOR_ATTR_MCUX_LPCMP_POSITIVE_MUX_INPUT, .val = CONFIG_LPCMP_POSOTIVE_PORT},
+	{.attr = SENSOR_ATTR_MCUX_LPCMP_NEGATIVE_MUX_INPUT, .val = CONFIG_LPCMP_NEGATIVE_PORT},
 
 	{.attr = SENSOR_ATTR_MCUX_LPCMP_DAC_ENABLE, .val = 1},
 	{.attr = SENSOR_ATTR_MCUX_LPCMP_DAC_HIGH_POWER_MODE_ENABLE, .val = 0},

jostapei99/cs736-zephyr

samples/sensor/mcux_lpcmp/src/main.c
@@ -17,8 +17,8 @@ struct lpcmp_attr {
 };
 
 static const struct lpcmp_attr attrs[] = {
-	{.attr = SENSOR_ATTR_MCUX_LPCMP_POSITIVE_MUX_INPUT, .val = 0},
-	{.attr = SENSOR_ATTR_MCUX_LPCMP_NEGATIVE_MUX_INPUT, .val = 7},
+	{.attr = SENSOR_ATTR_MCUX_LPCMP_POSITIVE_MUX_INPUT, .val = CONFIG_LPCMP_POSITIVE_PORT},
+	{.attr = SENSOR_ATTR_MCUX_LPCMP_NEGATIVE_MUX_INPUT, .val = CONFIG_LPCMP_NEGATIVE_PORT},
 
 	{.attr = SENSOR_ATTR_MCUX_LPCMP_DAC_ENABLE, .val = 1},
 	{.attr = SENSOR_ATTR_MCUX_LPCMP_DAC_HIGH_POWER_MODE_ENABLE, .val = 0},