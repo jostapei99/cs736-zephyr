zephyrproject-rtos/zephyr

tests: drivers: pwm: Add MCXWX flexio-pwm testing
Enabled testing for the flexio pwm on MCXW7X Platforms.

Signed-off-by: Emilio Benavente <emilio.benavente@nxp.com>

tests/drivers/pwm/pwm_api/src/test_pwm.c
@@ -75,6 +75,11 @@
 #define DEFAULT_PULSE_CYCLE 16384
 #define DEFAULT_PERIOD_NSEC 2000000
 #define DEFAULT_PULSE_NSEC 500000
+#elif defined(CONFIG_SOC_SERIES_MCXW)
+#define DEFAULT_PERIOD_CYCLE 64000
+#define DEFAULT_PULSE_CYCLE 32000
+#define DEFAULT_PERIOD_NSEC 4000000
+#define DEFAULT_PULSE_NSEC 2000000
 #else
 #define DEFAULT_PERIOD_CYCLE 64000
 #define DEFAULT_PULSE_CYCLE 32000

jostapei99/cs736-zephyr

tests/drivers/pwm/pwm_api/src/test_pwm.c
@@ -75,6 +75,11 @@
 #define DEFAULT_PULSE_CYCLE 16384
 #define DEFAULT_PERIOD_NSEC 2000000
 #define DEFAULT_PULSE_NSEC 500000
+#elif defined(CONFIG_SOC_FAMILY_MCXW)
+#define DEFAULT_PERIOD_CYCLE 64000
+#define DEFAULT_PULSE_CYCLE 32000
+#define DEFAULT_PERIOD_NSEC 4000000
+#define DEFAULT_PULSE_NSEC 2000000
 #else
 #define DEFAULT_PERIOD_CYCLE 64000
 #define DEFAULT_PULSE_CYCLE 32000