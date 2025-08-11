zephyrproject-rtos/zephyr

drivers: clock control: Add support clock control for RX261
Add support clock control for RX261

Signed-off-by: Phi Tran <phi.tran.jg@bp.renesas.com>

include/zephyr/drivers/clock_control/renesas_rx_cgc.h
@@ -17,6 +17,16 @@
 		    (UTIL_CAT(RX_CLOCKS_SOURCE_, DT_NODE_FULL_NAME_UPPER_TOKEN(node_id))),        \
 		    (RX_CLOCKS_CLOCK_DISABLED))
 
+#define RX_IF_CLK_SRC(node_id)\
+	COND_CODE_1(DT_NODE_HAS_STATUS(node_id, okay),\
+			(UTIL_CAT(RX_IF_CLOCKS_SOURCE_, DT_NODE_FULL_NAME_UPPER_TOKEN(node_id))),\
+			(RX_CLOCKS_CLOCK_DISABLED))
+
+#define RX_LPT_CLK_SRC(node_id)\
+	COND_CODE_1(DT_NODE_HAS_STATUS(node_id, okay),\
+			(UTIL_CAT(RX_LPT_CLOCKS_SOURCE_, DT_NODE_FULL_NAME_UPPER_TOKEN(node_id))),\
+			(RX_LPT_CLOCKS_NON_USE))
+
 struct clock_control_rx_pclk_cfg {
 	const struct device *clock_src_dev;
 	uint32_t clk_div;

jostapei99/cs736-zephyr

include/zephyr/drivers/clock_control/renesas_rx_cgc.h
@@ -17,6 +17,16 @@
 		    (UTIL_CAT(RX_CLOCKS_SOURCE_, DT_NODE_FULL_NAME_UPPER_TOKEN(node_id))),        \
 		    (RX_CLOCKS_CLOCK_DISABLED))
 
+#define RX_IF_CLK_SRC(node_id)                                                                     \
+	COND_CODE_1(DT_NODE_HAS_STATUS(node_id, okay),\
+			(UTIL_CAT(RX_IF_CLOCKS_SOURCE_, DT_NODE_FULL_NAME_UPPER_TOKEN(node_id))),\
+			(RX_CLOCKS_CLOCK_DISABLED))
+
+#define RX_LPT_CLK_SRC(node_id)                                                                    \
+	COND_CODE_1(DT_NODE_HAS_STATUS(node_id, okay),\
+			(UTIL_CAT(RX_LPT_CLOCKS_SOURCE_, DT_NODE_FULL_NAME_UPPER_TOKEN(node_id))),\
+			(RX_LPT_CLOCKS_NON_USE))
+
 struct clock_control_rx_pclk_cfg {
 	const struct device *clock_src_dev;
 	uint32_t clk_div;