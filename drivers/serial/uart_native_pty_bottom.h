zephyrproject-rtos/zephyr

serial: uart_native_pty: IRQ support
Add support for the interrupt-driven API. Interrupts are
emulated using a polling thread.

Signed-off-by: Tim Pambor <tim.pambor@codewrights.de>

drivers/serial/uart_native_pty_bottom.h
@@ -21,6 +21,7 @@ extern "C" {
 /* Note: None of these functions are public interfaces. But internal to the native ptty driver */
 
 int np_uart_stdin_poll_in_bottom(int in_f, unsigned char *p_char, int len);
+int np_uart_pty_poll_in_bottom(int in_f, unsigned char *p_char, int len);
 int np_uart_slave_connected(int fd);
 int np_uart_open_pty(const char *uart_name, const char *auto_attach_cmd,
 		     bool do_auto_attach, bool wait_pts);

jostapei99/cs736-zephyr

Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

drivers/serial/uart_native_pty_bottom.h
@@ -20,7 +20,7 @@ extern "C" {
 
 /* Note: None of these functions are public interfaces. But internal to the native ptty driver */
 
-int np_uart_stdin_poll_in_bottom(int in_f, unsigned char *p_char, int len);
+int np_uart_stdin_read_bottom(int in_f, unsigned char *p_char, int len);
 int np_uart_slave_connected(int fd);
 int np_uart_open_pty(const char *uart_name, const char *auto_attach_cmd,
 		     bool do_auto_attach, bool wait_pts);