zephyrproject-rtos/zephyr

rtio: Add a context pool
Adds a context pool that can be used when a number of threads may be
dynamically created to use with RTIO. While the pool has a small cost to
it, the cost of verifying a statically declared kobject likely makes it
worth avoiding dynamically allocating these with the typical
kobject_alloc method. Instead this arena style pool of objects can be
used where the kobject validation uses the gperf hash created at build
time.

Signed-off-by: Tom Burdick <thomas.burdick@intel.com>

subsys/rtio/rtio_syscalls.c
@@ -133,3 +133,22 @@ static inline int z_vrfy_rtio_submit(struct rtio *r, uint32_t wait_count)
 	return z_impl_rtio_submit(r, wait_count);
 }
 #include <zephyr/syscalls/rtio_submit_mrsh.c>
+
+
+static inline struct rtio * z_vrfy_rtio_pool_acquire(struct rtio_pool *rpool)
+{
+	K_OOPS(K_SYSCALL_OBJ(rpool, K_OBJ_RTIO_POOL));
+
+	return z_impl_rtio_pool_acquire(rpool);
+}
+#include <zephyr/sysaclls/rtio_pool_acquire_mrsh.c>
+
+
+static inline void z_vrfy_rtio_pool_release(struct rtio_pool *rpool, struct rtio *r)
+{
+	K_OOPS(K_SYSCALL_OBJ(rpool, K_OBJ_RTIO_POOL));
+	K_OOPS(K_SYSCALL_OBJ(r, K_OBJ_RTIO));
+
+	z_impl_rtio_pool_release(rpool, r);
+}
+#include <zephyr/sysaclls/rtio_pool_acquire_mrsh.c>

tests/subsys/rtio/rtio_api/src/test_rtio_pool.c
@@ -1,0 +1,61 @@
+/*
+ * Copyright (c) 2025 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/atomic.h>
+#include <zephyr/sys/kobject.h>
+#include <zephyr/sys/libc-hooks.h>
+#include <zephyr/app_memory/mem_domain.h>
+#include <zephyr/sys/util_loops.h>
+#include <zephyr/sys/time_units.h>
+#include <zephyr/timing/timing.h>
+#include <zephyr/rtio/rtio.h>
+#include <zephyr/rtio/pool.h>
+
+RTIO_POOL_DEFINE(rpool, 2, 8, 8);
+
+ZTEST_USER(rtio_pool, test_rtio_pool_acquire_release)
+{
+	struct rtio *r = rtio_pool_acquire(&rpool);
+
+	zassert_not_null(r, "expected valid rtio context");
+
+	struct rtio_sqe nop_sqe;
+	struct rtio_cqe nop_cqe;
+
+	rtio_sqe_prep_nop(&nop_sqe, NULL, NULL);
+	rtio_sqe_copy_in(r, &nop_sqe, 1);
+	rtio_submit(r, 1);
+	rtio_cqe_copy_out(r, &nop_cqe, 1, K_FOREVER);
+
+	rtio_pool_release(&rpool, r);
+}
+
+static void *rtio_pool_setup(void)
+{
+#ifdef CONFIG_USERSPACE
+	k_mem_domain_init(&rtio_domain, 0, NULL);
+	k_mem_domain_add_partition(&rtio_domain, &rtio_partition);
+#if Z_LIBC_PARTITION_EXISTS
+	k_mem_domain_add_partition(&rtio_domain, &z_libc_partition);
+#endif /* Z_LIBC_PARTITION_EXISTS */
+#endif /* CONFIG_USERSPACE */
+
+	return NULL;
+}
+
+static void rtio_pool_before(void *a)
+{
+	ARG_UNUSED(a);
+
+#ifdef CONFIG_USERSPACE
+	k_object_access_grant(&rpool, k_current_get());
+#endif /* CONFIG_USERSPACE */
+}
+
+ZTEST_SUITE(rtio_pool, NULL, rtio_pool_setup, rtio_pool_before, NULL, NULL);

jostapei99/cs736-zephyr

subsys/rtio/rtio_syscalls.c
@@ -135,3 +135,22 @@ static inline int z_vrfy_rtio_submit(struct rtio *r, uint32_t wait_count)
 	return z_impl_rtio_submit(r, wait_count);
 }
 #include <zephyr/syscalls/rtio_submit_mrsh.c>
+
+
+static inline struct rtio *z_vrfy_rtio_pool_acquire(struct rtio_pool *rpool)
+{
+	K_OOPS(K_SYSCALL_OBJ(rpool, K_OBJ_RTIO_POOL));
+
+	return z_impl_rtio_pool_acquire(rpool);
+}
+#include <zephyr/syscalls/rtio_pool_acquire_mrsh.c>
+
+
+static inline void z_vrfy_rtio_pool_release(struct rtio_pool *rpool, struct rtio *r)
+{
+	K_OOPS(K_SYSCALL_OBJ(rpool, K_OBJ_RTIO_POOL));
+	K_OOPS(K_SYSCALL_OBJ(r, K_OBJ_RTIO));
+
+	z_impl_rtio_pool_release(rpool, r);
+}
+#include <zephyr/syscalls/rtio_pool_release_mrsh.c>

tests/subsys/rtio/rtio_api/src/test_rtio_pool.c
@@ -1,0 +1,65 @@
+/*
+ * Copyright (c) 2025 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/atomic.h>
+#include <zephyr/sys/kobject.h>
+#include <zephyr/sys/libc-hooks.h>
+#include <zephyr/app_memory/mem_domain.h>
+#include <zephyr/sys/util_loops.h>
+#include <zephyr/sys/time_units.h>
+#include <zephyr/timing/timing.h>
+#include <zephyr/rtio/rtio.h>
+
+RTIO_POOL_DEFINE(rpool, 2, 8, 8);
+
+#ifdef CONFIG_USERSPACE
+struct k_mem_domain pool_domain;
+#endif
+
+
+ZTEST_USER(rtio_pool, test_rtio_pool_acquire_release)
+{
+	struct rtio *r = rtio_pool_acquire(&rpool);
+
+	zassert_not_null(r, "expected valid rtio context");
+
+	struct rtio_sqe nop_sqe;
+	struct rtio_cqe nop_cqe;
+
+	rtio_sqe_prep_nop(&nop_sqe, NULL, NULL);
+	rtio_sqe_copy_in(r, &nop_sqe, 1);
+	rtio_submit(r, 1);
+	rtio_cqe_copy_out(r, &nop_cqe, 1, K_FOREVER);
+
+	rtio_pool_release(&rpool, r);
+}
+
+static void *rtio_pool_setup(void)
+{
+#ifdef CONFIG_USERSPACE
+	k_mem_domain_init(&pool_domain, 0, NULL);
+	k_mem_domain_add_partition(&pool_domain, &rtio_partition);
+#if Z_LIBC_PARTITION_EXISTS
+	k_mem_domain_add_partition(&pool_domain, &z_libc_partition);
+#endif /* Z_LIBC_PARTITION_EXISTS */
+#endif /* CONFIG_USERSPACE */
+
+	return NULL;
+}
+
+static void rtio_pool_before(void *a)
+{
+	ARG_UNUSED(a);
+
+#ifdef CONFIG_USERSPACE
+	k_object_access_grant(&rpool, k_current_get());
+#endif /* CONFIG_USERSPACE */
+}
+
+ZTEST_SUITE(rtio_pool, NULL, rtio_pool_setup, rtio_pool_before, NULL, NULL);