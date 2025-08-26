zephyrproject-rtos/zephyr

nvs: support sector_size of 64KB
Allows NVS to work with flash device configured to
use only 64KB block erase. Due to how addresses
are encoded internally in NVS, 64KB is the maximum
sector size. Add a test for this during mount.

Add a native_sim unit test case for 64kb erase block size

Signed-off-by: Mike J. Chen <mjchen@google.com>

subsys/fs/nvs/nvs.c
@@ -1025,6 +1025,13 @@ int nvs_mount(struct nvs_fs *fs)
 		return -EINVAL;
 	}
 
+	/* check that sector size is not greater than max */
+	if (fs->sector_size > NVS_MAX_SECTOR_SIZE) {
+		LOG_ERR("Sector size %u too large, maximum is %u",
+			fs->sector_size, NVS_MAX_SECTOR_SIZE);
+		return -EINVAL;
+	}
+
 	/* check that sector size is a multiple of pagesize */
 	rc = flash_get_page_info_by_offs(fs->flash_device, fs->offset, &info);
 	if (rc) {

jostapei99/cs736-zephyr

subsys/fs/nvs/nvs.c
@@ -1025,6 +1025,13 @@ int nvs_mount(struct nvs_fs *fs)
 		return -EINVAL;
 	}
 
+	/* check that sector size is not greater than max */
+	if (fs->sector_size > NVS_MAX_SECTOR_SIZE) {
+		LOG_ERR("Sector size %u too large, maximum is %zu",
+			fs->sector_size, NVS_MAX_SECTOR_SIZE);
+		return -EINVAL;
+	}
+
 	/* check that sector size is a multiple of pagesize */
 	rc = flash_get_page_info_by_offs(fs->flash_device, fs->offset, &info);
 	if (rc) {