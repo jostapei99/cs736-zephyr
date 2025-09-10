zephyrproject-rtos/zephyr

soc: mspm0: add poweroff support
add support for SHUTDOWN operating mode in TI MSPM0 series for power-off
operation. Uses HWINFO for reset cause detection to handle shutdown IO
release on low power wakeup.

Signed-off-by: Sanjay Vallimanalan <sanjay@linumiz.com>

soc/ti/mspm0/common/poweroff.c
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Linumiz GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/sys/poweroff.h>
+#include <zephyr/toolchain.h>
+#include <zephyr/drivers/hwinfo.h>
+#include <soc.h>
+
+#include <ti/driverlib/driverlib.h>
+
+void z_sys_poweroff(void)
+{
+	DL_SYSCTL_setPowerPolicySHUTDOWN();
+	__WFI();
+
+	CODE_UNREACHABLE;
+}
+
+static int ti_mspm0l2xxx_poweroff_init(void)
+{
+	int ret;
+	uint32_t rst_cause;
+
+	ret = hwinfo_get_reset_cause(&rst_cause);
+	if (ret != 0) {
+		return ret;
+	}
+
+	if (RESET_LOW_POWER_WAKE == rst_cause) {
+		DL_SYSCTL_releaseShutdownIO();
+	}
+
+	return 0;
+}
+SYS_INIT(ti_mspm0l2xxx_poweroff_init, POST_KERNEL, 0);

jostapei99/cs736-zephyr

Signed-off-by: Parthiban Nallathambi <parthiban@linumiz.com>

soc/ti/mspm0/common/poweroff.c
@@ -1,0 +1,40 @@
+/*
+ * Copyright (c) 2025 Linumiz GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <soc.h>
+#include <zephyr/drivers/hwinfo.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/poweroff.h>
+#include <zephyr/toolchain.h>
+
+#include <ti/driverlib/driverlib.h>
+
+void z_sys_poweroff(void)
+{
+	DL_SYSCTL_setPowerPolicySHUTDOWN();
+	__WFI();
+
+	CODE_UNREACHABLE;
+}
+
+static int ti_mspm0_poweroff_init(void)
+{
+	int ret;
+	uint32_t rst_cause;
+
+	ret = hwinfo_get_reset_cause(&rst_cause);
+	if (ret != 0) {
+		return ret;
+	}
+
+	if (RESET_LOW_POWER_WAKE == rst_cause) {
+		DL_SYSCTL_releaseShutdownIO();
+	}
+
+	return 0;
+}
+
+SYS_INIT(ti_mspm0_poweroff_init, POST_KERNEL, 0);