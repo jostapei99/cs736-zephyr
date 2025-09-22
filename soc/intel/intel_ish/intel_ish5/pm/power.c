zephyrproject-rtos/zephyr

soc: intel_ish: Improve ISH PM logging
- Add Kconfig entries to register a dedicated ISH_PM logging module, so
  ISH PM can be configured independently.
- Rename LOG_MODULE_REGISTER from pm_service to ish_pm and use
  CONFIG_ISH_PM_LOG_LEVEL.
- Guard verbose debug traces so they only print for suspend-to-RAM/long
  idle states, reducing runtime noise.
- Promote an unsupported power state message from LOG_DBG to LOG_ERR.
- cleanup header files included.

No functional change to power handling; changes are limited to logging
and verbosity control.

Signed-off-by: Dong Wang <dong.d.wang@intel.com>

soc/intel/intel_ish/intel_ish5/pm/power.c
@@ -1,21 +1,13 @@
 /*
- * Copyright (c) 2023 Intel Corporation
+ * Copyright (c) 2023-2025 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/pm/pm.h>
-#include <zephyr/pm/policy.h>
-#include <zephyr/device.h>
-#include <zephyr/init.h>
-#include <zephyr/irq.h>
-#include <zephyr/logging/log_core.h>
-#include <stdio.h>
-#include <zephyr/drivers/interrupt_controller/ioapic.h>
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(pm_service, CONFIG_PM_LOG_LEVEL);
+LOG_MODULE_REGISTER(ish_pm, CONFIG_ISH_PM_LOG_LEVEL);
 
 /* low power modes */
 #define FW_D0		0
@@ -28,8 +20,10 @@ extern void sedi_pm_enter_power_state(int state);
 
 void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
-	LOG_DBG(" -> SoC D0i%u, idle=%u ms", substate_id,
+	if (state >= PM_STATE_SUSPEND_TO_RAM) {
+		LOG_DBG(" -> SoC D0i%u, idle=%u ms", substate_id,
 				k_ticks_to_ms_floor32(_kernel.idle));
+	}
 
 	switch (state) {
 	case PM_STATE_RUNTIME_IDLE:
@@ -45,11 +39,13 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 		sedi_pm_enter_power_state(FW_D0i3);
 		break;
 	default:
-		LOG_DBG("Unsupported power state %u", state);
+		LOG_ERR("Unsupported power state %u", state);
 		break;
 	}
 
-	LOG_DBG("SoC D0i%u ->", substate_id);
+	if (state >= PM_STATE_SUSPEND_TO_RAM) {
+		LOG_DBG("SoC D0i%u ->", substate_id);
+	}
 }
 
 /* Handle SOC specific activity after Low Power Mode Exit */

jostapei99/cs736-zephyr

  CONFIG_PM_LOG_LEVEL.

soc/intel/intel_ish/intel_ish5/pm/power.c
@@ -1,21 +1,13 @@
 /*
- * Copyright (c) 2023 Intel Corporation
+ * Copyright (c) 2023-2025 Intel Corporation
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/pm/pm.h>
-#include <zephyr/pm/policy.h>
-#include <zephyr/device.h>
-#include <zephyr/init.h>
-#include <zephyr/irq.h>
-#include <zephyr/logging/log_core.h>
-#include <stdio.h>
-#include <zephyr/drivers/interrupt_controller/ioapic.h>
 
 #include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(pm_service, CONFIG_PM_LOG_LEVEL);
+LOG_MODULE_REGISTER(ish_pm, CONFIG_PM_LOG_LEVEL);
 
 /* low power modes */
 #define FW_D0		0
@@ -28,8 +20,11 @@ extern void sedi_pm_enter_power_state(int state);
 
 void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
-	LOG_DBG(" -> SoC D0i%u, idle=%u ms", substate_id,
+	if (state >= PM_STATE_SUSPEND_TO_RAM) {
+		/* Avoid tedious logs for power states lower than SUSPEND_TO_RAM */
+		LOG_DBG(" -> SoC D0i%u, idle=%u ms", substate_id,
 				k_ticks_to_ms_floor32(_kernel.idle));
+	}
 
 	switch (state) {
 	case PM_STATE_RUNTIME_IDLE:
@@ -45,11 +40,13 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 		sedi_pm_enter_power_state(FW_D0i3);
 		break;
 	default:
-		LOG_DBG("Unsupported power state %u", state);
+		LOG_ERR("Unsupported power state %u", state);
 		break;
 	}
 
-	LOG_DBG("SoC D0i%u ->", substate_id);
+	if (state >= PM_STATE_SUSPEND_TO_RAM) {
+		LOG_DBG("SoC D0i%u ->", substate_id);
+	}
 }
 
 /* Handle SOC specific activity after Low Power Mode Exit */