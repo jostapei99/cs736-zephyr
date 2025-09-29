zephyrproject-rtos/zephyr

tests: kernel: gen_isr_table: Add support for hazard3
Extend the gen_isr_table test to support the Hazard3 cores on the
RP2350A. This allows some testing when building and running using flash,
but it seems like Twister builds with different flags. It's unclear if
this is intended or not.

Signed-off-by: Andrew Featherstone <andrew.featherstone@gmail.com>

subsys/testsuite/include/zephyr/interrupt_util.h
@@ -164,11 +164,20 @@ static inline void trigger_irq(int irq)
 }
 
 #elif defined(CONFIG_RISCV)
+#if defined(CONFIG_SOC_SERIES_RP2350)
+#include <hardware/irq.h>
+#endif
+
 #if defined(CONFIG_CLIC) || defined(CONFIG_NRFX_CLIC)
 void riscv_clic_irq_set_pending(uint32_t irq);
 static inline void trigger_irq(int irq)
 {
 	riscv_clic_irq_set_pending(irq);
+}
+#elif defined(CONFIG_SOC_SERIES_RP2350)
+static inline void trigger_irq(int irq)
+{
+	irq_set_pending(irq);
 }
 #else
 static inline void trigger_irq(int irq)

tests/kernel/gen_isr_table/src/main.c
@@ -12,6 +12,10 @@
 #include <zephyr/interrupt_util.h>
 #include <zephyr/sys/barrier.h>
 
+#if defined(CONFIG_SOC_SERIES_RP2350)
+#include <hardware/irq.h>
+#endif
+
 extern uintptr_t _irq_vector_table[];
 
 #if defined(ARCH_IRQ_DIRECT_CONNECT) && defined(CONFIG_GEN_IRQ_VECTOR_TABLE)
@@ -40,6 +44,12 @@ extern uintptr_t _irq_vector_table[];
 #define ISR3_OFFSET	17
 #define ISR5_OFFSET	18
 #define TRIG_CHECK_SIZE	19
+#elif defined(CONFIG_SOC_SERIES_RP2350)
+#define ISR3_OFFSET SPARE_IRQ_2
+#define ISR4_OFFSET SPARE_IRQ_3
+#define ISR5_OFFSET SPARE_IRQ_4
+#define ISR6_OFFSET SPARE_IRQ_5
+#define TRIG_CHECK_SIZE (ISR6_OFFSET + 1)
 #else
 
 /* RISC-V has very few IRQ lines which can be triggered from software */

jostapei99/cs736-zephyr

subsys/testsuite/include/zephyr/interrupt_util.h
@@ -165,11 +165,20 @@ static inline void trigger_irq(int irq)
 }
 
 #elif defined(CONFIG_RISCV)
+#if defined(CONFIG_HAZARD3_INTC)
+#include <hardware/irq.h>
+#endif
+
 #if defined(CONFIG_CLIC) || defined(CONFIG_NRFX_CLIC)
 void riscv_clic_irq_set_pending(uint32_t irq);
 static inline void trigger_irq(int irq)
 {
 	riscv_clic_irq_set_pending(irq);
+}
+#elif defined(CONFIG_HAZARD3_INTC)
+static inline void trigger_irq(int irq)
+{
+	irq_set_pending(irq);
 }
 #else
 static inline void trigger_irq(int irq)

tests/arch/common/gen_isr_table/src/main.c
@@ -12,6 +12,10 @@
 #include <zephyr/interrupt_util.h>
 #include <zephyr/sys/barrier.h>
 
+#if defined(CONFIG_HAZARD3_INTC)
+#include <hardware/irq.h>
+#endif
+
 extern const uintptr_t _irq_vector_table[];
 
 #if defined(ARCH_IRQ_DIRECT_CONNECT) && defined(CONFIG_GEN_IRQ_VECTOR_TABLE)
@@ -56,6 +60,12 @@ extern const uintptr_t _irq_vector_table[];
 #define ISR3_OFFSET	20
 #define ISR5_OFFSET	21
 #define TRIG_CHECK_SIZE	22
+#elif defined(CONFIG_HAZARD3_INTC)
+#define ISR3_OFFSET SPARE_IRQ_2
+#define ISR4_OFFSET SPARE_IRQ_3
+#define ISR5_OFFSET SPARE_IRQ_4
+#define ISR6_OFFSET SPARE_IRQ_5
+#define TRIG_CHECK_SIZE (ISR6_OFFSET + 1)
 #else
 
 #if !defined(IRQ1_USED)